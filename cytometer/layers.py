d6a799002ddf38c2531b8993236524b5ca056827
Ramón Casero <rcasero@gmail.com>
2017-08-17 18:25:47 +0100

    start extending MaxPooling2D to support dilation

--- cytometer/layers.py
+++ cytometer/layers.py
@@ -19,7 +19,7 @@ class _Pooling2D(Layer):
     """
 
     def __init__(self, pool_size=(2, 2), strides=None, padding='valid',
-                 data_format=None, **kwargs):
+                 data_format=None, dilation_rate=1, **kwargs):
         super(_Pooling2D, self).__init__(**kwargs)
         data_format = conv_utils.normalize_data_format(data_format)
         if strides is None:
@@ -28,6 +28,7 @@ class _Pooling2D(Layer):
         self.strides = conv_utils.normalize_tuple(strides, 2, 'strides')
         self.padding = conv_utils.normalize_padding(padding)
         self.data_format = conv_utils.normalize_data_format(data_format)
+        self.dilation_rate = conv_utils.normalize_tuple(dilation_rate, rank=2, 'dilation_rate')
         self.input_spec = InputSpec(ndim=4)
 
     def compute_output_shape(self, input_shape):
@@ -38,16 +39,18 @@ class _Pooling2D(Layer):
             rows = input_shape[1]
             cols = input_shape[2]
         rows = conv_utils.conv_output_length(rows, self.pool_size[0],
-                                             self.padding, self.strides[0])
+                                             self.padding, self.strides[0],
+                                             self.dilation_rate[0])
         cols = conv_utils.conv_output_length(cols, self.pool_size[1],
-                                             self.padding, self.strides[1])
+                                             self.padding, self.strides[1],
+                                             self.dilation_rate[1])
         if self.data_format == 'channels_first':
             return (input_shape[0], input_shape[1], rows, cols)
         elif self.data_format == 'channels_last':
             return (input_shape[0], rows, cols, input_shape[3])
 
     def _pooling_function(self, inputs, pool_size, strides,
-                          padding, data_format):
+                          padding, data_format, dilation_rate):
         raise NotImplementedError
 
     def call(self, inputs):
@@ -55,14 +58,16 @@ class _Pooling2D(Layer):
                                         pool_size=self.pool_size,
                                         strides=self.strides,
                                         padding=self.padding,
-                                        data_format=self.data_format)
+                                        data_format=self.data_format,
+                                        dilation_rate=self.dilation_rate)
         return output
 
     def get_config(self):
         config = {'pool_size': self.pool_size,
                   'padding': self.padding,
                   'strides': self.strides,
-                  'data_format': self.data_format}
+                  'data_format': self.data_format,
+                  'dilation_rate': self.dilation_rate}
         base_config = super(_Pooling2D, self).get_config()
         return dict(list(base_config.items()) + list(config.items()))
 
@@ -85,13 +90,14 @@ class MaxPooling2D(_Pooling2D):
             `channels_last` corresponds to inputs with shape
             `(batch, height, width, channels)` while `channels_first`
             corresponds to inputs with shape
-            `(batch, channels, height, width)`.An integer or tuple/list of n integers, specifying
-            the dilation rate to use for dilated convolution.
-            Currently, specifying any `dilation_rate` value != 1 is
-            incompatible with specifying any `strides` value != 1.
+            `(batch, channels, height, width)`.
             It defaults to the `image_data_format` value found in your
             Keras config file at `~/.keras/keras.json`.
             If you never set it, then it will be "channels_last".
+        dilation_rate: An integer or tuple/list of n integers, specifying
+            the dilation rate to use for dilated convolution.
+            Currently, specifying any `dilation_rate` value != 1 is
+            incompatible with specifying any `strides` value != 1.
 
     # Input shape
         - If `data_format='channels_last'`:
@@ -112,14 +118,15 @@ class MaxPooling2D(_Pooling2D):
 
     @interfaces.legacy_pooling2d_support
     def __init__(self, pool_size=(2, 2), strides=None, padding='valid',
-                 data_format=None, **kwargs):
+                 data_format=None, dilation_rate=1, **kwargs):
         super(MaxPooling2D, self).__init__(pool_size, strides, padding,
                                            data_format, **kwargs)
 
+    # TODO: Replace K.pool2d by pooling with dilation
     def _pooling_function(self, inputs, pool_size, strides,
-                          padding, data_format):
+                          padding, data_format, dilation_rate):
         output = K.pool2d(inputs, pool_size, strides,
-                          padding, data_format,
+                          padding, data_format, dilation_rate,
                           pool_mode='max')
         return output
 
