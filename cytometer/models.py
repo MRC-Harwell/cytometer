0670469e6ae166637f8ea7a3f7e10348bce64183
Ramón Casero <rcasero@gmail.com>
2017-08-17 18:26:28 +0100

    Start adding Keras 2 version of DeepCell's sparse_feature_net_61x61

---
+++ cytometer/models.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 15 17:59:44 2017
+
+@author: rcasero
+"""
+
+from keras import __version__ as keras_version
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Flatten
+from cytometer.layers import MaxPooling2D
+from keras.layers.normalization import BatchNormalization
+from keras.regularizers import l2
+import keras.backend as K
+
+def sparse_feature_net_61x61(input_shape = (3,1080,1280), n_features = 3, reg = 0.001, init = 'he_normal', weights_path = None):
+
+    model = Sequential()
+    
+    model.add(Conv2D(input_shape = input_shape, 
+                     filters = 64, kernel_size = (3, 3), dilation_rate = 1, 
+                     kernel_initializer = init, padding = 'valid', 
+                     kernel_regularizer = l2(reg)))
+    model.add(Activation('relu'))
+    
+    model.add(Conv2D(filters = 64, kernel_size = (4, 4), dilation_rate = 1, 
+                     kernel_initializer = init, padding = 'valid', 
+                     kernel_regularizer = l2(reg)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size = (2, 2), strides = (1, 1)))
+#        d *= 2
+#        
+#        model.add(sparse_Convolution2D(64, 3, 3, d = d, init=init, border_mode='valid', W_regularizer = l2(reg)))
+#        model.add(Activation('relu'))
+#        
+#        model.add(sparse_Convolution2D(64, 3, 3, d = d, init=init, border_mode = 'valid', W_regularizer = l2(reg)))
+#        model.add(Activation('relu'))
+#        model.add(sparse_MaxPooling2D(pool_size=(2, 2), strides = (d,d)))
+#        d *= 2
+#        
+#        model.add(sparse_Convolution2D(64, 3, 3, d = d, init=init, border_mode='valid', W_regularizer = l2(reg)))
+#        model.add(Activation('relu'))
+#        
+#        model.add(sparse_Convolution2D(64, 3, 3, d = d, init=init, border_mode = 'valid', W_regularizer = l2(reg)))
+#        model.add(Activation('relu'))
+#        model.add(sparse_MaxPooling2D(pool_size=(2, 2), strides = (d,d)))
+#        d *= 2
+#        
+#        model.add(sparse_Convolution2D(200, 4, 4, d = d, init=init, border_mode = 'valid', W_regularizer = l2(reg)))
+#        model.add(Activation('relu'))
+#        
+#        model.add(TensorProd2D(200, 200, kernel_initializer=init, W_regularizer = l2(reg)))
+#        model.add(Activation('relu'))
+#        
+#        model.add(TensorProd2D(200, n_features, kernel_initializer=init, W_regularizer = l2(reg)))
+#        model.add(Activation(tensorprod_softmax))
+#        
+    # exit
+    return model
