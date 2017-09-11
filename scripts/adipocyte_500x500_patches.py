e44311a59e8422581d8459ad018949fc0e4156a8
Ramón Casero <rcasero@gmail.com>
2017-09-11 18:46:38 +0100

    Plot raw images

--- scripts/adipocyte_500x500_patches.py
+++ scripts/adipocyte_500x500_patches.py
@@ -6,7 +6,7 @@ Created on Fri Sep  8 17:59:37 2017
 @author: rcasero
 """
 
-# Keras backend
+# select backend
 import os
 os.environ['KERAS_BACKEND'] = 'theano'
 #os.environ['KERAS_BACKEND'] = 'tensorflow'
@@ -27,8 +27,6 @@ if os.environ['KERAS_BACKEND'] == 'theano':
     theano.config.dnn.include_path = os.environ['CONDA_PREFIX'] + '/include'
     theano.config.dnn.library_path = os.environ['CONDA_PREFIX'] + '/lib'
     theano.config.blas.ldflags = '-lblas -lgfortran'
-    theano.config.nvcc.fastmath = True
-    theano.config.nvcc.flags = '-D_FORCE_INLINES'
     theano.config.cxx = os.environ['CONDA_PREFIX'] + '/bin/g++'
 else :
     raise Exception('No configuration found when the backend is ' + os.environ['KERAS_BACKEND'])
@@ -38,8 +36,6 @@ import keras
 import keras.backend as K
 from keras import __version__ as keras_version
 from pkg_resources import parse_version
-if (parse_version(keras_version) >= parse_version('2.0')):
-    raise RuntimeError('DeepCell requires Keras 1 to run')
 
 # configure Keras, to avoid using file ~/.keras/keras.json
 K.set_image_dim_ordering('th') # theano's image format (required by DeepCell)
@@ -49,3 +45,8 @@ K.set_epsilon('1e-07')
 # load dependencies
 import matplotlib.pyplot as plt
 import numpy as np
+import cytometer
+
+# data paths
+basedatadir = os.path.normpath(os.path.join(cytometer.__path__[0], '../data'))
+datadir = os.path.join(basedatadir, 'adipocyte_500x500_patches')
