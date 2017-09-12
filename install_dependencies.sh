52842c0e9f30e15f01668b0818ce3808471d6809
Ramón Casero <rcasero@gmail.com>
2017-09-12 16:43:29 +0100

    install opencv

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -104,6 +104,7 @@ conda install -y matplotlib pillow spyder
 conda install -y scikit-image scikit-learn h5py
 conda install -y -c conda-forge tifffile mahotas
 conda install -y nose pytest
+pip install opencv-python
 
 # clear Theano cache. Previous runs of Keras may cause CUDA compilation/version compatibility problems
 theano-cache purge
