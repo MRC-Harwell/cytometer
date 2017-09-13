626e878dce3432b6595796dc3fa3c533046dec9a
Lorna Nolan <l.nolan@har.mrc.ac.uk>
2017-09-13 15:35:13 +0100

    Update unet.py

--- cytometer/unet.py
+++ cytometer/unet.py
@@ -14,8 +14,8 @@ from data import load_train_data, load_test_data
 
 K.set_image_data_format('channels_last')  # TF dimension ordering in this code
 
-img_rows = 96
-img_cols = 96
+img_rows = 224
+img_cols = 224
 
 smooth = 1.
 
@@ -31,19 +31,22 @@ def dice_coef_loss(y_true, y_pred):
     return -dice_coef(y_true, y_pred)
 
 
-def get_unet():
-    inputs = Input((img_rows, img_cols, 1))
-    conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(inputs)
-    conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(conv1)
-    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+def get_unet(weights_path=None):
+    inputs = Input((img_rows, img_cols, 3))
+    conv1_7x7_s2 = Conv2D(64, (7, 7), activation='relu', padding='same')(inputs)
+    conv1_7x7_s2 = Conv2D(64, (7, 7), activation='relu', padding='same')(conv1_7x7_s2)
+    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1_7x7_s2)
+    conv1_7x7_s2.trainable = False
 
-    conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(pool1)
-    conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv2)
-    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+    conv2_3x3_reduce = Conv2D(64, (1, 1), activation='relu', padding='same')(pool1)
+    conv2_3x3_reduce = Conv2D(64, (1, 1), activation='relu', padding='same')(conv2_3x3_reduce)
+    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2_3x3_reduce)
+    conv2_3x3_reduce = False
 
-    conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(pool2)
-    conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv3)
-    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+    conv2_3x3 = Conv2D(192, (3, 3), activation='relu', padding='same')(pool2)
+    conv2_3x3 = Conv2D(192, (3, 3), activation='relu', padding='same')(conv2_3x3)
+    pool3 = MaxPooling2D(pool_size=(2, 2))(conv2_3x3)
+    conv2_3x3.trainable = False
 
     conv4 = Conv2D(256, (3, 3), activation='relu', padding='same')(pool3)
     conv4 = Conv2D(256, (3, 3), activation='relu', padding='same')(conv4)
@@ -71,17 +74,29 @@ def get_unet():
     conv10 = Conv2D(1, (1, 1), activation='sigmoid')(conv9)
 
     model = Model(inputs=[inputs], outputs=[conv10])
+    
+    if weights_path:
+        model.load_weights(weights_path, by_name=True)
 
     model.compile(optimizer=Adam(lr=1e-5), loss=dice_coef_loss, metrics=[dice_coef])
 
     return model
 
 
-def preprocess(imgs):
-    imgs_p = np.ndarray((imgs.shape[0], img_rows, img_cols), dtype=np.uint8)
+def preprocess_image(imgs):
+    imgs_p = np.ndarray((imgs.shape[0], img_rows, img_cols, 3), dtype=np.uint8)
+    for i in range(imgs.shape[0]):
+        imgs_p[i] = resize(imgs[i], (img_cols, img_rows, 3), preserve_range=True)
+
+    # imgs_p = imgs_p[..., np.newaxis]
+    return imgs_p
+
+
+def preprocess_mask(imgs):
+    imgs_p = np.ndarrat((imgs.shape[0], img_rows, img_cols), dtype=np.uint8)
     for i in range(imgs.shape[0]):
         imgs_p[i] = resize(imgs[i], (img_cols, img_rows), preserve_range=True)
-
+    
     imgs_p = imgs_p[..., np.newaxis]
     return imgs_p
 
@@ -92,8 +107,8 @@ def train_and_predict():
     print('-'*30)
     imgs_train, imgs_mask_train = load_train_data()
 
-    imgs_train = preprocess(imgs_train)
-    imgs_mask_train = preprocess(imgs_mask_train)
+    imgs_train = preprocess_image(imgs_train)
+    imgs_mask_train = preprocess_mask(imgs_mask_train)
 
     imgs_train = imgs_train.astype('float32')
     mean = np.mean(imgs_train)  # mean for data centering
@@ -108,15 +123,16 @@ def train_and_predict():
     print('-'*30)
     print('Creating and compiling model...')
     print('-'*30)
-    model = get_unet()
-    model_checkpoint = ModelCheckpoint('weights.h5', monitor='val_loss', save_best_only=True)
+    model = get_unet('googlenet_weights.h5')
+    model_checkpoint = ModelCheckpoint('weights_RGB.h5', monitor='val_loss', save_best_only=True)
 
     print('-'*30)
     print('Fitting model...')
     print('-'*30)
-    model.fit(imgs_train, imgs_mask_train, batch_size=32, nb_epoch=20, verbose=1, shuffle=True,
+    model.fit(imgs_train, imgs_mask_train, batch_size=32, epochs=20, verbose=1, shuffle=True,
               validation_split=0.2,
               callbacks=[model_checkpoint])
+    model.save_weights('weights_RGB.h5')
 
     print('-'*30)
     print('Loading and preprocessing test data...')
@@ -131,7 +147,7 @@ def train_and_predict():
     print('-'*30)
     print('Loading saved weights...')
     print('-'*30)
-    model.load_weights('weights.h5')
+    model.load_weights('weights_RGB.h5')
 
     print('-'*30)
     print('Predicting masks on test data...')
