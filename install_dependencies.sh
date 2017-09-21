b8408dccfe3e55aacfc62c89bf92fedf1887d7a0
Ramón Casero <rcasero@gmail.com>
2017-09-21 15:53:55 +0100

    Add pysto dependency

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -80,7 +80,7 @@ conda install -y matplotlib pillow spyder
 conda install -y scikit-image scikit-learn h5py
 conda install -y -c conda-forge tifffile mahotas
 conda install -y nose pytest
-pip install opencv-python
+pip install opencv-python pysto
 
 ########################################################################
 ## python environment for DeepCell
@@ -105,7 +105,7 @@ conda install -y matplotlib pillow spyder
 conda install -y scikit-image scikit-learn h5py
 conda install -y -c conda-forge tifffile mahotas
 conda install -y nose pytest
-pip install opencv-python
+pip install opencv-python pysto
 
 # clear Theano cache. Previous runs of Keras may cause CUDA compilation/version compatibility problems
 theano-cache purge
