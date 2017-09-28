c137f2f89f621595a467545282f64cd2a18c6994
Ramón Casero <rcasero@gmail.com>
2017-09-28 18:28:51 +0100

    start code to train DeepCell models redefined in Keras 2

---
+++ scripts/cytometer_deepcell_keras2_training.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 28 17:43:25 2017
+
+@author: rcasero
+"""
+
+import os
+import numpy as np
+import datetime
+
+###############################################################################
+## Keras configuration
+
+# Keras backend
+os.environ['KERAS_BACKEND'] = 'theano'
+os.environ['OMP_NUM_THREADS'] = '6'
+
+# access to python libraries
+if 'LIBRARY_PATH' in os.environ:
+    os.environ['LIBRARY_PATH'] = os.environ['CONDA_PREFIX'] + '/lib:' + os.environ['LIBRARY_PATH']
+else:
+    os.environ['LIBRARY_PATH'] = os.environ['CONDA_PREFIX'] + '/lib'
+
+# configure Theano global options
+#os.environ['THEANO_FLAGS'] = 'floatX=float32,device=cuda0,gpuarray.preallocate=0.5'
+#os.environ['THEANO_FLAGS'] = 'floatX=float32,device=cuda0,lib.cnmem=0.75'
+os.environ['THEANO_FLAGS'] = 'floatX=float32,device=gpu'
+
+# configure Theano
+if os.environ['KERAS_BACKEND'] == 'theano':
+    import theano
+    theano.config.enabled = True
+    theano.config.dnn.include_path = os.environ['CONDA_PREFIX'] + '/include'
+    theano.config.dnn.library_path = os.environ['CONDA_PREFIX'] + '/lib'
+    theano.config.blas.ldflags = '-lblas -lgfortran'
+    theano.config.nvcc.fastmath = True
+    theano.config.nvcc.flags = '-D_FORCE_INLINES'
+    theano.config.cxx = os.environ['CONDA_PREFIX'] + '/bin/g++'
+else :
+    raise Exception('No configuration found when the backend is ' + os.environ['KERAS_BACKEND'])
+
+# import and check Keras version
+import keras
+import keras.backend as K
+from keras import __version__ as keras_version
+from pkg_resources import parse_version
+if (parse_version(keras_version) >= parse_version('2.0')):
+    raise RuntimeError('DeepCell requires Keras 1 to run')
+
+# configure Keras, to avoid using file ~/.keras/keras.json
+K.set_image_dim_ordering('th') # theano's image format (required by DeepCell)
+K.set_floatx('float32')
+K.set_epsilon('1e-07')
+
+##
+###############################################################################
+
