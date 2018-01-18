4eb97aa7ac1581d8ffa7eb6e64a4338eab99656e
Ramón Casero <rcasero@gmail.com>
2018-01-18 15:55:55 +0000

    update list of dependencies

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 from setuptools import find_packages
 
 setup(name='cytometer',
-      version='0.1.0',
+      version='0.2.0',
       description='Cell segmentation and quantification',
       author='Ramón Casero',
       author_email='rcasero@gmail.com',
@@ -13,7 +13,8 @@ setup(name='cytometer',
       license='GPLv3',
       packages=find_packages(),
       data_files=[('data', ['data/*'])],
-      install_requires=['python>=3.6', 'gcc>=4.8.5',
-                            'keras>=2.0.8', 'theano>=0.10.0', 'tensorflow-gpu>=1.3.0', 'pysto>=1.1.1',
+      install_requires=['python>=3.6',
+                            'keras>=2.1.2', 'theano>=1.0.1+28.g96c910444',
+                            'tensorflow-gpu>=1.4.1',
                             'pyyaml>=3.12', 'nose-parameterized>=0.6.0']
      )
