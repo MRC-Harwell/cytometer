a70c14c75aea2db50984daeac16563dbee459afd
Ramón Casero <rcasero@gmail.com>
2018-04-18 17:14:21 +0100

    find tissue within histology image

--- scripts/klf14_b6ntac_preprocess.py
+++ scripts/klf14_b6ntac_preprocess.py
@@ -1,8 +1,10 @@
 import os
 import openslide
 import numpy as np
+from statistics import mode
 import matplotlib.pyplot as plt
-from sklearn.cluster import KMeans
+import cv2
+
 
 data_dir = '/home/rcasero/data/roger_data'
 
@@ -18,8 +20,51 @@ for file in os.listdir(data_dir):
 
     # get downsampled image
     im_4 = im.read_region(location=(0, 0), level=level_4, size=im.level_dimensions[level_4])
+    im_4 = np.array(im_4)
+    im_4 = im_4[:, :, 0:3]
 
     plt.imshow(im_4)
     plt.pause(.1)
 
-    Kmeans(n_clusters=2, random_state=0).fit()
+    # reshape image to matrix with one column per colour channel
+    im_4_mat = im_4.copy()
+    im_4_mat = im_4_mat.reshape((im_4_mat.shape[0] * im_4_mat.shape[1], im_4_mat.shape[2]))
+
+    # background colour
+    background_colour = []
+    for i in range(3):
+        background_colour += [mode(im_4_mat[:, i]), ]
+    background_colour_std = np.std(im_4_mat, axis=0)
+
+    # threshold segmentation
+    seg = np.ones(im_4.shape[0:2], dtype=bool)
+    for i in range(3):
+        seg = np.logical_and(seg, im_4[:, :, i] < background_colour[i] - background_colour_std[i])
+    seg = seg.astype(dtype=np.uint8)
+    seg[seg == 1] = 255
+
+    # dilate the segmentation
+    kernel = np.ones((20, 20), np.uint8)
+    seg = cv2.dilate(seg, kernel, iterations=1)
+
+    # find connected components
+    labels = seg.copy()
+    nlabels, labels, stats, centroids = cv2.connectedComponentsWithStats(seg)
+    lblareas = stats[:, cv2.CC_STAT_AREA]
+
+    # labels of large components, that we assume correspond to tissue areas (label=0 is the background)
+    labels_large = np.where(lblareas > 1e5)[0]
+    labels_large = list(labels_large)
+    labels_large.remove(0)
+
+    # only set pixels that belong to the 
+
+    foo = seg.any(labels_large)
+
+    plt.clf()
+    plt.imshow(seg)
+    plt.pause(.1)
+
+    plt.figure()
+    plt.imshow(output)
+    plt.pause(.1)
