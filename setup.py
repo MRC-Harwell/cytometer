154c2a0790bf92627ba6308e4a8f74618ff3a8d0
Ramón Casero <rcasero@har.mrc.ac.uk>
2018-06-18 16:58:20 +0100

    dependencies for LME4 package from GNU R, to compute generalised linear mixed models

--- setup.py
+++ setup.py
@@ -18,5 +18,6 @@ setup(name='cytometer',
                         'matplotlib>=2.1.2', 'scikit-image>=0.13.1',
                         'scikit-learn>=0.19.1', 'scipy>=1.0.0',
                         'openslide-python>=1.1.1', 'tifffile>=0.14.0',
-                        'svgpathtools>=1.3.2', 'statsmodels>=0.9.0']
+                        'svgpathtools>=1.3.2', 'statsmodels>=0.9.0',
+                        'rpy2>=2.9.3', 'tzlocal>=1.5.1']
      )
