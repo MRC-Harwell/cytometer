c9add35819a0ab1a97b8962051ebeb1a4b9e4010
Ramón Casero <r.casero@har.mrc.ac.uk>
2018-07-03 00:31:38 +0100

    reduce arrays size to avoid out of memory error on laptop

--- scripts/keras_branch_sample_weight.py
+++ scripts/keras_branch_sample_weight.py
@@ -14,17 +14,18 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'  # Just disables the warning, doesn't e
 assert(K.image_data_format() == 'channels_first')
 
 # simulate input images
-im = np.zeros(shape=(10, 3, 128, 128), dtype='uint8')
+im = np.zeros(shape=(10, 3, 64, 64), dtype='uint8')
 
 # simulate network output
-out = np.zeros(shape=(10, 1, 128, 128), dtype='float32')
+out = 2 * np.ones(shape=(10, 1, 64, 64), dtype='float32')
 
 # simulate training weights for network output
-weight = np.zeros(shape=(10, 1, 128, 128), dtype='float32')
+weight = np.ones(shape=(10, 1, 64, 64), dtype='float32')
+#weight = np.zeros(shape=(10, ), dtype='float32')
 
 # create network model
 model = Sequential()
-model.add(Conv2D(input_shape=(3, 128, 128),
+model.add(Conv2D(input_shape=(3, 64, 64),
                  filters=32, kernel_size=(3, 3), strides=1, padding='same'))
 model.add(BatchNormalization(axis=3))
 model.add(Activation('relu'))
@@ -32,6 +33,6 @@ model.add(Conv2D(filters=1, kernel_size=(1, 1), strides=1, padding='same'))
 
 optimizer = keras.optimizers.SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
 
-model.compile(loss='mae', optimizer=optimizer, metrics=['accuracy'])
+model.compile(loss='mae', optimizer=optimizer, metrics=['accuracy'], sample_weight_mode='element')
 
 model.fit(im, out, sample_weight=weight, batch_size=3, epochs=3)
