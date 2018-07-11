2f4de4abfb8c24ae69421e02c5eb5304cd682145
Ramón Casero <rcasero@gmail.com>
2018-07-11 17:15:29 +0100

    small script to trigger bug with BatchNormalization(axis=1) in keras

---
+++ scripts/keras_bug_batchnormalization.py
@@ -0,0 +1,26 @@
+# import os
+# os.environ['KERAS_BACKEND'] = 'tensorflow'
+# import keras.backend as K
+# from keras.layers import Activation, Conv2D, Input
+# from keras.layers.normalization import BatchNormalization
+#
+# # declare network model with channels first: ERROR
+# K.set_image_data_format('channels_first')
+# input = Input(shape=(3, 1001, 1001), dtype='float32')
+# x = Conv2D(filters=64, kernel_size=(3, 3), strides=1, padding='same')(input)
+# x = BatchNormalization(axis=1)(x)
+# x = Activation('relu')(x)
+
+import os
+os.environ['KERAS_BACKEND'] = 'tensorflow'
+import keras.backend as K
+from keras.layers import Activation, Conv2D, Input
+from keras.layers.normalization import BatchNormalization
+
+# declare network model with channels last: NO ERROR
+K.set_image_data_format('channels_last')
+input = Input(shape=(1001, 1001, 3), dtype='float32')
+x = Conv2D(filters=64, kernel_size=(3, 3), strides=1, padding='same')(input)
+x = BatchNormalization(axis=3)(x)
+x = Activation('relu')(x)
+
