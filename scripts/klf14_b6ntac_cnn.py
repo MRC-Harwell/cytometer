f63685382f5551e93c5c41008c08c05f2302d34e
Ramón Casero <rcasero@gmail.com>
2018-07-11 17:43:13 +0100

    change from channels_first to channels_last

--- scripts/klf14_b6ntac_cnn.py
+++ scripts/klf14_b6ntac_cnn.py
@@ -6,14 +6,21 @@ import numpy as np
 import pysto.imgproc as pystoim
 
 # use CPU for testing on laptop
-os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"   # see issue #152
-os.environ["CUDA_VISIBLE_DEVICES"] = ""
+#os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"   # see issue #152
+#os.environ["CUDA_VISIBLE_DEVICES"] = ""
 
 os.environ['KERAS_BACKEND'] = 'tensorflow'
 import keras.backend as K
 from keras.models import Model
-from keras.layers import Activation, Conv2D, Input
+from keras.layers import Activation, Conv2D, Input, MaxPooling2D
 from keras.layers.normalization import BatchNormalization
+
+import tensorflow as tf
+from keras.backend.tensorflow_backend import set_session
+config = tf.ConfigProto()
+config.gpu_options.per_process_gpu_memory_fraction = 0.7
+set_session(tf.Session(config=config))
+
 
 
 DEBUG = False
@@ -25,14 +32,15 @@ training_nooverlap_data_dir = os.path.join(root_data_dir, 'klf14_b6ntac_training
 '''Load data
 '''
 
-K.set_image_data_format('channels_first')
+K.set_image_data_format('channels_last')
+norm_axis = 3
 
 labels_file_list = glob.glob(os.path.join(training_nooverlap_data_dir, '*.tif'))
 
 # allocate arrays for data
-im = np.zeros((len(labels_file_list), 3, 1001, 1001), dtype='uint8')
-labels = np.zeros((len(labels_file_list), 1, 1001, 1001), dtype='uint8')
-mask = np.zeros((len(labels_file_list), 1, 1001, 1001), dtype='uint8')
+im = np.zeros((len(labels_file_list), 1001, 1001, 3), dtype='uint8')
+labels = np.zeros((len(labels_file_list), 1001, 1001, 1), dtype='uint8')
+mask = np.zeros((len(labels_file_list), 1001, 1001, 1), dtype='uint8')
 
 # labels
 LABEL_BACKGROUND = 1
@@ -44,51 +52,33 @@ for i, labels_file in enumerate(labels_file_list):
     im_file = labels_file.replace(training_nooverlap_data_dir, training_dir)
 
     # load image
-    im[i, :, :, :] = tifffile.imread(im_file).transpose((2, 0, 1))
+    im[i, :, :, :] = tifffile.imread(im_file)
 
     # load labels
-    labels[i, :, :, :] = tifffile.imread(labels_file)[0, :, :]
+    labels[i, :, :, 0] = tifffile.imread(labels_file)[0, :, :]
 
     # convert to mask of cells vs. no cells
-    mask[i, :, :, :] = labels[i, :, :] != LABEL_BACKGROUND
+    mask[i, :, :, :] = labels[i, :, :, :] != LABEL_BACKGROUND
 
     # plot labels
     if DEBUG:
         plt.clf()
         plt.subplot(221)
-        plt.imshow(im[i, :, :, :].transpose((1, 2, 0)))
+        plt.imshow(im[i, :, :, :])
         plt.subplot(222)
-        plt.imshow(labels[i, :, :, :].reshape((1001, 1001)))
+        plt.imshow(labels[i, :, :, 0])
         plt.subplot(223)
-        plt.imshow(mask[i, :, :, :].reshape((1001, 1001)))
+        plt.imshow(mask[i, :, :, 0])
 
 
 '''CNN
 '''
 
-if K.image_data_format() == 'channels_first':
-    norm_axis = 1
-elif K.image_data_format() == 'channels_last':
-    norm_axis = 3
-
 # declare network model
-input = Input(shape=(3, 1001, 1001), dtype='float32')
+input = Input(shape=(1001, 1001, 3), dtype='float32')
 x = Conv2D(filters=64, kernel_size=(3, 3), strides=1, padding='same')(input)
-x = BatchNormalization(axis=3)(x)
 x = BatchNormalization(axis=norm_axis)(x)
 x = Activation('relu')(x)
-
-
-input = Input(shape=(3, 64, 64), dtype='float32')
-x = Conv2D(filters=32, kernel_size=(3, 3), strides=1, padding='same')(input)
-x = BatchNormalization(axis=3)(x)
-x = Activation('relu')(x)
-
-
-
-
-
-
 
 x = Conv2D(filters=64, kernel_size=(3, 3), strides=1, padding='same')(x)
 x = BatchNormalization(axis=norm_axis)(x)
@@ -121,7 +111,7 @@ x = Conv2D(filters=200, kernel_size=(1, 1), strides=1, padding='same')(x)
 x = BatchNormalization(axis=norm_axis)(x)
 x = Activation('relu')(x)
 
-x = Conv2D(filters=4, kernel_size=(1, 1), strides=1, padding='same')(x)
+x = Conv2D(filters=1, kernel_size=(1, 1), strides=1, padding='same')(x)
 x = Activation('softmax')(x)
 
 main_output = Activation('softmax', name='main_output')(x)
