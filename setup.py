8809ef1f4f3068a07e7f52c8ed43d52adf1957d6
Ramón Casero <rcasero@gmail.com>
2018-07-12 16:39:21 +0100

    add pysto and receptivefield dependencies

--- setup.py
+++ setup.py
@@ -13,11 +13,12 @@ setup(name='cytometer',
       license='GPLv3',
       packages=find_packages(),
       data_files=[('data', ['data/*'])],
-      install_requires=['python>=3.5', 'keras>=2.1.2', 'pillow>=5',
+      install_requires=['python>=3.6', 'keras>=2.2.0', 'pillow>=5',
                         'numpy>=1.14.2', 'pandas>=0.22.0',
                         'matplotlib>=2.1.2', 'scikit-image>=0.13.1',
                         'scikit-learn>=0.19.1', 'scipy>=1.0.0',
                         'openslide-python>=1.1.1', 'tifffile>=0.14.0',
                         'svgpathtools>=1.3.2', 'statsmodels>=0.9.0',
-                        'rpy2>=2.9.3', 'tzlocal>=1.5.1']
+                        'rpy2>=2.9.3', 'tzlocal>=1.5.1',
+                        'receptivefield>=0.19', 'pysto>=1.4.1']
      )
