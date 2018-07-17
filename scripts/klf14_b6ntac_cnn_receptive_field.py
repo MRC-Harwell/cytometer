51508e2fba841e1f47df384103b1842e6ff75534
Ramón Casero <rcasero@gmail.com>
2018-07-17 18:22:39 +0100

    compute receptive field for variations of fcn_conv_bnorm_maxpool_regression

--- scripts/klf14_b6ntac_cnn_receptive_field.py
+++ scripts/klf14_b6ntac_cnn_receptive_field.py
@@ -4,28 +4,66 @@ os.environ['KERAS_BACKEND'] = 'tensorflow'
 import cytometer.models as models
 from receptivefield.keras import KerasReceptiveField
 import matplotlib.pyplot as plt
+import tifffile
+from tensorflow.python.framework.errors_impl import ResourceExhaustedError
+
+training_dir = '/home/rcasero/Dropbox/klf14/klf14_b6ntac_training'
+
+
+nblocks_vec = list(range(3, 6))
+kernel_len_vec = list(range(2, 5))
+dilation_rate_vec = list(range(1, 4))
+rf_size = []
+output = dict('nblocks': [], 'kernel_len': [], 'dilation_rate': [], 'rf_size': [])
+for nblocks in nblocks_vec:
+    for kernel_len in kernel_len_vec:
+        for dilation_rate in dilation_rate_vec:
+
+            # estimate receptive field of the model
+            def model_build_func(input_shape):
+                return models.fcn_conv_bnorm_maxpool_regression(input_shape=input_shape, for_receptive_field=True,
+                                                                nblocks=nblocks, kernel_len=kernel_len,
+                                                                dilation_rate=dilation_rate)
+
+            rf = KerasReceptiveField(model_build_func, init_weights=True)
+
+            try:
+                rf_params = rf.compute(
+                    input_shape=(400, 400, 1),
+                    input_layer='input_image',
+                    output_layer='main_output'
+                )
+
+                rf_size.append(rf_params[2].w)
+            except ResourceExhaustedError:  # if we run out of memory
+                rf_size.append(float('nan'))
+
+
+
+output = {'nblocks': [], 'kernel_len': [], 'dilation_rate': [], 'rf_size': []}
+i=0
+for nblocks in nblocks_vec:
+    for kernel_len in kernel_len_vec:
+        for dilation_rate in dilation_rate_vec:
+            output['nblocks'].append(nblocks)
+            output['kernel_len'].append(kernel_len)
+            output['dilation_rate'].append(dilation_rate)
+            output['rf_size'].append(rf_size[i])
+            i = i + 1
+
+
+import numpy as np
+np.where(np.array(output['rf_size']) == 400)
 
 
 
 
-# estimate receptive field of the model
-def model_build_func(input_shape):
-    return models.fcn_9_conv_8_bnorm_3_maxpool_binary_classifier(input_shape=input_shape,
-                                                                 for_receptive_field=True,
-                                                                 dilation_rate=2)
 
-rf = KerasReceptiveField(model_build_func, init_weights=True)
+# load typical histology window
+im_file = os.path.join(training_dir, 'KLF14-B6NTAC-PAT-37.2g  415-16 C1 - 2016-03-16 11.47.52_row_031860_col_033476.tif')
+im = tifffile.imread(im_file)
 
-rf_params = rf.compute(
-    input_shape=(200, 200, 3),
-    input_layer='input_image',
-    output_layer='main_output'
-)
-print(rf_params)
-
-plt.clf()
-plt.imshow(im_split[i, :, :, :])
-rf.plot_rf_grid(custom_image=im_split[i, :, :, :], figsize=(6, 6))
+# plot receptive field on histology image
+rf.plot_rf_grid(custom_image=im)
 plt.show()
 
-rf.plot_rf_grid(get_default_image(shape, name='doge'))
