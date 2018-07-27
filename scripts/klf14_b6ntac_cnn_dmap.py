cf42290079e819fdd2ab5cc3724e809402de161f
Ramón Casero <r.casero@har.mrc.ac.uk>
2018-07-27 01:54:28 +0100

    edit code to train models.fcn_sherrah2016 with augmented data

--- scripts/klf14_b6ntac_cnn_dmap.py
+++ scripts/klf14_b6ntac_cnn_dmap.py
@@ -6,8 +6,7 @@ home = str(Path.home())
 # with "python scriptname.py"
 import os
 import sys
-sys.path.extend([os.path.join(home, 'Software/cytometer'),
-                 os.path.join(home, 'Software/cytometer')])
+sys.path.extend([os.path.join(home, 'Software/cytometer')])
 
 # other imports
 import glob
@@ -22,7 +21,7 @@ from PIL import Image
 #os.environ["CUDA_VISIBLE_DEVICES"] = ""
 
 # limit number of GPUs
-os.environ['CUDA_VISIBLE_DEVICES'] = '0,3'
+#os.environ['CUDA_VISIBLE_DEVICES'] = '0,3'
 
 # set display for the server
 #os.environ['DISPLAY'] = 'localhost:11.0'
@@ -55,27 +54,34 @@ batch_size = 1
 root_data_dir = os.path.join(home, 'Dropbox/klf14')
 training_dir = os.path.join(home, 'Dropbox/klf14/klf14_b6ntac_training')
 training_non_overlap_data_dir = os.path.join(root_data_dir, 'klf14_b6ntac_training_non_overlap')
-training_augmented_dir = os.path.join(home, 'Dropbox/klf14/klf14_b6ntac_training_augmented')
+training_augmented_dir = os.path.join(home, 'OfflineData/klf14/klf14_b6ntac_training_augmented')
 saved_models_dir = os.path.join(home, 'Dropbox/klf14/saved_models')
 
-# list of segmented files
-seg_file_list = glob.glob(os.path.join(training_non_overlap_data_dir, '*.tif'))
-
-# list of corresponding image patches
-im_file_list = [seg_file.replace(training_non_overlap_data_dir, training_dir) for seg_file in seg_file_list]
-
-# load segmentations and compute distance maps
-dmap, mask, seg = cytometer.data.load_watershed_seg_and_compute_dmap(seg_file_list)
-
-# load corresponding images and convert to float format
-im = cytometer.data.load_im_file_list_to_array(im_file_list)
-im = im.astype('float32', casting='safe')
-im /= 255
+# list of training images
+im_file_list = glob.glob(os.path.join(training_augmented_dir, 'im_*.tif'))
+dmap_file_list = [x.replace('im_', 'dmap_') for x in im_file_list]
+mask_file_list = [x.replace('im_', 'mask_') for x in im_file_list]
 
 # number of training images
-n_im = im.shape[0]
+n_im = len(im_file_list)
 
-# TODO here load data from the augmentation script
+# load images
+im = cytometer.data.load_im_file_list_to_array(im_file_list)
+dmap = cytometer.data.load_im_file_list_to_array(dmap_file_list)
+dmap = dmap.reshape(dmap.shape + (1,))
+mask = cytometer.data.load_im_file_list_to_array(mask_file_list)
+mask = mask.reshape(mask.shape + (1,))
+
+if DEBUG:
+    for i in range(im.shape[0]):
+        plt.clf()
+        plt.subplot(221)
+        plt.imshow(im[i, :, :, :])
+        plt.subplot(222)
+        plt.imshow(dmap[i, :, :, :].reshape(dmap.shape[1:3]))
+        plt.subplot(223)
+        plt.imshow(mask[i, :, :, :].reshape(mask.shape[1:3]))
+        plt.show()
 
 # # remove a 1-pixel thick border so that images are 999x999 and we can split them into 3x3 tiles
 # dmap = dmap[:, 1:-1, 1:-1, :]
@@ -86,25 +92,34 @@ n_im = im.shape[0]
 # remove a 1-pixel so that images are 1000x1000 and we can split them into 2x2 tiles
 dmap = dmap[:, 0:-1, 0:-1, :]
 mask = mask[:, 0:-1, 0:-1, :]
-seg = seg[:, 0:-1, 0:-1, :]
 im = im[:, 0:-1, 0:-1, :]
 
 # split images into smaller blocks to avoid GPU memory overflows in training
-dmap_slices, dmap_blocks, _ = pystoim.block_split(dmap, nblocks=(1, 2, 2, 1))
-im_slices, im_blocks, _ = pystoim.block_split(im, nblocks=(1, 2, 2, 1))
-mask_slices, mask_blocks, _ = pystoim.block_split(mask, nblocks=(1, 2, 2, 1))
+_, dmap, _ = pystoim.block_split(dmap, nblocks=(1, 2, 2, 1))
+_, im, _ = pystoim.block_split(im, nblocks=(1, 2, 2, 1))
+_, mask, _ = pystoim.block_split(mask, nblocks=(1, 2, 2, 1))
 
-dmap_split = np.concatenate(dmap_blocks, axis=0)
-im_split = np.concatenate(im_blocks, axis=0)
-mask_split = np.concatenate(mask_blocks, axis=0)
+dmap = np.concatenate(dmap, axis=0)
+im = np.concatenate(im, axis=0)
+mask = np.concatenate(mask, axis=0)
+
+if DEBUG:
+    for i in range(dmap.shape[0]):
+        plt.clf()
+        plt.subplot(221)
+        plt.imshow(im[i, :, :, :])
+        plt.subplot(222)
+        plt.imshow(dmap[i, :, :, :].reshape(dmap.shape[1:3]))
+        plt.subplot(223)
+        plt.imshow(mask[i, :, :, :].reshape(mask.shape[1:3]))
 
 # find images that have no valid pixels, to remove them from the dataset
-idx_to_keep = np.sum(np.sum(np.sum(mask_split, axis=3), axis=2), axis=1)
+idx_to_keep = np.sum(np.sum(np.sum(mask, axis=3), axis=2), axis=1)
 idx_to_keep = idx_to_keep != 0
 
-dmap_split = dmap_split[idx_to_keep, :, :, :]
-im_split = im_split[idx_to_keep, :, :, :]
-mask_split = mask_split[idx_to_keep, :, :, :]
+dmap = dmap[idx_to_keep, :, :, :]
+im = im[idx_to_keep, :, :, :]
+mask = mask[idx_to_keep, :, :, :]
 
 
 '''CNN
@@ -115,8 +130,7 @@ function
 
 # declare network model
 with tf.device('/cpu:0'):
-    model = models.fcn_sherrah2016_modified(input_shape=im_split.shape[1:])
-#model.load_weights(os.path.join(saved_models_dir, 'foo.h5'))
+    model = models.fcn_sherrah2016(input_shape=im.shape[1:])
 
 # list all CPUs and GPUs
 device_list = K.get_session().list_devices()
@@ -132,20 +146,18 @@ if gpu_number > 1:  # compile and train model: Multiple GPUs
 
     # train model
     tic = datetime.datetime.now()
-    parallel_model.fit(im_split, dmap_split, batch_size=1, epochs=10, validation_split=.1,
-                       sample_weight=mask_split)
+    parallel_model.fit(im, dmap, batch_size=1, epochs=10, validation_split=.1, sample_weight=mask)
     toc = datetime.datetime.now()
     print('Training duration: ' + str(toc - tic))
 
 else:  # compile and train model: One GPU
 
     # compile model
-    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-    # model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
+    model.compile(loss='mse', optimizer='adam', metrics=['mse', 'mae'], sample_weight_mode='element')
 
     # train model
     tic = datetime.datetime.now()
-    model.fit(im_split, mask_split, batch_size=1, epochs=10, validation_split=.1)
+    model.fit(im, mask, batch_size=1, epochs=10, validation_split=.1, sample_weight=mask)
     toc = datetime.datetime.now()
     print('Training duration: ' + str(toc - tic))
 
