ee78f9daa7a0565190ce3ef1a937f933bcb35879
Ramón Casero <rcasero@har.mrc.ac.uk>
2018-07-31 03:17:19 +0100

    halve number of features in last layer of fcn_sherrah2016

--- cytometer/models.py
+++ cytometer/models.py
@@ -57,7 +57,7 @@ def fcn_sherrah2016(input_shape, for_receptive_field=False):
         x = Activation('relu')(x)
         x = MaxPooling2D(pool_size=(17, 17), strides=1, padding='same')(x)
 
-    x = Conv2D(filters=1024, kernel_size=(3, 3), strides=1, dilation_rate=16, padding='same')(x)
+    x = Conv2D(filters=512, kernel_size=(3, 3), strides=1, dilation_rate=16, padding='same')(x)
     if for_receptive_field:
         x = Activation('linear')(x)
     else:
