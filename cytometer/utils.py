a865f77b0b90a5ee12d04888daaef513a17273e8
Ramón Casero <rcasero@gmail.com>
2018-08-21 17:24:17 +0100

    remove old receptive field function, add function to compute principal curvatures from Kurita and Boulanger 1992

--- cytometer/utils.py
+++ cytometer/utils.py
@@ -1,53 +1,99 @@
-import math
-import cytometer.models as models
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
 
-def propagate_receptive_field(layer, receptiveField):
-    n_in = receptiveField[0]
-    j_in = receptiveField[1]
-    r_in = receptiveField[2]
-    start_in = receptiveField[3]
-    if 'conv2d' in layer.name:
-        k = layer.kernel.shape[0].value  # kernel size (first dimension)
-    elif 'max_pooling2d' in layer.name:
-        k = layer.pool_size[0]           # kernel size (first dimension)
-    s = layer.strides[0]                 # stride (first dimension)
-    if layer.padding == 'same':
-        p = math.floor((k - 1) / 2.0)    # unilateral padding (first dimension)
-    elif layer.padding == 'valid':
-        p = 0                            # zero padding
+DEBUG = False
 
-    n_out = math.floor((n_in - k + 2 * p) / s) + 1
-    actualP = (n_out - 1) * s - n_in + k
-    pR = math.ceil(actualP / 2)
-    pL = math.floor(actualP / 2)
+img = dmap_test_pred[0, :, :, 0]
 
-    j_out = j_in * s
-    r_out = r_in + (k - 1) * j_in
-    start_out = start_in + ((k - 1) / 2 - pL) * j_in
-    return n_out, j_out, r_out, start_out
+def principal_curvatures_range_image(img, ksize=3):
+
+    # image gradients
+    hx = cv2.Sobel(img, cv2.CV_32F, 1, 0, ksize=ksize)
+    hy = cv2.Sobel(img, cv2.CV_32F, 0, 1, ksize=ksize)
+
+    # image second derivatives
+    hxx = cv2.Sobel(hx, cv2.CV_32F, 1, 0, ksize=ksize)
+    hxy = cv2.Sobel(hx, cv2.CV_32F, 0, 1, ksize=ksize)
+    hyy = cv2.Sobel(hy, cv2.CV_32F, 0, 1, ksize=ksize)
+
+    if DEBUG:
+        plt.clf()
+        plt.subplot(321)
+        plt.imshow(img)
+        plt.title('h')
+        plt.subplot(323)
+        plt.imshow(hx)
+        plt.title('hx')
+        plt.subplot(324)
+        plt.imshow(hy)
+        plt.title('hy')
+        plt.subplot(322)
+        plt.imshow(hxy)
+        plt.title('hxy')
+        plt.subplot(325)
+        plt.imshow(hxx)
+        plt.title('hxx')
+        plt.subplot(326)
+        plt.imshow(hyy)
+        plt.title('hyy')
+
+    # auxiliary products
+    hx2 = hx * hx
+    hy2 = hy * hy
+
+    # Gaussian curvature
+    K = (hxx * hyy - hxy * hxy) / (1 + hx2 + hy2)**2
+
+    # mean curvature
+    H = (1 + hx2) * hyy + (1 + hy2) * hxx - 2 * hx * hy * hxy
+    H /= 2 * (1 + hx2 + hy2)**1.5
+
+    # principal curvatures
+    H2 = H * H
+    k1 = H + np.sqrt(H2 - K)
+    k2 = H - np.sqrt(H2 - K)
+
+    if DEBUG:
+        plt.clf()
+        plt.subplot(321)
+        plt.imshow(img)
+        plt.title('h')
+        plt.subplot(322)
+        plt.imshow(np.logical_and(k1 > 0, k2 < 0).astype(np.uint8))
+        plt.title('Valley detector')
+        plt.subplot(323)
+        plt.imshow(K, vmin=np.percentile(K, .1), vmax=np.percentile(K, 99.9))
+        plt.title('Gaussian curvature (K)')
+        plt.subplot(324)
+        plt.imshow(H, vmin=np.percentile(H, .1), vmax=np.percentile(H, 99.9))
+        plt.title('Mean curvature (H)')
+        plt.subplot(325)
+        plt.imshow(k1, vmin=np.percentile(k1, .1), vmax=np.percentile(k1, 99.9))
+        plt.title('Principal curvature k1')
+        plt.subplot(326)
+        plt.imshow(k2, vmin=np.percentile(k2, .1), vmax=np.percentile(k2, 99.9))
+        plt.title('Principal curvature k2')
+
+        plt.clf()
+        plt.subplot(321)
+        plt.imshow(img)
+        plt.title('h')
+        plt.subplot(323)
+        plt.imshow(K, vmin=np.percentile(K, .1), vmax=np.percentile(K, 99.9))
+        plt.title('Gaussian curvature (K)')
+        plt.subplot(324)
+        plt.imshow(H, vmin=np.percentile(H, .1), vmax=np.percentile(H, 99.9))
+        plt.title('Mean curvature (H)')
+        plt.subplot(325)
+        plt.imshow(np.sqrt(np.abs(k1)) * np.sign(k1))
+        plt.title('Principal curvature k1')
+        plt.subplot(326)
+        plt.imshow(np.sqrt(np.abs(k2)) * np.sign(k2))
+        plt.title('Principal curvature k2')
 
 
-if __name__ == '__main__':
 
-    # first layer is the data layer (image) with n_0 = image size; j_0 = 1; r_0 = 1; and start_0 = 0.5
-    imsize = 1000
-    j_0 = 1
-    r_0 = 1
-    start_0 = 0.5
-    receptiveField = [imsize, j_0, r_0, start_0]
-    print('input_image' + ': ' + 'n = ' + str(receptiveField[0]) + ', j = ' + str(receptiveField[1])
-          + ', r = ' + str(receptiveField[2]) + ', start = ' + str(receptiveField[3]))
+# if __name__ == '__main__':
 
-    # loop layers
-    for layer in model.layers:
-
-        if 'conv2d' in layer.name or 'max_pooling2d' in layer.name:
-
-            receptiveField = propagate_receptive_field(layer, receptiveField)
-            print(layer.name + ': ' + 'n = ' + str(receptiveField[0]) + ', j = ' + str(receptiveField[1])
-                  + ', r = ' + str(receptiveField[2]) + ', start = ' + str(receptiveField[3]) )
-
-        else:
-
-            print(layer.name + ': N/A')
 
