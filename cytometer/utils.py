5655f9227809a40619083ecbcbb9580296f9933e
Ramón Casero <rcasero@gmail.com>
2018-08-23 17:18:01 +0100

    add principal_curvatures_range_image()

--- cytometer/utils.py
+++ cytometer/utils.py
@@ -1,47 +1,83 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.interpolate import RectBivariateSpline
+from scipy.ndimage.filters import gaussian_filter
 
 DEBUG = False
 
-img = dmap_test_pred[0, :, :, 0]
 
-def principal_curvatures_range_image(img, ksize=3):
+def principal_curvatures_range_image(img, sigma=10):
+    """
+    Compute Gaussian, Mean and principal curvatures of an image with depth values. Examples of such images
+    are topographic maps, range images, depth maps or distance transformations.
 
-    # normalize image intensities to interval [0.0, 1.0]
-    imax = np.max(img)
-    imin = np.min(img)
-    img = (img - imin) / (imax - imin)
+    Any of this images can be projected as a Monge patch, a 2D surface embedded in 3D space, f:U->R^3,
+    f(x,y) = (x, y, img(x, y)).
 
-    # image gradients
-    hx = cv2.Sobel(img, cv2.CV_32F, 1, 0, ksize=ksize)
-    hy = cv2.Sobel(img, cv2.CV_32F, 0, 1, ksize=ksize)
+    We use a cubic B-spline in tensor-product form to represent the Monge patch by fitting it to the image.
+    The reason is that B-splines can be fitted efficiently (scipy.interpolate.RectBivariateSpline), they
+    feature compact support, and as they are polynomial functions, the first and second derivatives can be
+    easily computed.
 
-    # image second derivatives
-    hxx = cv2.Sobel(hx, cv2.CV_32F, 1, 0, ksize=ksize)
-    hxy = cv2.Sobel(hx, cv2.CV_32F, 0, 1, ksize=ksize)
-    hyy = cv2.Sobel(hy, cv2.CV_32F, 0, 1, ksize=ksize)
+    Using these derivatives, there are formulas from elementary Differential Geometry to compute the
+    Gaussian, Normal and principal curvatures of the Monge patch.
+
+    Note that these formulas are very sensitive to noise. Thus, we smooth the input image before fitting
+    the spline.
+
+    :param img: 2D numpy.ndarray with the distance/depth/range values. The 2D image correponds to a Monge
+    patch, or 2D surface embedded in 3D space of the form (x, y, img(x, y))
+    :param sigma: (def sigma=10) Standard deviation in pixels of Gaussian low-pass filtering of the image.
+    For sigma=0, no smoothing is performed.
+    :return: K, H, k1, k2 = Gaussian curvature, Mean curvature, principal curvature 1, principal
+    curvature 2. Each output is an array of the same size as img, with a curvature value per pixel.
+    """
+
+    # Note: here x -> rows, y -> columns
+
+    # low-pass filtering
+    img = gaussian_filter(img, sigma=sigma)
+
+    # quadratic B-spline interpolation of the image
+    sp = RectBivariateSpline(range(img.shape[0]), range(img.shape[1]), img, kx=3, ky=3, s=0)
+
+    # image derivatives and second derivatives
+    hx = sp(range(img.shape[0]), range(img.shape[1]), dx=1, grid=True)
+    hy = sp(range(img.shape[0]), range(img.shape[1]), dy=1, grid=True)
+    hxx = sp(range(img.shape[0]), range(img.shape[1]), dx=2, grid=True)
+    hxy = sp(range(img.shape[0]), range(img.shape[1]), dx=1, dy=1, grid=True)
+    hyy = sp(range(img.shape[0]), range(img.shape[1]), dy=2, grid=True)
 
     if DEBUG:
         plt.clf()
-        plt.subplot(321)
+        plt.subplot(421)
         plt.imshow(img)
-        plt.title('h')
-        plt.subplot(323)
+        plt.title('img')
+        plt.xlabel('y')
+        plt.ylabel('x')
+        plt.subplot(422)
+        img_resampled = sp(range(img.shape[0]), range(img.shape[1]), grid=True)
+        plt.imshow(img_resampled)
+        plt.title('B-spline(img)')
+        plt.subplot(423)
         plt.imshow(hx)
         plt.title('hx')
-        plt.subplot(324)
+        plt.subplot(424)
         plt.imshow(hy)
         plt.title('hy')
-        plt.subplot(322)
-        plt.imshow(hxy)
-        plt.title('hxy')
-        plt.subplot(325)
+        plt.subplot(425)
         plt.imshow(hxx)
         plt.title('hxx')
-        plt.subplot(326)
+        plt.subplot(426)
         plt.imshow(hyy)
         plt.title('hyy')
+        plt.subplot(427)
+        plt.imshow(hxy)
+        plt.title('hxy')
+        plt.subplot(427)
+        plt.imshow(hxy)
+        plt.title('hxy')
 
     # auxiliary products
     hx2 = hx * hx
@@ -63,44 +99,9 @@ def principal_curvatures_range_image(img, ksize=3):
         plt.clf()
         plt.subplot(321)
         plt.imshow(img)
-        plt.title('h')
-        plt.subplot(322)
-        plt.imshow(np.logical_and(k1 > 0, k2 < 0).astype(np.uint8))
-        plt.title('Valley detector')
-        plt.subplot(323)
-        plt.imshow(K, vmin=np.percentile(K, .1), vmax=np.percentile(K, 99.9))
-        plt.title('Gaussian curvature (K)')
-        plt.subplot(324)
-        plt.imshow(H, vmin=np.percentile(H, .1), vmax=np.percentile(H, 99.9))
-        plt.title('Mean curvature (H)')
-        plt.subplot(325)
-        plt.imshow(k1, vmin=np.percentile(k1, .1), vmax=np.percentile(k1, 99.9))
-        plt.title('Principal curvature k1')
-        plt.subplot(326)
-        plt.imshow(k2, vmin=np.percentile(k2, .1), vmax=np.percentile(k2, 99.9))
-        plt.title('Principal curvature k2')
-
-        plt.clf()
-        plt.subplot(321)
-        plt.imshow(img)
-        plt.title('h')
-        plt.subplot(323)
-        plt.imshow(K, vmin=np.percentile(K, .1), vmax=np.percentile(K, 99.9))
-        plt.title('Gaussian curvature (K)')
-        plt.subplot(324)
-        plt.imshow(H, vmin=np.percentile(H, .1), vmax=np.percentile(H, 99.9))
-        plt.title('Mean curvature (H)')
-        plt.subplot(325)
-        plt.imshow(np.sqrt(np.abs(k1)) * np.sign(k1))
-        plt.title('Principal curvature k1')
-        plt.subplot(326)
-        plt.imshow(np.sqrt(np.abs(k2)) * np.sign(k2))
-        plt.title('Principal curvature k2')
-
-        plt.clf()
-        plt.subplot(321)
-        plt.imshow(img)
-        plt.title('h')
+        plt.title('img')
+        plt.xlabel('y')
+        plt.ylabel('x')
         plt.subplot(323)
         plt.imshow(K)
         plt.title('Gaussian curvature (K)')
@@ -108,17 +109,13 @@ def principal_curvatures_range_image(img, ksize=3):
         plt.imshow(H)
         plt.title('Mean curvature (H)')
         plt.subplot(325)
-        plt.imshow(k1)
+        plt.imshow(k1.astype(np.float32))
         plt.title('Principal curvature k1')
         plt.subplot(326)
-        plt.imshow(k2)
+        plt.imshow(k2.astype(np.float32))
         plt.title('Principal curvature k2')
 
-        np.min(K)
-        np.max(K)
-
+    return K, H, k1, k2
 
 
 # if __name__ == '__main__':
-
-
