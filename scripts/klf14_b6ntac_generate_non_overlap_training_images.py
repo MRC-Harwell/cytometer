b39e88cf7a65edf6cd75bcbe4c6d5b86ea6b759f
Ramón Casero <r.casero@har.mrc.ac.uk>
2018-09-08 20:43:25 +0100

    compute borders between labels explicitly, because watershed alone leaves interrupted borders due to intermittent overlaps

--- scripts/klf14_b6ntac_generate_non_overlap_training_images.py
+++ scripts/klf14_b6ntac_generate_non_overlap_training_images.py
@@ -13,9 +13,10 @@ import PIL.ImageDraw
 from PIL.TiffTags import TAGS
 import tifffile
 from cv2 import watershed
+import mahotas
 
 
-DEBUG = True
+DEBUG = False
 
 root_data_dir = os.path.join(home, 'Dropbox/klf14')
 training_data_dir = os.path.join(root_data_dir, 'klf14_b6ntac_training')
@@ -130,6 +131,13 @@ for n, file_svg in enumerate(file_list):
 
     # set background pixels back to zero
     labels[labels == background_label] = 0
+
+    # compute borders between labels, because in some cases, adjacent cells have intermittent overlaps
+    # that produce interrupted 0 boundaries
+    borders = mahotas.labeled.borders(labels, mode='ignore')
+
+    #add borders to the labels
+    labels[borders] = 0
 
     if DEBUG:
         plt.subplot(224)
