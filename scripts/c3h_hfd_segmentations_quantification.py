38beec6ee228f7a6c9f1a1171dfa5f443fc99d62
Gianluca Cientanni <g.cientanni@har.mrc.ac.uk>
2018-09-24 11:27:54 +0000

    now generates dmap test predictions and computes areas from them

--- scripts/c3h_hfd_segmentations_quantification.py
+++ scripts/c3h_hfd_segmentations_quantification.py
@@ -1,31 +1,56 @@
-import csv
-import glob
 import pandas as pd
 from pathlib import Path
 home = str(Path.home())
 import os
 import sys
 sys.path.extend([os.path.join(home, 'Software/cytometer')])
-import numpy as np
 from PIL import Image
-import matplotlib.pyplot as plt
 from scipy import ndimage as ndi
 from skimage.morphology import watershed
 from skimage.feature import peak_local_max
-import openslide
 import PIL
+# cross-platform home directory
+from pathlib import Path
+home = str(Path.home())
+
+# PyCharm automatically adds cytometer to the python path, but this doesn't happen if the script is run
+# with "python scriptname.py"
+import glob
+import numpy as np
+import openslide
+import csv
+import pickle
+
+# limit number of GPUs
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+
+# limit GPU memory used
+os.environ['KERAS_BACKEND'] = 'tensorflow'
+import tensorflow as tf
+from keras.backend.tensorflow_backend import set_session
+config = tf.ConfigProto()
+config.gpu_options.per_process_gpu_memory_fraction = 0.8
+set_session(tf.Session(config=config))
+
+# Note: you need to use my branch of keras with the new functionality, that allows element-wise weights of the loss
+# function
+from keras.models import Model
+from keras.layers import Input, Conv2D, MaxPooling2D, AvgPool2D, Activation
+import keras.backend as K
+import cytometer.data
+from cytometer.utils import principal_curvatures_range_image
+import matplotlib.pyplot as plt
+
+K.set_image_data_format('channels_last')
 
 DEBUG = False
 
 
-''' Defining watershed algorithm
+''' Defining watershed algorithm and CNN model
 ========================================================================================================================
 '''
 
-def watershed_segmentation(file, x_res, y_res):
-
-    image = Image.open(file)
-    pixels = np.array(image)
+def watershed_segmentation(pixels, x_res, y_res):
 
     local_maxi = peak_local_max(image=pixels, min_distance=20, indices=False)
 
@@ -38,6 +63,54 @@ def watershed_segmentation(file, x_res, y_res):
     area_list = unique_cells[1] * pixel_res
 
     return np.array(area_list)
+
+def fcn_sherrah2016_regression(input_shape, for_receptive_field=False):
+
+    input = Input(shape=input_shape, dtype='float32', name='input_image')
+
+    x = Conv2D(filters=32, kernel_size=(5, 5), strides=1, dilation_rate=1, padding='same')(input)
+    if for_receptive_field:
+        x = Activation('linear')(x)
+        x = AvgPool2D(pool_size=(3, 3), strides=1, padding='same')(x)
+    else:
+        x = Activation('relu')(x)
+        x = MaxPooling2D(pool_size=(3, 3), strides=1, padding='same')(x)
+
+    x = Conv2D(filters=96, kernel_size=(5, 5), strides=1, dilation_rate=2, padding='same')(x)
+    if for_receptive_field:
+        x = Activation('linear')(x)
+        x = AvgPool2D(pool_size=(5, 5), strides=1, padding='same')(x)
+    else:
+        x = Activation('relu')(x)
+        x = MaxPooling2D(pool_size=(5, 5), strides=1, padding='same')(x)
+
+    x = Conv2D(filters=128, kernel_size=(3, 3), strides=1, dilation_rate=4, padding='same')(x)
+    if for_receptive_field:
+        x = Activation('linear')(x)
+        x = AvgPool2D(pool_size=(9, 9), strides=1, padding='same')(x)
+    else:
+        x = Activation('relu')(x)
+        x = MaxPooling2D(pool_size=(9, 9), strides=1, padding='same')(x)
+
+    x = Conv2D(filters=196, kernel_size=(3, 3), strides=1, dilation_rate=8, padding='same')(x)
+    if for_receptive_field:
+        x = Activation('linear')(x)
+        x = AvgPool2D(pool_size=(17, 17), strides=1, padding='same')(x)
+    else:
+        x = Activation('relu')(x)
+        x = MaxPooling2D(pool_size=(17, 17), strides=1, padding='same')(x)
+
+    x = Conv2D(filters=512, kernel_size=(3, 3), strides=1, dilation_rate=16, padding='same')(x)
+    if for_receptive_field:
+        x = Activation('linear')(x)
+    else:
+        x = Activation('relu')(x)
+
+    # dimensionality reduction layer
+    main_output = Conv2D(filters=1, kernel_size=(1, 1), strides=1, dilation_rate=1, padding='same',
+                         name='main_output')(x)
+
+    return Model(inputs=input, outputs=main_output)
 
 ''' Declaring data directories and initialising pixel variables
 ========================================================================================================================
@@ -136,55 +209,147 @@ for file in file_list:
     for i, a in enumerate(areas):
         if a == 0.0:
             print('Warning! Area == 0.0: index ' + str(i) + ':' + image_id)
-        df_no = df_no.append({'area': a, 'mouse_id': mouse_id, 'sex': mouse_sex, 'diet': mouse_diet, 'image_id': image_id},
+        df_no = df_no.append({'area': a, 'mouse_id': mouse_id, 'sex': mouse_sex,
+                              'diet': mouse_diet, 'image_id': image_id},
                              ignore_index=True)
 
 
-''' dmap estimates produced by network
+''' Run test images through trained network, extract areas, create dataframe with area for each data group
 ========================================================================================================================
 '''
 
-# file_list = glob.glob(os.path.join(training_data_dir, '*.tif'))
+saved_model_basename = 'c3h_hfd_exp_0000_cnn_dmap'
 
-# create empty dataframe to host the data
-df = pd.DataFrame(data={'area': [], 'mouse_id': [], 'sex': [], 'image_id': []})
-print(df)
+model_name = saved_model_basename + '*.h5'
 
-# read all dmap files, and categorise them into hfd/lfd and f/m
-for file in file_list:
+# load model weights for each fold
+model_files = glob.glob(os.path.join(saved_models_dir, model_name))
+n_folds = len(model_files)
 
-    # image ID
-    image_id = os.path.basename(file)
-    image_id = os.path.splitext(image_id)[-2]
-    image_id = image_id.replace('-', ' ', 1)
-    image_id = image_id.replace(' ', '', 2)
+# load k-fold sets that were used to train the models
+saved_model_kfold_filename = os.path.join(saved_models_dir, saved_model_basename + '_info.pickle')
+with open(saved_model_kfold_filename, 'rb') as f:
+    aux = pickle.load(f)
+im_file_list = aux['file_list']
+idx_test_all = aux['idx_test_all']
 
+# correct home directory if we are in a different system than what was used to train the models
+im_file_list = cytometer.data.change_home_directory(im_file_list,
+                                                    '/users/rittscher/rcasero/Dropbox/c3h/c3h_hfd_training_augmented',
+                                                    training_augmented_dir,
+                                                    check_isfile=True)
 
-    # get mouse ID from the file name
-    mouse_id = None
-    for x in c3h_ids:
-        if x in image_id:
-            mouse_id = x
-            break
-    if mouse_id is None:
-        raise ValueError('Filename does not seem to correspond to any known mouse ID: ' + file)
+# Run test images through the trained network
+# list of model files to inspect
+model_files = glob.glob(os.path.join(saved_models_dir, model_name))
 
-    # index of mouse ID
-    idx = c3h_ids.index(mouse_id)
+for fold_i, model_file in enumerate(model_files):
 
-    # metainformation for this mouse
-    mouse_sex = c3h_info[idx]['sex']
-    mouse_diet = c3h_info[idx]['diet']
+    # split the data into training and testing datasets
+    im_test_file_list, _ = cytometer.data.split_list(im_file_list, idx_test_all[fold_i])
 
-    # watershed
-    areas = watershed_segmentation(file, x_res=1, y_res=1)
+    # load im, seg and mask datasets
+    test_datasets, _, _ = cytometer.data.load_datasets(im_test_file_list, prefix_from='im',
+                                                       prefix_to=['im', 'mask', 'dmap'], nblocks=2)
+    im_test = test_datasets['im']
+    mask_test = test_datasets['mask']
+    dmap_test = test_datasets['dmap']
+    del test_datasets
 
-    # add to dataframe: area, image id, mouse id, sex, diet
-    for i, a in enumerate(areas):
-        if a == 0.0:
-            print('Warning! Area == 0.0: index ' + str(i) + ':' + image_id)
-        df = df.append({'area': a, 'mouse_id': mouse_id, 'sex': mouse_sex, 'diet': mouse_diet, 'image_id': image_id},
-                       ignore_index=True)
+    # load model
+    model = fcn_sherrah2016_regression(input_shape=im_test.shape[1:])
+    model.load_weights(model_file)
+
+    # visualise results
+    i = 0
+    # run image through network
+    dmap_test_pred = model.predict(im_test[i, :, :, :].reshape((1,) + im_test.shape[1:]))
+
+    # compute mean curvature from dmap
+    _, mean_curvature, _, _ = principal_curvatures_range_image(dmap_test_pred[0, :, :, 0], sigma=10)
+
+    # plot results
+    plt.clf()
+    plt.subplot(221)
+    plt.imshow(im_test[i, :, :, :])
+    plt.title('histology, i = ' + str(i))
+    plt.subplot(222)
+    plt.imshow(dmap_test[i, :, :, 0])
+    plt.title('ground truth dmap')
+    plt.subplot(223)
+    plt.imshow(dmap_test_pred[0, :, :, 0])
+    plt.title('predicted dmap')
+    plt.subplot(224)
+    plt.imshow(mean_curvature)
+    plt.title('mean curvature of dmap')
+
+    # visualise results
+    i = 18
+    # run image through network
+    dmap_test_pred = model.predict(im_test[i, :, :, :].reshape((1,) + im_test.shape[1:]))
+
+    # compute mean curvature from dmap
+    _, mean_curvature, _, _ = principal_curvatures_range_image(dmap_test_pred[0, :, :, 0], sigma=10)
+
+    # plot results
+    plt.clf()
+    plt.subplot(221)
+    plt.imshow(im_test[i, :, :, :])
+    plt.title('histology, i = ' + str(i))
+    plt.subplot(222)
+    plt.imshow(dmap_test[i, :, :, 0])
+    plt.title('ground truth dmap')
+    plt.subplot(223)
+    plt.imshow(dmap_test_pred[0, :, :, 0])
+    plt.title('predicted dmap')
+    plt.subplot(224)
+    plt.imshow(mean_curvature)
+    plt.title('mean curvature of dmap')
+
+    # From dmaps calculate area using watershed method
+    areas = watershed_segmentation(dmap_test_pred, x_res=1, y_res=1)
+
+# # file_list = glob.glob(os.path.join(training_data_dir, '*.tif'))
+#
+# # create empty dataframe to host the data
+# df = pd.DataFrame(data={'area': [], 'mouse_id': [], 'sex': [], 'image_id': []})
+# print(df)
+#
+# # read all dmap files, and categorise them into hfd/lfd and f/m
+# for file in file_list:
+#
+#     # image ID
+#     image_id = os.path.basename(file)
+#     image_id = os.path.splitext(image_id)[-2]
+#     image_id = image_id.replace('-', ' ', 1)
+#     image_id = image_id.replace(' ', '', 2)
+#
+#
+#     # get mouse ID from the file name
+#     mouse_id = None
+#     for x in c3h_ids:
+#         if x in image_id:
+#             mouse_id = x
+#             break
+#     if mouse_id is None:
+#         raise ValueError('Filename does not seem to correspond to any known mouse ID: ' + file)
+#
+#     # index of mouse ID
+#     idx = c3h_ids.index(mouse_id)
+#
+#     # metainformation for this mouse
+#     mouse_sex = c3h_info[idx]['sex']
+#     mouse_diet = c3h_info[idx]['diet']
+#
+#     # watershed
+#     areas = watershed_segmentation(file, x_res=1, y_res=1)
+#
+#     # add to dataframe: area, image id, mouse id, sex, diet
+#     for i, a in enumerate(areas):
+#         if a == 0.0:
+#             print('Warning! Area == 0.0: index ' + str(i) + ':' + image_id)
+#         df = df.append({'area': a, 'mouse_id': mouse_id, 'sex': mouse_sex, 'diet': mouse_diet, 'image_id': image_id},
+#                        ignore_index=True)
 
 
 ''' split full dataset into smaller datasets for different groups 
