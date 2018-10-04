5ba75d3a5f9f2823dd344309c73ca37f51906ca4
Ramón Casero <rcasero@gmail.com>
2018-10-04 16:36:32 +0100

    finish script, adding regression CNN for Dice coefficients

--- scripts/klf14_b6ntac_exp_0013_cnn_dice_coeff.py
+++ scripts/klf14_b6ntac_exp_0013_cnn_dice_coeff.py
@@ -1,6 +1,6 @@
-'''
+"""
 Contour segmentation
-'''
+"""
 
 # cross-platform home directory
 from pathlib import Path
@@ -26,7 +26,7 @@ import matplotlib.pyplot as plt
 #os.environ["CUDA_VISIBLE_DEVICES"] = ""
 
 # limit number of GPUs
-os.environ['CUDA_VISIBLE_DEVICES'] = '0,1,2,3'
+os.environ['CUDA_VISIBLE_DEVICES'] = '1'
 
 os.environ['KERAS_BACKEND'] = 'tensorflow'
 import keras
@@ -55,7 +55,7 @@ K.set_image_data_format('channels_last')
 DEBUG = False
 
 # number of blocks to split each image into so that training fits into GPU memory
-nblocks = 3
+nblocks = 2
 
 # number of folds for k-fold cross validation
 n_folds = 11
@@ -80,11 +80,11 @@ if experiment_id == '<input>':
 else:
     experiment_id = os.path.splitext(os.path.basename(experiment_id))[0]
 
-'''CNN Model
+'''CNN classifier to estimate Dice coefficient
 '''
 
 
-def fcn_sherrah2016_classifier(input_shape, for_receptive_field=False):
+def fcn_sherrah2016_regression(input_shape, for_receptive_field=False):
 
     input = Input(shape=input_shape, dtype='float32', name='input_image')
 
@@ -126,57 +126,65 @@ def fcn_sherrah2016_classifier(input_shape, for_receptive_field=False):
     else:
         x = Activation('relu')(x)
 
-    # dimensionality reduction
-    x = Conv2D(filters=2, kernel_size=(1, 1), strides=1, dilation_rate=1, padding='same')(x)
+    # regression output
+    x = Conv2D(filters=1, kernel_size=(1, 1), strides=1, dilation_rate=1, padding='same')(x)
+    regression_output = Activation('hard_sigmoid', name='regression_output')(x)
 
-    # classification output
-    classification_output = Activation('hard_sigmoid', name='classification_output')(x)
-
-    return Model(inputs=input, outputs=[classification_output])
+    return Model(inputs=input, outputs=[regression_output])
 
 
-'''Prepare folds
+'''Models that were used to generate the segmentations
 '''
 
-# list of original training images, pre-augmentation
-im_orig_file_list = glob.glob(os.path.join(training_augmented_dir, 'im_*_nan_*.tif'))
+saved_contour_model_basename = 'klf14_b6ntac_exp_0006_cnn_contour'  # contour
+saved_dmap_model_basename = 'klf14_b6ntac_exp_0007_cnn_dmap'  # dmap
+
+contour_model_name = saved_contour_model_basename + '*.h5'
+dmap_model_name = saved_dmap_model_basename + '*.h5'
+
+'''Filenames of whole dataset, and indices of train vs. test subsets
+'''
+
+# load list of images, and indices for training vs. testing indices
+contour_model_kfold_filename = os.path.join(saved_models_dir, saved_contour_model_basename + '_info.pickle')
+with open(contour_model_kfold_filename, 'rb') as f:
+    aux = pickle.load(f)
+im_orig_file_list = aux['file_list']
+idx_orig_test_all = aux['idx_test_all']
 
 # number of original training images
-n_orig_im = len(im_orig_file_list)
+n_im = len(im_orig_file_list)
 
-# create k-fold sets to split the data into training vs. testing
-kfold_seed = 0
-random.seed(kfold_seed)
-idx = random.sample(range(n_orig_im), n_orig_im)
-idx_test_all = np.array_split(idx, n_folds)
+# correct home directory if we are in a different system than what was used to train the models
+im_orig_file_list = cytometer.data.change_home_directory(im_orig_file_list, '/users/rittscher/rcasero', home,
+                                                         check_isfile=True)
 
-# save the k-fold description for future reference
-saved_model_datainfo_filename = os.path.join(saved_models_dir, experiment_id + '_info.pickle')
-with open(saved_model_datainfo_filename, 'wb') as f:
-    x = {'file_list': im_orig_file_list, 'idx_test_all': idx_test_all, 'kfold_seed': kfold_seed}
-    pickle.dump(x, f, pickle.HIGHEST_PROTOCOL)
+'''Loop folds (in this script, we actually only work with fold 0)
+'''
 
 # loop each fold: we split the data into train vs test, train a model, and compute errors with the
 # test data. In each fold, the test data is different
 # for i_fold, idx_test in enumerate(idx_test_all):
-for i_fold, idx_test in enumerate([idx_test_all[0]]):
+for i_fold, idx_test in enumerate([idx_orig_test_all[0]]):
 
     '''Load data
     '''
 
     # split the data into training and testing datasets
-    im_test_file_list, im_train_file_list = cytometer.data.split_list(im_orig_file_list, idx_test)
+    im_orig_test_file_list, im_orig_train_file_list = cytometer.data.split_list(im_orig_file_list, idx_test)
 
     # add the augmented image files
-    im_train_file_list = cytometer.data.augment_file_list(im_train_file_list, '_nan_', '_*_')
-    im_test_file_list = cytometer.data.augment_file_list(im_test_file_list, '_nan_', '_*_')
+    im_train_file_list = cytometer.data.augment_file_list(im_orig_train_file_list, '_nan_', '_*_')
+    im_test_file_list = cytometer.data.augment_file_list(im_orig_test_file_list, '_nan_', '_*_')
 
     # load the train and test data: im, seg, dmap and mask data
     train_dataset, train_file_list, train_shuffle_idx = \
-        cytometer.data.load_datasets(im_train_file_list, prefix_from='im', prefix_to=['im', 'seg', 'mask'],
+        cytometer.data.load_datasets(im_train_file_list, prefix_from='im',
+                                     prefix_to=['im', 'mask', 'dice_kfold_' + str(i_fold).zfill(2)],
                                      nblocks=nblocks, shuffle_seed=i_fold)
     test_dataset, test_file_list, test_shuffle_idx = \
-        cytometer.data.load_datasets(im_test_file_list, prefix_from='im', prefix_to=['im', 'seg', 'mask'],
+        cytometer.data.load_datasets(im_test_file_list, prefix_from='im',
+                                     prefix_to=['im', 'mask', 'dice_kfold_' + str(i_fold).zfill(2)],
                                      nblocks=nblocks, shuffle_seed=i_fold)
 
     # remove training data where the mask has very few valid pixels
@@ -218,7 +226,7 @@ for i_fold, idx_test in enumerate([idx_test_all[0]]):
 
     # instantiate model
     with tf.device('/cpu:0'):
-        model = fcn_sherrah2016_classifier(input_shape=train_dataset['im'].shape[1:])
+        model = fcn_sherrah2016_regression(input_shape=train_dataset['im'].shape[1:])
 
     saved_model_filename = os.path.join(saved_models_dir, experiment_id + '_model_fold_' + str(i_fold) + '.h5')
 
@@ -229,19 +237,18 @@ for i_fold, idx_test in enumerate([idx_test_all[0]]):
                                                                            verbose=1, save_best_only=True)
         # compile model
         parallel_model = multi_gpu_model(model, gpus=gpu_number)
-        parallel_model.compile(loss={'classification_output': 'binary_crossentropy'},
+        parallel_model.compile(loss={'regression_output': 'mse'},
                                optimizer='Adadelta',
-                               metrics={'classification_output': 'accuracy'},
+                               metrics={'regression_output': ['mse', 'mae']},
                                sample_weight_mode='element')
 
         # train model
         tic = datetime.datetime.now()
         parallel_model.fit(train_dataset['im'],
-                           {'classification_output': train_dataset['seg']},
-                           sample_weight={'classification_output': train_dataset['mask'][..., 0]},
+                           {'regression_output': train_dataset['dice_kfold_' + str(i_fold).zfill(2)]},
                            validation_data=(test_dataset['im'],
-                                            {'classification_output': test_dataset['seg']},
-                                            {'classification_output': test_dataset['mask'][..., 0]}),
+                                            {'regression_output': test_dataset['dice_kfold_' + str(i_fold).zfill(2)]},
+                                            {'regression_output': test_dataset['mask'][..., 0]}),
                            batch_size=10, epochs=epochs, initial_epoch=0,
                            callbacks=[checkpointer])
         toc = datetime.datetime.now()
@@ -254,19 +261,19 @@ for i_fold, idx_test in enumerate([idx_test_all[0]]):
                                                        verbose=1, save_best_only=True)
 
         # compile model
-        model.compile(loss={'classification_output': 'binary_crossentropy'},
+        model.compile(loss={'regression_output': 'mse'},
                       optimizer='Adadelta',
-                      metrics={'classification_output': 'accuracy'},
+                      metrics={'regression_output': ['mse', 'mae']},
                       sample_weight_mode='element')
 
         # train model
         tic = datetime.datetime.now()
         model.fit(train_dataset['im'],
-                  {'classification_output': train_dataset['seg']},
-                  sample_weight={'classification_output': train_dataset['mask'][..., 0]},
+                  {'regression_output': train_dataset['dice_kfold_' + str(i_fold).zfill(2)]},
+                  sample_weight={'regression_output': train_dataset['mask'][..., 0]},
                   validation_data=(test_dataset['im'],
-                                   {'classification_output': test_dataset['seg']},
-                                   {'classification_output': test_dataset['mask'][..., 0]}),
+                                   {'regression_output': test_dataset['dice_kfold_' + str(i_fold).zfill(2)]},
+                                   {'regression_output': test_dataset['mask'][..., 0]}),
                   batch_size=10, epochs=epochs, initial_epoch=0,
                   callbacks=[checkpointer])
         toc = datetime.datetime.now()
