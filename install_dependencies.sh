6448fc257d69ced1572d0cf7d137f4774ba37eb9
Ramón Casero <r.casero@har.mrc.ac.uk>
2018-10-08 20:27:49 +0100

    add networkx dependency

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -49,7 +49,7 @@ conda install -y matplotlib pillow
 conda install -y scikit-image scikit-learn h5py 
 conda install -y nose pytest
 pip install opencv-python pysto openslide-python
-pip install tifffile mahotas
+pip install tifffile mahotas networkx
 conda install -y pandas
 
 ########################################################################
