8c1dc7d4b6f1ff8580191fa818bfce245e519a02
Ramón Casero <rcasero@gmail.com>
2018-10-10 17:36:16 +0100

    Full rewrite. Apply transforms using skimage.transform rather than Keras, so that we can use nearest neighbour interpolation

--- scripts/klf14_b6ntac_generate_augmented_training_images.py
+++ scripts/klf14_b6ntac_generate_augmented_training_images.py
@@ -20,23 +20,22 @@ import sys
 sys.path.extend([os.path.join(home, 'Software/cytometer')])
 
 # other imports
+import pickle
 import glob
 import shutil
 import numpy as np
-import pysto.imgproc as pystoim
 import matplotlib.pyplot as plt
 from PIL import Image
 import cv2
+import random
+from skimage.transform import warp, SimilarityTransform, AffineTransform
 
 # use CPU for testing on laptop
 #os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"   # see issue #152
 #os.environ["CUDA_VISIBLE_DEVICES"] = ""
 
 # limit number of GPUs
-os.environ['CUDA_VISIBLE_DEVICES'] = '1,2'
-
-# set display for the server
-#os.environ['DISPLAY'] = 'localhost:11.0'
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 
 os.environ['KERAS_BACKEND'] = 'tensorflow'
 import keras
@@ -46,7 +45,7 @@ import cytometer.data
 import tensorflow as tf
 from keras.backend.tensorflow_backend import set_session
 config = tf.ConfigProto()
-config.gpu_options.per_process_gpu_memory_fraction = 1.0
+config.gpu_options.per_process_gpu_memory_fraction = 0.95
 set_session(tf.Session(config=config))
 
 
@@ -93,7 +92,7 @@ seg = seg * mask
 for i in range(n_im):
     seg[i, :, :, 0] = cv2.dilate(seg[i, :, :, 0], kernel=np.ones(shape=(3, 3)))
 
-'''Data augmentation
+'''Copy original data
 '''
 
 # copy or save the original data to the augmented directory, so that later we can use a simple flow_from_directory()
@@ -130,45 +129,41 @@ for i, base_file in enumerate(im_file_list):
     shutil.copy2(base_file, im_file)
 
     # save distance transforms (note: we have to save as float mode)
-    im_out = dmap[i, :, :, 0]
-    im_out = Image.fromarray(im_out, mode='F')
+    im_out = Image.fromarray(dmap[i, :, :, 0].astype(np.float32), mode='F')
     im_out.save(dmap_file)
 
     # save mask (note: we can save as black and white 1 byte)
-    im_out = mask[i, :, :, 0]
-    im_out = im_out.astype(np.uint8)
-    im_out = Image.fromarray(im_out, mode='L')
+    im_out = Image.fromarray(mask[i, :, :, 0].astype(np.uint8), mode='L')
     im_out.save(mask_file)
 
-    # set all contour pixels
-    im_out = seg[i, :, :, 0]
-    im_out = Image.fromarray(im_out.astype(np.uint32), mode='I')
+    # set all contour pixels (note: we can save as black and white 1 byte)
+    im_out = Image.fromarray(seg[i, :, :, 0].astype(np.uint8), mode='L')
     im_out.save(seg_file)
 
-    # save labels
-    im_out = lab[i, :, :, 0]
-    im_out = Image.fromarray(im_out, mode='L')
+    # save labels (0-255 levels with 1 byte)
+    if np.max(lab[i, :, :, 0]) > 255:
+        raise ValueError('There is a label > 255, so it cannot be saved with a uint8 type')
+    im_out = Image.fromarray(lab[i, :, :, 0].astype(np.uint8), mode='L')
     im_out.save(lab_file)
 
+
+'''Data augmentation
+'''
 
 # data augmentation factor (e.g. "10" means that we generate 9 augmented images + the original input image)
 augment_factor = 10
 
-# we create two instances with the same arguments
-data_gen_args = dict(
+# generator of random transformations
+data_gen_args_float32 = dict(
     rotation_range=90,     # randomly rotate images up to 90 degrees
     fill_mode="constant",  # fill points outside boundaries with zeros
     cval=0,                #
     zoom_range=.1,         # [1-zoom_range, 1+zoom_range]
     horizontal_flip=True,  # randomly flip images
-    vertical_flip=True     # randomly flip images
+    vertical_flip=True,    # randomly flip images
+    dtype=np.float32       # explicit type out
 )
-
-dmap_datagen = keras.preprocessing.image.ImageDataGenerator(**data_gen_args)
-im_datagen = keras.preprocessing.image.ImageDataGenerator(**data_gen_args)
-mask_datagen = keras.preprocessing.image.ImageDataGenerator(**data_gen_args)
-seg_datagen = keras.preprocessing.image.ImageDataGenerator(**data_gen_args)
-lab_datagen = keras.preprocessing.image.ImageDataGenerator(**data_gen_args)
+datagen = keras.preprocessing.image.ImageDataGenerator(**data_gen_args_float32)
 
 # augment data, using the same seed so that all corresponding images, dmaps and masks undergo
 # the same transformations
@@ -177,69 +172,101 @@ for seed in range(augment_factor - 1):
 
     print('* Augmentation round: ' + str(seed + 1) + '/' + str(augment_factor - 1))
 
-    dmap_augmented = dmap_datagen.flow(dmap, seed=seed, shuffle=False, batch_size=n_im).next()
-    im_augmented = im_datagen.flow(im, seed=seed, shuffle=False, batch_size=n_im).next()
-    mask_augmented = np.round(mask_datagen.flow(mask, seed=seed, shuffle=False, batch_size=n_im).next())
-    seg_augmented = np.round(seg_datagen.flow(seg, seed=seed, shuffle=False, batch_size=n_im).next())
-    seg_augmented = seg_augmented.astype(np.uint8)
-    lab_augmented = np.round(lab_datagen.flow(lab, seed=seed, shuffle=False, batch_size=n_im).next())
-    lab_augmented = lab_augmented.astype(np.uint8)
+    # generate random transformations. We give this its own loop, so that it's easy to replicate, if necessary
+    transform = []
+    random.seed(seed)
+    for i in range(n_im):
+        # generate random transformation
+        transform.append(datagen.get_random_transform(img_shape=im.shape[1:3],
+                                                      seed=random.randint(0, 2 ** 32)))
 
+    # loop applying transformations to data
     for i in range(n_im):
 
         print('  ** Image: ' + str(i) + '/' + str(n_im - 1))
 
+        # convert transform from keras to skimage format
+        transform_skimage_center = SimilarityTransform(translation=(im.shape[2] / 2, im.shape[1] / 2))
+        transform_skimage_center_inv = SimilarityTransform(translation=(-im.shape[2] / 2, -im.shape[1] / 2))
+        transform_skimage_affine = AffineTransform(matrix=None, scale=(transform[i]['zx'], transform[i]['zy']),
+                                                   rotation=transform[i]['theta'] / 180.0 * np.pi, shear=None,
+                                                   translation=None)
+        transform_skimage = transform_skimage_center_inv + (transform_skimage_affine + transform_skimage_center)
+
+        # apply affine transformation
+        im_augmented = warp(im[i, :, :, :], transform_skimage.inverse, order=1, preserve_range=True)
+        dmap_augmented = warp(dmap[i, :, :, 0], transform_skimage.inverse, order=1, preserve_range=True)
+        mask_augmented = warp(mask[i, :, :, 0], transform_skimage.inverse, order=0, preserve_range=True)
+        seg_augmented = warp(seg[i, :, :, 0], transform_skimage.inverse, order=0, preserve_range=True)
+        lab_augmented = warp(lab[i, :, :, 0], transform_skimage.inverse, order=0, preserve_range=True)
+
+        # apply flips
+        if transform[i]['flip_horizontal'] == 1:
+            im_augmented = im_augmented[:, ::-1, :]
+            dmap_augmented = dmap_augmented[:, ::-1]
+            mask_augmented = mask_augmented[:, ::-1]
+            seg_augmented = seg_augmented[:, ::-1]
+            lab_augmented = lab_augmented[:, ::-1]
+        if transform[i]['flip_vertical'] == 1:
+            im_augmented = im_augmented[::-1, :, :]
+            dmap_augmented = dmap_augmented[::-1, :]
+            mask_augmented = mask_augmented[::-1, :]
+            seg_augmented = seg_augmented[::-1, :]
+            lab_augmented = lab_augmented[::-1, :]
+
+        # convert to types for display and save to file
+        im_augmented = (255 * im_augmented).astype(np.uint8)
+        dmap_augmented = dmap_augmented.astype(np.float32)
+        mask_augmented = mask_augmented.astype(np.uint8)
+        seg_augmented = seg_augmented.astype(np.uint8)
+        lab_augmented = lab_augmented.astype(np.uint8)
+
         if DEBUG:
             plt.clf()
             plt.subplot(321)
-            plt.imshow(im_augmented[i, :, :, :])
+            plt.imshow(im[i, :, :, :])
             plt.subplot(322)
-            plt.imshow(dmap_augmented[i, :, :, 0])
+            plt.imshow(im_augmented)
             plt.subplot(323)
-            plt.imshow(mask_augmented[i, :, :, 0])
+            plt.imshow(dmap_augmented)
             plt.subplot(324)
-            a = im_augmented[i, :, :, :]
-            b = mask_augmented[i, :, :, 0]
-            plt.imshow(pystoim.imfuse(a, b))
+            plt.imshow(mask_augmented)
             plt.subplot(325)
-            plt.imshow(seg_augmented[i, :, :, 0])
+            plt.imshow(seg_augmented)
             plt.subplot(326)
-            plt.imshow(lab_augmented[i, :, :, 0])
+            plt.imshow(lab_augmented)
 
         # create filenames based on the original foo.tif, so that we have im_seed_001_foo.tif, dmap_seed_001_foo.tif,
         # mask_seed_001_foo.tif, where seed_001 means data augmented using seed=1
         base_file = im_file_list[i]
         base_path, base_name = os.path.split(base_file)
+        transform_file = os.path.join(training_augmented_dir, 'transform_seed_' + str(seed).zfill(3) + '_'
+                                      + base_name.replace('.tif', '.pickle'))
         im_file = os.path.join(training_augmented_dir, 'im_seed_' + str(seed).zfill(3) + '_' + base_name)
         dmap_file = os.path.join(training_augmented_dir, 'dmap_seed_' + str(seed).zfill(3) + '_' + base_name)
         mask_file = os.path.join(training_augmented_dir, 'mask_seed_' + str(seed).zfill(3) + '_' + base_name)
         seg_file = os.path.join(training_augmented_dir, 'seg_seed_' + str(seed).zfill(3) + '_' + base_name)
         lab_file = os.path.join(training_augmented_dir, 'lab_seed_' + str(seed).zfill(3) + '_' + base_name)
 
+        # save tranformation in keras format
+        pickle.dump(transform[i], open(transform_file, 'wb'))
+
         # save transformed image
-        im_out = im_augmented[i, :, :, :]
-        im_out *= 255
-        im_out = im_out.astype(np.uint8)
-        im_out = Image.fromarray(im_out, mode='RGB')
+        im_out = Image.fromarray(im_augmented, mode='RGB')
         im_out.save(im_file)
 
         # save distance transforms (note: we have to save as float mode)
-        im_out = dmap_augmented[i, :, :, 0]
-        im_out = Image.fromarray(im_out, mode='F')
+        im_out = Image.fromarray(dmap_augmented, mode='F')
         im_out.save(dmap_file)
 
         # save mask (note: we can save as black and white 1 byte)
-        im_out = mask_augmented[i, :, :, 0]
-        im_out = im_out.astype(np.uint8)
-        im_out = Image.fromarray(im_out, mode='L')
+        im_out = Image.fromarray(mask_augmented, mode='L')
         im_out.save(mask_file)
 
         # save contours (note: we can save as black and white 1 byte)
-        im_out = seg_augmented[i, :, :, 0]
-        im_out = Image.fromarray(im_out, mode='L')
+        im_out = Image.fromarray(seg_augmented, mode='L')
         im_out.save(seg_file)
 
         # save labels (note: we can save as black and white 1 byte)
-        im_out = lab_augmented[i, :, :, 0]
-        im_out = Image.fromarray(im_out, mode='L')
+        im_out = Image.fromarray(lab_augmented, mode='L')
         im_out.save(lab_file)
