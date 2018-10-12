877f1142c0767e85967de486cdd9b354977d16a8
Ramón Casero <rcasero@gmail.com>
2018-10-12 17:35:37 +0100

    add u-net

--- scripts/klf14_b6ntac_exp_0019_cnn_dice_coloured_labels.py
+++ scripts/klf14_b6ntac_exp_0019_cnn_dice_coloured_labels.py
@@ -38,7 +38,7 @@ import keras
 import keras.backend as K
 
 from keras.models import Model
-from keras.layers import Input, Conv2D, MaxPooling2D, AvgPool2D, Activation
+from keras.layers import Input, Conv2D, MaxPooling2D, AvgPool2D, Activation, UpSampling2D, Dropout, concatenate
 
 # for data parallelism in keras models
 from keras.utils import multi_gpu_model
@@ -127,6 +127,64 @@ plt.ylabel('Count', fontsize=18)
 '''
 
 
+def unet(input_shape=(256, 256, 1), for_receptive_field=False):
+
+    if for_receptive_field:
+        activation = 'linear'
+        pool_f = AvgPool2D
+    else:
+        activation = 'relu'
+        pool_f = MaxPooling2D
+
+    inputs = Input(input_shape, name='input_image')
+    conv1 = Conv2D(64, 3, activation=activation, padding='same', kernel_initializer='he_normal')(inputs)
+    conv1 = Conv2D(64, 3, activation=activation, padding='same', kernel_initializer='he_normal')(conv1)
+    pool1 = pool_f(pool_size=(2, 2))(conv1)
+    conv2 = Conv2D(128, 3, activation=activation, padding='same', kernel_initializer='he_normal')(pool1)
+    conv2 = Conv2D(128, 3, activation=activation, padding='same', kernel_initializer='he_normal')(conv2)
+    pool2 = pool_f(pool_size=(2, 2))(conv2)
+    conv3 = Conv2D(256, 3, activation=activation, padding='same', kernel_initializer='he_normal')(pool2)
+    conv3 = Conv2D(256, 3, activation=activation, padding='same', kernel_initializer='he_normal')(conv3)
+    pool3 = pool_f(pool_size=(2, 2))(conv3)
+    conv4 = Conv2D(512, 3, activation=activation, padding='same', kernel_initializer='he_normal')(pool3)
+    conv4 = Conv2D(512, 3, activation=activation, padding='same', kernel_initializer='he_normal')(conv4)
+    drop4 = Dropout(0.5)(conv4)
+    pool4 = pool_f(pool_size=(2, 2))(drop4)
+
+    conv5 = Conv2D(1024, 3, activation=activation, padding='same', kernel_initializer='he_normal')(pool4)
+    conv5 = Conv2D(1024, 3, activation=activation, padding='same', kernel_initializer='he_normal')(conv5)
+    drop5 = Dropout(0.5)(conv5)
+
+    up6 = Conv2D(512, 2, activation=activation, padding='same', kernel_initializer='he_normal')(UpSampling2D(size=(2, 2))(drop5))
+    merge6 = concatenate([drop4, up6])
+    conv6 = Conv2D(512, 3, activation=activation, padding='same', kernel_initializer='he_normal')(merge6)
+    conv6 = Conv2D(512, 3, activation=activation, padding='same', kernel_initializer='he_normal')(conv6)
+
+    up7 = Conv2D(256, 2, activation=activation, padding='same', kernel_initializer='he_normal')(UpSampling2D(size=(2, 2))(conv6))
+    merge7 = concatenate([conv3, up7], axis=3)
+    conv7 = Conv2D(256, 3, activation=activation, padding='same', kernel_initializer='he_normal')(merge7)
+    conv7 = Conv2D(256, 3, activation=activation, padding='same', kernel_initializer='he_normal')(conv7)
+
+    up8 = Conv2D(128, 2, activation=activation, padding='same', kernel_initializer='he_normal')(UpSampling2D(size=(2, 2))(conv7))
+    merge8 = concatenate([conv2, up8], axis=3)
+    conv8 = Conv2D(128, 3, activation=activation, padding='same', kernel_initializer='he_normal')(merge8)
+    conv8 = Conv2D(128, 3, activation=activation, padding='same', kernel_initializer='he_normal')(conv8)
+
+    up9 = Conv2D(64, 2, activation=activation, padding='same', kernel_initializer='he_normal')(UpSampling2D(size=(2, 2))(conv8))
+    merge9 = concatenate([conv1, up9], axis=3)
+    conv9 = Conv2D(64, 3, activation=activation, padding='same', kernel_initializer='he_normal')(merge9)
+    conv9 = Conv2D(64, 3, activation=activation, padding='same', kernel_initializer='he_normal')(conv9)
+    conv9 = Conv2D(2, 3, activation=activation, padding='same', kernel_initializer='he_normal')(conv9)
+    if for_receptive_field:
+        regression_output = Conv2D(1, 1, activation='linear', name='regression_output')(conv9)
+    else:
+        regression_output = Conv2D(1, 1, activation='sigmoid', name='regression_output')(conv9)
+
+    model = Model(inputs=inputs, outputs=regression_output)
+
+    return model
+
+
 def fcn_sherrah2016_regression(input_shape, for_receptive_field=False):
 
     input = Input(shape=input_shape, dtype='float32', name='input_image')
@@ -183,13 +241,14 @@ def fcn_sherrah2016_regression(input_shape, for_receptive_field=False):
 
 # estimate receptive field of the model
 def model_build_func(input_shape):
-    model = fcn_sherrah2016_regression(input_shape=input_shape, for_receptive_field=True)
+    # model = fcn_sherrah2016_regression(input_shape=input_shape, for_receptive_field=True)
+    model = unet(input_shape=input_shape, for_receptive_field=True)
     return model
 
 rf = KerasReceptiveField(model_build_func, init_weights=True)
 
 rf_params = rf.compute(
-    input_shape=(500, 500, 3),
+    input_shape=(512, 512, 3),
     input_layer='input_image',
     output_layers=['regression_output'])
 print(rf_params)
