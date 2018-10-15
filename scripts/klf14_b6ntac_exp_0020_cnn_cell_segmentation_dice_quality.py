d8ad2c075d2104f11a66bfd46b75da157dafe6df
Ramón Casero <r.casero@har.mrc.ac.uk>
2018-10-15 02:00:32 +0100

    [broken] start writing script to create an image for each segmented cell, and together with their Dice coeff, train a neural network to estimate segmentation quality

---
+++ scripts/klf14_b6ntac_exp_0020_cnn_cell_segmentation_dice_quality.py
@@ -0,0 +1,406 @@
+"""
+Training of CNN for regression of Dice coefficients, using element-wise weighting of the loss function to ignore image
+regions outside the training data.
+"""
+
+# cross-platform home directory
+from pathlib import Path
+home = str(Path.home())
+
+# PyCharm automatically adds cytometer to the python path, but this doesn't happen if the script is run
+# with "python scriptname.py"
+import os
+import sys
+sys.path.extend([os.path.join(home, 'Software/cytometer')])
+import pickle
+import inspect
+
+# other imports
+import glob
+import shutil
+import datetime
+import numpy as np
+import matplotlib.pyplot as plt
+
+# use CPU for testing on laptop
+#os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"   # see issue #152
+#os.environ["CUDA_VISIBLE_DEVICES"] = ""
+
+# limit number of GPUs
+os.environ['CUDA_VISIBLE_DEVICES'] = '0,1'
+
+os.environ['KERAS_BACKEND'] = 'tensorflow'
+import keras
+import keras.backend as K
+
+from keras.models import Model
+from keras.layers import Input, Conv2D, MaxPooling2D, AvgPool2D, Activation
+
+# for data parallelism in keras models
+from keras.utils import multi_gpu_model
+
+import cytometer.data
+import cytometer.model_checkpoint_parallel
+import tensorflow as tf
+from skimage.measure import regionprops
+
+# # limit GPU memory used
+# from keras.backend.tensorflow_backend import set_session
+# config = tf.ConfigProto()
+# config.gpu_options.per_process_gpu_memory_fraction = 1.0
+# set_session(tf.Session(config=config))
+
+# specify data format as (n, row, col, channel)
+K.set_image_data_format('channels_last')
+
+DEBUG = False
+
+# number of blocks to split each image into so that training fits into GPU memory
+# nblocks = 2
+
+# number of folds for k-fold cross validation
+n_folds = 11
+
+# number of epochs for training
+epochs = 20
+
+# area (pixel**2) of the smallest object we accept as a cell (pi * (16 pixel)**2 = 804.2 pixel**2)
+smallest_cell_area = 804
+
+# training window length
+training_window_len = 401
+
+'''Directories and filenames
+'''
+
+# data paths
+root_data_dir = os.path.join(home, 'Data/cytometer_data/klf14')
+training_dir = os.path.join(root_data_dir, 'klf14_b6ntac_training')
+training_non_overlap_data_dir = os.path.join(root_data_dir, 'klf14_b6ntac_training_non_overlap')
+training_augmented_dir = os.path.join(root_data_dir, 'klf14_b6ntac_training_augmented')
+saved_models_dir = os.path.join(root_data_dir, 'saved_models')
+
+# script name to identify this experiment
+experiment_id = inspect.getfile(inspect.currentframe())
+if experiment_id == '<input>':
+    experiment_id = 'unknownscript'
+else:
+    experiment_id = os.path.splitext(os.path.basename(experiment_id))[0]
+
+'''CNN classifier to estimate Dice coefficient
+'''
+
+
+def fcn_sherrah2016_regression(input_shape, for_receptive_field=False):
+
+    input = Input(shape=input_shape, dtype='float32', name='input_image')
+
+    x = Conv2D(filters=32, kernel_size=(5, 5), strides=1, dilation_rate=1, padding='same')(input)
+    if for_receptive_field:
+        x = Activation('linear')(x)
+        x = AvgPool2D(pool_size=(3, 3), strides=1, padding='same')(x)
+    else:
+        x = Activation('relu')(x)
+        x = MaxPooling2D(pool_size=(3, 3), strides=1, padding='same')(x)
+
+    x = Conv2D(filters=int(96/2), kernel_size=(5, 5), strides=1, dilation_rate=2, padding='same')(x)
+    if for_receptive_field:
+        x = Activation('linear')(x)
+        x = AvgPool2D(pool_size=(5, 5), strides=1, padding='same')(x)
+    else:
+        x = Activation('relu')(x)
+        x = MaxPooling2D(pool_size=(5, 5), strides=1, padding='same')(x)
+
+    x = Conv2D(filters=int(128/2), kernel_size=(3, 3), strides=1, dilation_rate=4, padding='same')(x)
+    if for_receptive_field:
+        x = Activation('linear')(x)
+        x = AvgPool2D(pool_size=(9, 9), strides=1, padding='same')(x)
+    else:
+        x = Activation('relu')(x)
+        x = MaxPooling2D(pool_size=(9, 9), strides=1, padding='same')(x)
+
+    x = Conv2D(filters=int(196/2), kernel_size=(3, 3), strides=1, dilation_rate=8, padding='same')(x)
+    if for_receptive_field:
+        x = Activation('linear')(x)
+        x = AvgPool2D(pool_size=(17, 17), strides=1, padding='same')(x)
+    else:
+        x = Activation('relu')(x)
+        x = MaxPooling2D(pool_size=(17, 17), strides=1, padding='same')(x)
+
+    x = Conv2D(filters=int(512/2), kernel_size=(3, 3), strides=1, dilation_rate=16, padding='same')(x)
+    if for_receptive_field:
+        x = Activation('linear')(x)
+    else:
+        x = Activation('relu')(x)
+
+    # regression output
+    x = Conv2D(filters=1, kernel_size=(1, 1), strides=1, dilation_rate=1, padding='same')(x)
+    regression_output = Activation('hard_sigmoid', name='regression_output')(x)
+
+    return Model(inputs=input, outputs=[regression_output])
+
+
+'''Models that were used to generate the segmentations
+'''
+
+saved_contour_model_basename = 'klf14_b6ntac_exp_0006_cnn_contour'  # contour
+saved_dmap_model_basename = 'klf14_b6ntac_exp_0015_cnn_dmap'  # dmap
+
+contour_model_name = saved_contour_model_basename + '*.h5'
+dmap_model_name = saved_dmap_model_basename + '*.h5'
+
+'''Filenames of whole dataset, and indices of train vs. test subsets
+'''
+
+# load list of images, and indices for training vs. testing indices
+contour_model_kfold_filename = os.path.join(saved_models_dir, saved_contour_model_basename + '_info.pickle')
+with open(contour_model_kfold_filename, 'rb') as f:
+    aux = pickle.load(f)
+im_orig_file_list = aux['file_list']
+idx_orig_test_all = aux['idx_test_all']
+
+# number of original training images
+n_im = len(im_orig_file_list)
+
+# correct home directory if we are in a different system than what was used to train the models
+im_orig_file_list = cytometer.data.change_home_directory(im_orig_file_list, '/users/rittscher/rcasero', home,
+                                                         check_isfile=True)
+
+'''Loop folds (in this script, we actually only work with fold 0)
+'''
+
+# loop each fold: we split the data into train vs test, train a model, and compute errors with the
+# test data. In each fold, the test data is different
+# for i_fold, idx_test in enumerate(idx_test_all):
+for i_fold, idx_test in enumerate([idx_orig_test_all[0]]):
+
+    '''Load data
+    '''
+
+    # split the data into training and testing datasets
+    im_orig_test_file_list, im_orig_train_file_list = cytometer.data.split_list(im_orig_file_list, idx_test)
+
+    # add the augmented image files
+    im_train_file_list = cytometer.data.augment_file_list(im_orig_train_file_list, '_nan_', '_*_')
+    im_test_file_list = cytometer.data.augment_file_list(im_orig_test_file_list, '_nan_', '_*_')
+
+    # load the train and test data: im, mask, preddice_kfold_00, predlab_kfold_00 data
+    # Note: we need to load whole images, to avoid splitting cells, that then become unusable
+    preddice_str = 'preddice_kfold_' + str(i_fold).zfill(2)
+    predlab_str = 'predlab_kfold_' + str(i_fold).zfill(2)
+    train_dataset, train_file_list, train_shuffle_idx = \
+        cytometer.data.load_datasets(im_train_file_list, prefix_from='im',
+                                     prefix_to=['im', 'mask', preddice_str, predlab_str],
+                                     nblocks=1, shuffle_seed=i_fold)
+    test_dataset, test_file_list, test_shuffle_idx = \
+        cytometer.data.load_datasets(im_test_file_list, prefix_from='im',
+                                     prefix_to=['im', 'mask', preddice_str, predlab_str],
+                                     nblocks=1, shuffle_seed=i_fold)
+
+    if DEBUG:
+        i = 150
+        plt.clf()
+        for pi, prefix in enumerate(train_dataset.keys()):
+            plt.subplot(1, len(train_dataset.keys()), pi + 1)
+            if train_dataset[prefix].shape[-1] < 3:
+                plt.imshow(train_dataset[prefix][i, :, :, 0])
+            else:
+                plt.imshow(train_dataset[prefix][i, :, :, :])
+            plt.title('out[' + prefix + ']')
+
+        i = 22
+        plt.clf()
+        for pi, prefix in enumerate(test_dataset.keys()):
+            plt.subplot(1, len(test_dataset.keys()), pi + 1)
+            if test_dataset[prefix].shape[-1] < 3:
+                plt.imshow(test_dataset[prefix][i, :, :, 0])
+            else:
+                plt.imshow(test_dataset[prefix][i, :, :, :])
+            plt.title('out[' + prefix + ']')
+
+    '''Extract one window for each individual cell, with the corresponding Dice coefficient
+    '''
+
+    # (r,c) size of the image
+    n_row = train_dataset['im'].shape[1]
+    n_col = train_dataset['im'].shape[2]
+
+    training_windows_list = []
+    dice_list = []
+    for i in range(train_dataset['im'].shape[0]):
+
+        # print('Image ' + str(i) + '/' + str(train_dataset['im'].shape[0] - 1))
+
+        # for convenience, we save a copy of the Dice coefficients for the current image
+        dice_aux = train_dataset[preddice_str][i, :, :, 0]
+
+        # mask labels that have a Dice coefficient value. These are the ones we can use for training. We ignore the
+        # other labels, as they have no ground truth to compare against, but by default they get Dice = 0.0
+        labels = train_dataset[predlab_str][i, :, :, 0] \
+                 * (train_dataset[preddice_str][i, :, :, 0] > 0.0).astype(np.float32)
+        labels = labels.astype(np.int32)
+
+        # compute centers of mass for the testing labels (note that the background 0 label is ignored)
+        props = regionprops(labels, coordinates='rc')
+
+        for p in props:
+
+            # sometimes we get artifact labels, for really tiny objects. We ignore those
+            if p['area'] < smallest_cell_area:
+                continue
+
+            # Dice value of the label (note we take the median to ignore small interpolation errors that pick Dice
+            # values from adjacent labels)
+            dice_list.append(dice_aux[labels == p['label']])
+
+            # width and height of the label's bounding box. Taking into account: Bounding box
+            # (min_row, min_col, max_row, max_col). Pixels belonging to the bounding box are in the half-open interval
+            # [min_row; max_row) and [min_col; max_col).
+            bbox_min_row = p['bbox'][0]
+            bbox_max_row = p['bbox'][2]
+            bbox_min_col = p['bbox'][1]
+            bbox_max_col = p['bbox'][3]
+            bbox_width = bbox_max_col - bbox_min_col
+            bbox_height = bbox_max_row - bbox_min_row
+
+            # padding of cell bbox so that it's centered in the larger bbox (i.e. the training window)
+            pad_left = int(np.round((training_window_len - bbox_width) / 2.0))
+            pad_bottom = int(np.round((training_window_len - bbox_height) / 2.0))
+
+            # (r,c)-coordinates of the larger bbox within dataset['im'][i, :, :, 0]
+            # Note: if the bbox is quite close to an edge, the larger bbox may overflow out of the image
+            lbox_min_row = bbox_min_row - pad_bottom
+            lbox_max_row = lbox_min_row + training_window_len
+            lbox_min_col = bbox_min_col - pad_left
+            lbox_max_col = lbox_min_col + training_window_len
+
+            # compute correction to the larger bbox to avoid overflowing the image
+            delta_min_row = - lbox_min_row if lbox_min_row < 0 else 0
+            delta_max_row = n_row - lbox_max_row if lbox_max_row > n_row else 0
+            delta_min_col = - lbox_min_col if lbox_min_col < 0 else 0
+            delta_max_col = n_col - lbox_max_col if lbox_max_col > n_col else 0
+
+            # apply the correction
+            lbox_min_row += delta_min_row
+            lbox_max_row += delta_max_row
+            lbox_min_col += delta_min_col
+            lbox_max_col += delta_max_col
+
+            # array indices for the training window
+            i_min_row = delta_min_row
+            i_max_row = training_window_len + delta_max_row
+            i_min_col = delta_min_col
+            i_max_col = training_window_len + delta_max_col
+
+            # check that the larger bbox we extract from 'im' has the same size as the subarray we target in the
+            # training window
+            assert(lbox_max_row - lbox_min_row == i_max_row - i_min_row)
+            assert(lbox_max_col - lbox_min_col == i_max_col - i_min_col)
+
+            # extract the training window
+            training_window = np.zeros(shape=(training_window_len, training_window_len, train_dataset['im'].shape[3]),
+                                       dtype=train_dataset['im'].dtype)
+            training_window[i_min_row:i_max_row, i_min_col:i_max_col, :] = \
+                train_dataset['im'][i, lbox_min_row:lbox_max_row, lbox_min_col:lbox_max_col, :]
+            training_windows_list.append(training_window)
+
+            if DEBUG:
+                plt.clf()
+                plt.subplot(311)
+                plt.imshow(train_dataset['im'][i, :, :, :])
+                plt.contour(labels == p['label'], levels=1)
+                plt.plot([bbox_min_col, bbox_max_col, bbox_max_col, bbox_min_col, bbox_min_col],
+                         [bbox_min_row, bbox_min_row, bbox_max_row, bbox_max_row, bbox_min_row], 'r')
+                plt.subplot(312)
+                # plt.imshow(labels)
+                plt.imshow(dice_aux)
+                plt.subplot(313)
+                plt.imshow(training_window)
+
+    # convert list to array
+    training_windows_list = np.stack(training_windows_list)
+
+
+    '''Convolutional neural network training
+    
+    Note: you need to use my branch of keras with the new functionality, that allows element-wise weights of the loss
+    function
+    '''
+
+    # list all CPUs and GPUs
+    device_list = K.get_session().list_devices()
+
+    # number of GPUs
+    gpu_number = np.count_nonzero(['GPU' in str(x) for x in device_list])
+
+    # instantiate model
+    with tf.device('/cpu:0'):
+        model = fcn_sherrah2016_regression(input_shape=train_dataset['im'].shape[1:])
+
+    saved_model_filename = os.path.join(saved_models_dir, experiment_id + '_model_fold_' + str(i_fold) + '.h5')
+
+    if gpu_number > 1:  # compile and train model: Multiple GPUs
+
+        # checkpoint to save model after each epoch
+        checkpointer = cytometer.model_checkpoint_parallel.ModelCheckpoint(filepath=saved_model_filename,
+                                                                           verbose=1, save_best_only=True)
+        # compile model
+        parallel_model = multi_gpu_model(model, gpus=gpu_number)
+        parallel_model.compile(loss={'regression_output': 'mse'},
+                               optimizer='Adadelta',
+                               metrics={'regression_output': ['mse', 'mae']},
+                               sample_weight_mode='element')
+
+        # train model
+        tic = datetime.datetime.now()
+        parallel_model.fit(train_dataset['im'],
+                           {'regression_output': train_dataset['preddice_kfold_' + str(i_fold).zfill(2)]},
+                           sample_weight={'regression_output': train_dataset['mask'][..., 0]},
+                           validation_data=(test_dataset['im'],
+                                            {'regression_output': test_dataset['preddice_kfold_' + str(i_fold).zfill(2)]},
+                                            {'regression_output': test_dataset['mask'][..., 0]}),
+                           batch_size=10, epochs=epochs, initial_epoch=0,
+                           callbacks=[checkpointer])
+        toc = datetime.datetime.now()
+        print('Training duration: ' + str(toc - tic))
+
+    else:  # compile and train model: One GPU
+
+        # checkpoint to save model after each epoch
+        checkpointer = keras.callbacks.ModelCheckpoint(filepath=saved_model_filename,
+                                                       verbose=1, save_best_only=True)
+
+        # compile model
+        model.compile(loss={'regression_output': 'mse'},
+                      optimizer='Adadelta',
+                      metrics={'regression_output': ['mse', 'mae']},
+                      sample_weight_mode='element')
+
+        # train model
+        tic = datetime.datetime.now()
+        model.fit(train_dataset['im'],
+                  {'regression_output': train_dataset['preddice_kfold_' + str(i_fold).zfill(2)]},
+                  sample_weight={'regression_output': train_dataset['mask'][..., 0]},
+                  validation_data=(test_dataset['im'],
+                                   {'regression_output': test_dataset['preddice_kfold_' + str(i_fold).zfill(2)]},
+                                   {'regression_output': test_dataset['mask'][..., 0]}),
+                  batch_size=10, epochs=epochs, initial_epoch=0,
+                  callbacks=[checkpointer])
+        toc = datetime.datetime.now()
+        print('Training duration: ' + str(toc - tic))
+
+# if we run the script with qsub on the cluster, the standard output is in file
+# klf14_b6ntac_exp_0001_cnn_dmap_contour.sge.sh.oPID where PID is the process ID
+# Save it to saved_models directory
+log_filename = os.path.join(saved_models_dir, experiment_id + '.log')
+stdout_filename = os.path.join(home, 'Software', 'cytometer', 'scripts', experiment_id + '.sge.sh.o*')
+stdout_filename = glob.glob(stdout_filename)[0]
+if stdout_filename and os.path.isfile(stdout_filename):
+    shutil.copy2(stdout_filename, log_filename)
+else:
+    # if we ran the script with nohup in linux, the standard output is in file nohup.out.
+    # Save it to saved_models directory
+    log_filename = os.path.join(saved_models_dir, experiment_id + '.log')
+    nohup_filename = os.path.join(home, 'Software', 'cytometer', 'scripts', 'nohup.out')
+    if os.path.isfile(nohup_filename):
+        shutil.copy2(nohup_filename, log_filename)
