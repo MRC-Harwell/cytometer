b5a1bdea4ced8b956ca691cda78603889cc631a5
Ramón Casero <rcasero@gmail.com>
2018-11-06 17:22:23 +0000

    [broken] start writing function for segmentation_pipeline()

--- scripts/klf14_b6ntac_exp_0031_cell_population_from_segmentation_pipeline.py
+++ scripts/klf14_b6ntac_exp_0031_cell_population_from_segmentation_pipeline.py
@@ -33,7 +33,7 @@ from statsmodels.distributions.empirical_distribution import ECDF
 #os.environ["CUDA_VISIBLE_DEVICES"] = ""
 
 # limit number of GPUs
-os.environ['CUDA_VISIBLE_DEVICES'] = '0,1'
+os.environ['CUDA_VISIBLE_DEVICES'] = '1,3'
 
 os.environ['KERAS_BACKEND'] = 'tensorflow'
 import keras
@@ -104,7 +104,7 @@ im = PIL.Image.open(os.path.join(training_dir, orig_file))
 xres = 0.0254 / im.info['dpi'][0] * 1e6  # um
 yres = 0.0254 / im.info['dpi'][1] * 1e6  # um
 
-# load the train and test data: im, seg, dmap and mask data
+# load data
 full_dataset, full_file_list, full_shuffle_idx = \
     cytometer.data.load_datasets(im_file_list, prefix_from='im', prefix_to=['im', 'lab'], nblocks=1)
 
@@ -212,97 +212,46 @@ Automatically extracted cells
 '''Load data
 '''
 
+saved_contour_model_basename = 'klf14_b6ntac_exp_0006_cnn_contour'  # contour
+saved_dmap_model_basename = 'klf14_b6ntac_exp_0015_cnn_dmap'  # dmap
+
+contour_model_name = saved_contour_model_basename + '*.h5'
+dmap_model_name = saved_dmap_model_basename + '*.h5'
+
+# load model weights for each fold
+contour_model_files = glob.glob(os.path.join(saved_models_dir, contour_model_name))
+dmap_model_files = glob.glob(os.path.join(saved_models_dir, dmap_model_name))
+contour_n_folds = len(contour_model_files)
+dmap_n_folds = len(dmap_model_files)
+
+# load k-fold sets that were used to train the models (we assume they are the same for contours and dmaps)
+saved_contour_model_kfold_filename = os.path.join(saved_models_dir, saved_contour_model_basename + '_info.pickle')
+with open(saved_contour_model_kfold_filename, 'rb') as f:
+    aux = pickle.load(f)
+im_file_list = aux['file_list']
+idx_test_all = aux['idx_test_all']
+
+# correct home directory if we are in a different system than what was used to train the models
+im_file_list = cytometer.data.change_home_directory(im_file_list, '/users/rittscher/rcasero', home, check_isfile=True)
+
 # in this script, we actually only work with fold 0
-i_fold = 0
+fold_i = 0
 
-# one-cell windows from the segmentations obtained with our pipeline
-# ['train_cell_im', 'train_cell_reflab', 'train_cell_testlab', 'train_cell_dice', 'test_cell_im',
-# 'test_cell_reflab', 'test_cell_testlab', 'test_cell_dice']
-onecell_filename = os.path.join(training_augmented_dir, 'onecell_kfold_' + str(i_fold).zfill(2) + '_worsen_000.npz')
-dataset_orig = np.load(onecell_filename)
+# split the data into training and testing datasets
+im_test_file_list, _ = cytometer.data.split_list(im_file_list, idx_test_all[fold_i])
 
-# read the data into memory
-train_cell_im = dataset_orig['train_cell_im']
-train_cell_reflab = dataset_orig['train_cell_reflab']
-train_cell_testlab = dataset_orig['train_cell_testlab']
-train_cell_dice = dataset_orig['train_cell_dice']
-test_cell_im = dataset_orig['test_cell_im']
-test_cell_reflab = dataset_orig['test_cell_reflab']
-test_cell_testlab = dataset_orig['test_cell_testlab']
-test_cell_dice = dataset_orig['test_cell_dice']
-
-# multiply histology by segmentations to have a single input tensor
-masked_test_cell_im = test_cell_im * np.repeat(test_cell_testlab.astype(np.float32),
-                                               repeats=test_cell_im.shape[3], axis=3)
-
-# free up memory
-del dataset_orig
-
-'''Load neural network for predictions
-'''
-
-model_name = experiment_id + '_model_fold_' + str(i_fold) + '.h5'
-
-saved_model_filename = os.path.join(saved_models_dir, model_name)
+# load im, seg and mask datasets
+test_datasets, _, _ = cytometer.data.load_datasets(im_test_file_list, prefix_from='im',
+                                                   prefix_to=['im', 'lab', 'seg', 'mask'], nblocks=1)
+im_test = test_datasets['im']
+seg_test = test_datasets['seg']
+mask_test = test_datasets['mask']
+lab_test = test_datasets['lab']
+del test_datasets
 
 # list of model files to inspect
-model_files = glob.glob(os.path.join(saved_models_dir, model_name))
+contour_model_files = glob.glob(os.path.join(saved_models_dir, contour_model_name))
+dmap_model_files = glob.glob(os.path.join(saved_models_dir, dmap_model_name))
 
-model_file = model_files[i_fold]
-
-# load model
-model = keras.models.load_model(model_file)
-
-test_cell_preddice = np.zeros(shape=test_cell_dice.shape, dtype=test_cell_dice.dtype)
-for i in range(test_cell_im.shape[0]):
-
-    if not i % 20:
-        print('Test image: ' + str(i) + '/' + str(test_cell_im.shape[0]-1))
-
-    # predict Dice coefficient for test segmentation
-    test_cell_preddice[i] = model.predict(np.expand_dims(masked_test_cell_im[i, :, :, :], axis=0))
-
-# plot scatter plot all predicted vs. ground truth Dice values
-if DEBUG:
-    plt.clf()
-    plt.scatter(test_cell_dice, test_cell_preddice)
-    plt.plot([0.2, 1.0], [0.2, 1.0])
-    plt.plot([.9, .9], [.1, 1.0], 'r')
-    plt.plot([.1, 1.0], [.9, .9], 'r')
-    plt.xlabel('Ground truth')
-    plt.ylabel('Estimated')
-    plt.title('Dice coefficient')
-
-# Pearson coefficient
-print('Pearson coeff = ' + "{:.2f}".format(np.corrcoef(np.stack((test_cell_dice, test_cell_preddice)))[0, 1]))
-
-# confusion table: estimated vs ground truth
-est0_gt0 = np.count_nonzero(np.logical_and(test_cell_preddice < valid_threshold, test_cell_dice < valid_threshold)) \
-    / len(test_cell_preddice)
-est0_gt1 = np.count_nonzero(np.logical_and(test_cell_preddice < valid_threshold, test_cell_dice >= valid_threshold)) \
-    / len(test_cell_preddice)
-est1_gt0 = np.count_nonzero(np.logical_and(test_cell_preddice >= valid_threshold, test_cell_dice < valid_threshold)) \
-    / len(test_cell_preddice)
-est1_gt1 = np.count_nonzero(np.logical_and(test_cell_preddice >= valid_threshold, test_cell_dice >= valid_threshold)) \
-    / len(test_cell_preddice)
-
-print(np.array([["{:.2f}".format(est1_gt0), "{:.2f}".format(est1_gt1)],
-                ["{:.2f}".format(est0_gt0), "{:.2f}".format(est0_gt1)]]))
-
-
-# plot prediction
-if DEBUG:
-    i = 150
-    plt.clf()
-    plt.imshow(test_cell_im[i, :, :, :])
-    plt.contour(test_cell_testlab[i, :, :, 0], levels=1, colors='green')
-    plt.title('Dice: ' + "{:.2f}".format(test_cell_dice[i]) + ' (ground truth)\n'
-              + "{:.2f}".format(test_cell_preddice[i]) + ' (estimated)')
-
-    i = 1001
-    plt.clf()
-    plt.imshow(test_cell_im[i, :, :, :])
-    plt.contour(test_cell_testlab[i, :, :, 0], levels=1, colors='green')
-    plt.title('Dice: ' + "{:.2f}".format(test_cell_dice[i]) + ' (ground truth)\n'
-              + "{:.2f}".format(test_cell_preddice[i]) + ' (estimated)')
-
+contour_model_file = contour_model_files[fold_i]
+dmap_model_file = dmap_model_files[fold_i]
