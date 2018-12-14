1df5702e7852c2e5e8cda32fda97471cb00c489a
Ramón Casero <r.casero@har.mrc.ac.uk>
2018-12-14 22:58:59 +0000

    fix name

--- scripts/klf14_b6ntac_exp_0041_cnn_qualitynet_thresholded_sigmoid_pm_1_masked_segmentation.sge.sh
+++ scripts/klf14_b6ntac_exp_0041_cnn_qualitynet_thresholded_sigmoid_pm_1_masked_segmentation.sge.sh
@@ -14,7 +14,7 @@ module load Anaconda3/5.1.0
 
 source activate cytometer_tensorflow
 
-python klf14_b6ntac_exp_0041_cnn_qualitynet_thresholded_sigmoid_masked_segmentation.py
+python klf14_b6ntac_exp_0041_cnn_qualitynet_thresholded_sigmoid_pm_1_masked_segmentation.py
 
 echo "Finished at :"`date`
 exit 0
