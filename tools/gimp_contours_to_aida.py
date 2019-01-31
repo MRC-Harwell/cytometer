b8e3d1b1fa71f8f007cc8a174e3924f403499012
Ramón Casero <rcasero@gmail.com>
2019-01-31 20:56:18 +0000

    [broken code] small changes to read more contours from SVG files

--- tools/gimp_contours_to_aida.py
+++ tools/gimp_contours_to_aida.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
 
+import os
 import sys
 import getopt
+
+sys.path.extend([os.path.join(os.path.dirname(os.path.abspath(__file__)), '..')])  # access to cytometer python modules
 from cytometer.data import read_paths_from_svg_file
 
 
+import glob
+argv = glob.glob('/home/rcasero/Data/cytometer_data/klf14/klf14_b6ntac_training/KLF14-B6NTAC-*.svg')
 def main(argv):
 
     # default output directory
@@ -13,7 +18,7 @@ def main(argv):
     print("Number of total arguments: " + str(len(argv)))
 
     try:
-        opts, svg_files = getopt.getopt(argv, "ho:", ["outdir="])
+        opts, svg_files = getopt.getopt(argv, 'ho:', ['outdir='])
     except getopt.GetoptError:
         print(sys.argv[0] + ' -o <outdir> file1.svg [file2.svg, ...]')
         sys.exit(2)
@@ -27,10 +32,18 @@ def main(argv):
 
     print("Number of SVG files: " + str(len(svg_files)))
 
+    # collapse the list of multiple windows per image to list of original images
+    im_files = []
+    for file in
+
+    # loop SVG files
+    contour = []
     for file in svg_files:
-        print(file)
-        foo = read_paths_from_svg_file(file, tag='Cell')
-        print(foo)
+
+        # read the contours
+        contour.append(read_paths_from_svg_file(file, tag='Cell', add_offset_from_filename=True))
+
+
 
 
 if __name__ == "__main__":
