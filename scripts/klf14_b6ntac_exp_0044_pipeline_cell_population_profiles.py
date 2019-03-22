c1cb1fcfc84f101a4aa36a96fd7166979fa1f343
Ramón Casero <r.casero@har.mrc.ac.uk>
2019-03-22 01:03:51 +0000

    apply pipeline to training dataset, to compute areas

--- scripts/klf14_b6ntac_exp_0044_pipeline_cell_population_profiles.py
+++ scripts/klf14_b6ntac_exp_0044_pipeline_cell_population_profiles.py
@@ -12,7 +12,7 @@ sys.path.extend([os.path.join(home, 'Software/cytometer')])
 import cytometer.utils
 
 # limit number of GPUs
-os.environ['CUDA_VISIBLE_DEVICES'] = '2,3'
+os.environ['CUDA_VISIBLE_DEVICES'] = '0,1'
 
 os.environ['KERAS_BACKEND'] = 'tensorflow'
 
@@ -69,6 +69,9 @@ smallest_cell_area = 804
 
 # training window length
 training_window_len = 401
+
+# threshold for quality network
+quality_threshold = 0.9
 
 
 '''
@@ -401,10 +404,17 @@ with open(contour_model_kfold_filename, 'rb') as f:
 im_orig_file_list = aux['file_list']
 idx_orig_test_all = aux['idx_test_all']
 
+# read pixel size information
+im = PIL.Image.open(im_orig_file_list[0])
+xres = 0.0254 / im.info['dpi'][0] * 1e6  # um
+yres = 0.0254 / im.info['dpi'][1] * 1e6  # um
+
 # trained models for all folds
 contour_model_files = sorted(glob.glob(os.path.join(saved_models_dir, contour_model_name)))
 dmap_model_files = sorted(glob.glob(os.path.join(saved_models_dir, dmap_model_name)))
 quality_model_files = sorted(glob.glob(os.path.join(saved_models_dir, quality_model_name)))
+
+areas_all = []
 
 for fold_i, idx_test in enumerate(idx_orig_test_all):
 
@@ -413,13 +423,13 @@ for fold_i, idx_test in enumerate(idx_orig_test_all):
     '''Load data 
     '''
 
+    # split the data into training and testing datasets
+    im_test_file_list, im_train_file_list = cytometer.data.split_list(im_orig_file_list, idx_test)
+
     # load training dataset
-    datasets, _, _ = cytometer.data.load_datasets(im_orig_file_list, prefix_from='im',
-                                                  prefix_to=['im', 'lab', 'seg', 'mask'], nblocks=1)
+    datasets, _, _ = cytometer.data.load_datasets(im_test_file_list, prefix_from='im',
+                                                  prefix_to=['im'], nblocks=1)
     im = datasets['im']
-    seg = datasets['seg']
-    mask = datasets['mask']
-    reflab = datasets['lab']
     del datasets
 
     # number of images
@@ -435,193 +445,61 @@ for fold_i, idx_test in enumerate(idx_orig_test_all):
     dmap_model = keras.models.load_model(dmap_model_file)
     quality_model = keras.models.load_model(quality_model_file)
 
-    '''Cell segmentation, match estimated segmentations to ground truth segmentations and Dice coefficients
+    '''Cell segmentation with quality control
     '''
 
-    labels = np.zeros(shape=im.shape[:-1] + (1,), dtype=np.int32)
-    labels_borders = np.zeros(shape=im.shape[:-1] + (1,), dtype=np.uint8)
-    for i in range(n_im):
+    # segment histology
+    labels, labels_info = \
+        cytometer.utils.segmentation_pipeline(im,
+                                              contour_model, dmap_model, quality_model,
+                                              quality_model_type='-1_1_band',
+                                              smallest_cell_area=smallest_cell_area)
 
-        print('\tImage ' + str(i) + '/' + str(n_im-1))
+    for i in range(im.shape[0]):
 
-        # segment histology
-        labels[i, :, :, :], labels_info = \
-            cytometer.utils.segmentation_pipeline(im[i:i+1, :, :, :],
-                                                  contour_model, dmap_model, quality_model,
-                                                  quality_model_type='-1_1_band',
-                                                  smallest_cell_area=smallest_cell_area)
-
-        # run histology image through network
-        contour_pred = contour_model.predict(im[i, :, :, :].reshape((1,) + im.shape[1:]))
-        dmap_pred = dmap_model.predict(im[i, :, :, :].reshape((1,) + im.shape[1:]))
-
-        # cell segmentation
-        labels[i, :, :, 0], labels_borders[i, :, :, 0] \
-            = cytometer.utils.segment_dmap_contour(dmap_pred[0, :, :, 0],
-                                                   contour=contour_pred[0, :, :, 0],
-                                                   border_dilation=0)
-
-        # plot results of cell segmentation
         if DEBUG:
-            plt.clf()
-            plt.subplot(231)
-            plt.imshow(im[i, :, :, :])
-            plt.title('histology, i = ' + str(i))
-            plt.subplot(232)
-            plt.imshow(contour_pred[0, :, :, 0])
-            plt.title('predicted contours')
-            plt.subplot(233)
-            plt.imshow(dmap_pred[0, :, :, 0])
-            plt.title('predicted dmap')
-            plt.subplot(234)
-            plt.imshow(labels[i, :, :, 0])
-            plt.title('labels')
-            plt.subplot(235)
-            plt.imshow(labels_borders[i, :, :, 0])
-            plt.title('label borders')
-            plt.subplot(236)
-            plt.imshow(seg[i, :, :, 0])
-            plt.title('ground truth borders')
-
-        # compute quality measure of estimated labels
-        qual = cytometer.utils.match_overlapping_labels(labels_test=labels[i, :, :, 0],
-                                                        labels_ref=reflab[i, :, :, 0])
-
-        # plot validation of cell segmentation
-        if DEBUG:
-            labels_qual = cytometer.utils.paint_labels(labels=labels[i, :, :, 0], paint_labs=qual['lab_test'],
-                                                       paint_values=qual['dice'])
-
             plt.clf()
             plt.subplot(221)
             plt.imshow(im[i, :, :, :])
-            plt.title('histology, i = ' + str(i))
             plt.subplot(222)
-            plt.imshow(border[i, :, :, 0])
-            plt.title('ground truth labels')
+            plt.imshow(im[i, :, :, :])
+            plt.contour(labels[i, :, :, 0], levels=np.unique(labels[i, :, :, 0]), colors='C0')
+
+        # list of labels that are on the edges
+        lab_edge = cytometer.utils.edge_labels(labels[i, :, :, 0])
+
+        # delete edge cell labels from labels_info
+        idx_delete = np.where(np.logical_and(labels_info['im'] == i, np.isin(labels_info['label'], lab_edge)))[0]
+        labels_info = np.delete(labels_info, idx_delete)
+
+        # delete edge cells from the segmentation
+        labels[i, :, :, 0] = np.logical_not(np.isin(labels[i, :, :, 0], lab_edge)) * labels[i, :, :, 0]
+
+        if DEBUG:
             plt.subplot(223)
-            aux = np.zeros(shape=labels_borders[i, :, :, 0].shape + (3,), dtype=np.float32)
-            aux[:, :, 0] = border[i, :, :, 0]
-            aux[:, :, 1] = labels_borders[i, :, :, 0]
-            aux[:, :, 2] = border[i, :, :, 0]
-            plt.imshow(aux)
-            plt.title('estimated (green) vs. ground truth (purple)')
+            plt.imshow(im[i, :, :, :])
+            plt.contour(labels[i, :, :, 0], levels=np.unique(labels[i, :, :, 0]), colors='C0')
+
+        # list of labels that the quality network rejects
+        idx_bad = np.logical_and(labels_info['im'] == i, labels_info['quality'] < quality_threshold)
+        lab_bad = labels_info['label'][idx_bad]
+
+        # delete bad labels from labels_info
+        labels_info = np.delete(labels_info, idx_bad)
+
+        # delete bad cells from the segmentation
+        labels[i, :, :, 0] = np.logical_not(np.isin(labels[i, :, :, 0], lab_bad)) * labels[i, :, :, 0]
+
+        if DEBUG:
             plt.subplot(224)
-            aux = np.zeros(shape=labels_borders[i, :, :, 0].shape + (3,), dtype=np.float32)
-            aux[:, :, 0] = labels_qual
-            aux[:, :, 1] = labels_qual
-            aux[:, :, 2] = labels_qual
-            aux_r = aux[:, :, 0]
-            aux_r[border[i, :, :, 0] == 1.0] = 1.0
-            aux[:, :, 0] = aux_r
-            aux[:, :, 2] = aux_r
-            aux_g = aux[:, :, 1]
-            aux_g[labels_borders[i, :, :, 0] == 1.0] = 1.0
-            aux[:, :, 1] = aux_g
-            plt.imshow(aux, cmap='Greys_r')
-            plt.title('Dice coeff')
+            plt.imshow(im[i, :, :, :])
+            plt.contour(labels[i, :, :, 0], levels=np.unique(labels[i, :, :, 0]), colors='C0')
 
-        # filenames for output files
-        base_file = im_orig_file_list[i]
-        base_path, base_name = os.path.split(base_file)
-        predlab_file = os.path.join(training_augmented_dir,
-                                    base_file.replace('im_', 'predlab_kfold_' + str(fold_i).zfill(2) + '_'))
-        predseg_file = os.path.join(training_augmented_dir,
-                                    base_file.replace('im_', 'predseg_kfold_' + str(fold_i).zfill(2) + '_'))
-        labcorr_file = base_file.replace('im_', 'labcorr_kfold_' + str(fold_i).zfill(2) + '_')
-        labcorr_file = os.path.join(training_augmented_dir,
-                                    labcorr_file.replace('.tif', '.npy'))
+        # compute cell areas
+        props = regionprops(labels[i, :, :, 0])
+        p_label = [p['label'] for p in props]
+        p_area = np.array([p['area'] for p in props])
+        areas = p_area * xres * yres  # (m^2)
 
-        # save the predicted labels (one per cell)
-        im_out = Image.fromarray(labels[i, :, :, 0], mode='I')  # int32
-        im_out.save(predlab_file)
-
-        # save the predicted contours
-        im_out = Image.fromarray(labels_borders[i, :, :, 0], mode='L')  # uint8
-        im_out.save(predseg_file)
-
-        # save the correspondence between labels and Dice coefficients
-        np.save(file=labcorr_file, arr=qual)
-
-    '''Data augmentation
-    '''
-
-    # augment segmentation results
-    for seed in range(augment_factor - 1):
-
-        print('\t* Augmentation round: ' + str(seed + 1) + '/' + str(augment_factor - 1))
-
-        for i in range(n_im):
-
-            print('\t** Image ' + str(i) + '/' + str(n_im - 1))
-
-            # file name of the histology corresponding to the random transformation of the Dice coefficient image
-            base_file = im_orig_file_list[i]
-            base_path, base_name = os.path.split(base_file)
-            transform_file = os.path.join(base_path, base_name.replace('im_', 'transform_')
-                                          .replace('.tif', '.pickle').replace('seed_nan', 'seed_' + str(seed).zfill(3)))
-            transform = pickle.load(open(transform_file, 'br'))
-
-            # convert transform to skimage format
-            transform_skimage = cytometer.utils.keras2skimage_transform(transform=transform, shape=im.shape[1:3])
-
-            # apply transform to reference images
-            labels_augmented = warp(labels[i, :, :, 0], transform_skimage.inverse,
-                                    order=0, preserve_range=True)
-            labels_borders_augmented = warp(labels_borders[i, :, :, 0], transform_skimage.inverse,
-                                            order=0, preserve_range=True)
-
-            # apply flips
-            if transform['flip_horizontal'] == 1:
-                labels_augmented = labels_augmented[:, ::-1]
-                labels_borders_augmented = labels_borders_augmented[:, ::-1]
-            if transform['flip_vertical'] == 1:
-                labels_augmented = labels_augmented[::-1, :]
-                labels_borders_augmented = labels_borders_augmented[::-1, :]
-
-            # convert to correct types for display and saving to file
-            labels_augmented = labels_augmented.astype(np.int32)
-            labels_borders_augmented = labels_borders_augmented.astype(np.uint8)
-
-            if DEBUG:
-
-                # file name of the histology corresponding to the random transformation of the Dice coefficient image
-                im_file_augmented = os.path.join(training_augmented_dir, base_file.replace('seed_nan', 'seed_' + str(seed).zfill(3)))
-
-                # load histology
-                aux_dataset, _, _ = cytometer.data.load_datasets([im_file_augmented], prefix_from='im', prefix_to=['im'], nblocks=1)
-
-                qual = cytometer.utils.match_overlapping_labels(labels_test=labels[i, :, :, 0],
-                                                                labels_ref=reflab[i, :, :, 0])
-                labels_qual_augmented = cytometer.utils.paint_labels(labels=labels_augmented, paint_labs=qual['lab_test'],
-                                                                     paint_values=qual['dice'])
-
-                # compare randomly transformed histology to corresponding Dice coefficient
-                plt.clf()
-                plt.subplot(321)
-                plt.imshow(aux_dataset['im'][0, :, :, :])
-                plt.subplot(322)
-                plt.imshow(labels_qual_augmented, cmap='Greys_r')
-                plt.subplot(323)
-                aux = pystoim.imfuse(aux_dataset['im'][0, :, :, :], labels_qual_augmented)
-                plt.imshow(aux, cmap='Greys_r')
-                plt.subplot(324)
-                plt.imshow(labels_augmented)
-                plt.subplot(325)
-                plt.imshow(labels_borders_augmented)
-
-            # filenames for the Dice coefficient augmented files
-            predlab_file = os.path.join(training_augmented_dir,
-                                        base_name.replace('im_seed_nan_',
-                                                          'predlab_kfold_' + str(fold_i).zfill(2) + '_seed_' + str(seed).zfill(3) + '_'))
-            predseg_file = os.path.join(training_augmented_dir,
-                                        base_name.replace('im_seed_nan_',
-                                                          'predseg_kfold_' + str(fold_i).zfill(2) + '_seed_' + str(seed).zfill(3) + '_'))
-
-            # save the predicted labels (one per cell)
-            im_out = Image.fromarray(labels_augmented.astype(np.int32), mode='I')  # int32
-            im_out.save(predlab_file)
-
-            # save the predicted contours
-            im_out = Image.fromarray(labels_borders_augmented.astype(np.uint8), mode='L')  # uint8
-            im_out.save(predseg_file)
+        # save area values to exit list
+        areas_all += list(areas)
