3fd7cfb8df6066af653a6070ed1d27ac1d4ec024
Ramón Casero <rcasero@har.mrc.ac.uk>
2019-03-25 00:59:56 +0000

    add exp 0045: train quality network with -1 band's size proportional to equivalent radius of the object

---
+++ scripts/klf14_b6ntac_exp_0045_cnn_qualitynet_thresholded_sigmoid_pm_1_prop_band_masked_segmentation.sge.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#$ -P rittscher.prjc -q gpu8.q
+#$ -l gpu=4 -l gputype=p100
+#$ -cwd -V
+#$ -pe shmem 4
+
+echo "job ID: " $JOB_ID
+echo "Run on: " `hostname`
+echo "Started at: " `date`
+
+module use -a /mgmt/modules/eb/modules/all
+module load Anaconda3/5.1.0
+
+source activate cytometer_tensorflow
+
+python klf14_b6ntac_exp_0045_cnn_qualitynet_thresholded_sigmoid_pm_1_prop_band_masked_segmentation.py
+rv="$?"
+
+# exit status held by qacct
+echo "Main task exit status: ${rv}"
+
+echo "Finished at :"`date`
+
+exit "${rv}"
