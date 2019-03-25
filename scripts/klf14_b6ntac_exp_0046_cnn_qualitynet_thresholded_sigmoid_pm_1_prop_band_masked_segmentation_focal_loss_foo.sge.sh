4013628917f5292cff1269e3ca352545ef47db03
Ramón Casero <rcasero@har.mrc.ac.uk>
2019-03-25 22:51:38 +0000

    send 0046 experiments also on 2 GPUs, in case it cannot allocate 4 GPUs

---
+++ scripts/klf14_b6ntac_exp_0046_cnn_qualitynet_thresholded_sigmoid_pm_1_prop_band_masked_segmentation_focal_loss_foo.sge.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#$ -P rittscher.prjc -q gpu8.q
+#$ -l gpu=2 -l gputype=p100
+#$ -cwd -V
+#$ -pe shmem 2
+
+echo "job ID: " $JOB_ID
+echo "Run on: " `hostname`
+echo "Started at: " `date`
+
+module use -a /mgmt/modules/eb/modules/all
+module load Anaconda3/5.1.0
+
+source activate cytometer_tensorflow
+
+python klf14_b6ntac_exp_0046_cnn_qualitynet_thresholded_sigmoid_pm_1_prop_band_masked_segmentation_focal_loss_foo.py
+rv="$?"
+
+# exit status held by qacct
+echo "Main task exit status: ${rv}"
+
+echo "Finished at :"`date`
+
+exit "${rv}"
