66f4aa431b64f4defcdcc8e5058ea4a3f2e359fc
Ramón Casero <rcasero@har.mrc.ac.uk>
2019-04-01 23:33:47 +0100

    fix typo

--- scripts/klf14_b6ntac_exp_0049_cnn_qualitynet_pm_1_prop_band_focal_loss_dice_geq_0_5.sge.sh
+++ scripts/klf14_b6ntac_exp_0049_cnn_qualitynet_pm_1_prop_band_focal_loss_dice_geq_0_5.sge.sh
@@ -14,7 +14,7 @@ module load Anaconda3/5.1.0
 
 source activate cytometer_tensorflow
 
-python klf14_klf14_b6ntac_exp_0049_cnn_qualitynet_pm_1_prop_band_focal_loss_dice_geq_0_5.py
+python klf14_b6ntac_exp_0049_cnn_qualitynet_pm_1_prop_band_focal_loss_dice_geq_0_5.py
 rv="$?"
 
 # exit status held by qacct
