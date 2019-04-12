0a3dcee5d3b3453baf5f8beb2934c63e3698d42a
Ramón Casero <rcasero@gmail.com>
2019-04-12 18:13:55 +0100

    [broken] start experiment to try a new type of quality network training

---
+++ scripts/klf14_b6ntac_exp_0052_cnn_quality_network_train_with_overlapped_contours.py
@@ -0,0 +1,282 @@
+"""
+Training all folds of DenseNet for quality assessement of individual cells based on classification of
+thresholded Dice coefficient (Dice >= 0.9). Here the loss is binary focal loss.
+
+The reason is to center the decision boundary on 0.9, to get finer granularity around that threshold.
+
+Mask one-cell histology windows with 0/-1/+1 mask. The mask has a band with a width of 20% the equivalent radius
+of the cell (equivalent radius is the radius of a circle with the same area as the cell).
+
+The difference of this one with 0046 is that here we remove segmentations with Dice < 0.5 (automatic segmentations that
+have poor ground truth) from the training dataset.
+
+This is part of a series of experiments with different types of masks: 0039, 0040, 0041, 0042, 0045, 0046, 0048.
+"""
+
+# cross-platform home directory
+from pathlib import Path
+home = str(Path.home())
+
+# PyCharm automatically adds cytometer to the python path, but this doesn't happen if the script is run
+# with "python scriptname.py"
+import os
+import sys
+sys.path.extend([os.path.join(home, 'Software/cytometer')])
+import pickle
+import inspect
+
+# other imports
+import glob
+import shutil
+import datetime
+from PIL import Image, ImageDraw
+import numpy as np
+import matplotlib.pyplot as plt
+from skimage.measure import regionprops
+
+# use CPU for testing on laptop
+#os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"   # see issue #152
+#os.environ["CUDA_VISIBLE_DEVICES"] = ""
+
+# limit number of GPUs
+os.environ['CUDA_VISIBLE_DEVICES'] = '0,1'
+
+os.environ['KERAS_BACKEND'] = 'tensorflow'
+import keras
+import keras.backend as K
+
+from keras.applications import densenet
+# from keras_applications.densenet import DenseNet
+from keras.models import Model
+from keras.layers import Dense
+
+# for data parallelism in keras models
+from keras.utils import multi_gpu_model
+
+import cytometer.model_checkpoint_parallel
+import cytometer.utils
+import cytometer.data
+import tensorflow as tf
+
+# limit GPU memory used
+from keras.backend.tensorflow_backend import set_session
+config = tf.ConfigProto()
+config.gpu_options.per_process_gpu_memory_fraction = 0.95
+set_session(tf.Session(config=config))
+
+# specify data format as (n, row, col, channel)
+K.set_image_data_format('channels_last')
+
+DEBUG = False
+
+# number of folds for k-fold cross validation
+n_folds = 11
+
+# number of epochs for training
+epochs = 40
+
+# area (pixel**2) of the smallest object we accept as a cell (pi * (16 pixel)**2 = 804.2 pixel**2)
+smallest_cell_area = 804
+
+# training window length
+training_window_len = 401
+
+# remove from training cells that don't have a good enough overlap with a reference label
+smallest_dice = 0.5
+
+# segmentations with Dice >= threshold are accepted
+dice_threshold = 0.9
+
+# batch size for training
+batch_size = 16
+
+# data augmentation factor (e.g. "10" means that we generate 9 augmented images + the original input image)
+augment_factor = 10
+
+# generator of random transformations
+data_gen_args_float32 = dict(
+    rotation_range=90,     # randomly rotate images up to 90 degrees
+    fill_mode="constant",  # fill points outside boundaries with zeros
+    cval=0,                #
+    zoom_range=.1,         # [1-zoom_range, 1+zoom_range]
+    horizontal_flip=True,  # randomly flip images
+    vertical_flip=True,    # randomly flip images
+    dtype=np.float32       # explicit type out
+)
+datagen = keras.preprocessing.image.ImageDataGenerator(**data_gen_args_float32)
+
+'''Directories and filenames
+'''
+
+# data paths
+root_data_dir = os.path.join(home, 'Data/cytometer_data/klf14')
+training_dir = os.path.join(root_data_dir, 'klf14_b6ntac_training')
+training_data_dir = os.path.join(root_data_dir, 'klf14_b6ntac_training')
+training_non_overlap_data_dir = os.path.join(root_data_dir, 'klf14_b6ntac_training_non_overlap')
+training_augmented_dir = os.path.join(root_data_dir, 'klf14_b6ntac_training_augmented')
+saved_models_dir = os.path.join(root_data_dir, 'saved_models')
+
+saved_contour_model_basename = 'klf14_b6ntac_exp_0034_cnn_contour'  # contour
+
+# script name to identify this experiment
+experiment_id = inspect.getfile(inspect.currentframe())
+if experiment_id == '<input>':
+    experiment_id = 'unknownscript'
+else:
+    experiment_id = os.path.splitext(os.path.basename(experiment_id))[0]
+
+# # list of images, and indices for training vs. testing indices
+# contour_model_kfold_filename = os.path.join(saved_models_dir, saved_contour_model_basename + '_info.pickle')
+# with open(contour_model_kfold_filename, 'rb') as f:
+#     aux = pickle.load(f)
+# im_orig_file_list = aux['file_list']
+# idx_orig_test_all = aux['idx_test_all']
+#
+# # correct home directory, if necessary
+# im_orig_file_list = cytometer.data.change_home_directory(im_orig_file_list, home_path_from='/users/rittscher/rcasero',
+#                                                          home_path_to=home,
+#                                                          check_isfile=False)
+
+'''Process the data
+'''
+
+
+# we are interested only in .tif files for which we created hand segmented contours
+file_list = glob.glob(os.path.join(training_data_dir, '*.svg'))
+
+for i, file_svg in enumerate(file_list):
+
+    print('file ' + str(i) + '/' + str(len(file_list) - 1))
+
+    # change file extension from .svg to .tif
+    file_tif = file_svg.replace('.svg', '.tif')
+
+    # open histology training image
+    im = Image.open(file_tif)
+
+    # make array copy
+    im_array = np.array(im)
+
+    if DEBUG:
+        plt.clf()
+        plt.subplot(221)
+        plt.imshow(im)
+
+    # read the cell contours in the SVG file. This produces a list [contour_0, ..., contour_N-1] where each
+    # contour_i = [(X_0, Y_0), ..., (X_P-1, X_P-1)]
+    contours = cytometer.data.read_paths_from_svg_file(file_svg, tag='Cell', add_offset_from_filename=False)
+
+    # loop cell contours
+    for j, contour in enumerate(contours):
+
+        # centre of current cell
+        xy_c = (np.mean([p[0] for p in contour]), np.mean([p[1] for p in contour]))
+
+        if DEBUG:
+            plt.plot([p[0] for p in contour], [p[1] for p in contour])
+            plt.scatter(xy_c[0], xy_c[1])
+
+        # rasterise current cell
+        cell_seg_gtruth = Image.new("1", im.size, "black")  # I = 32-bit signed integer pixels
+        draw = ImageDraw.Draw(cell_seg_gtruth)
+        draw.polygon(contour, outline="white", fill="white")
+        cell_seg_gtruth = np.array(cell_seg_gtruth, dtype=np.uint8)
+
+        if DEBUG:
+            plt.subplot(222)
+            plt.imshow(cell_seg_gtruth)
+
+        # compute mask
+        cell_mask = cytometer.utils.quality_model_mask(cell_seg_gtruth, quality_model_type='-1_1_prop_band')[0, :, :, 0]
+        cell_mask = cell_mask.astype(np.int32)
+
+        # compute bounding box that contains the mask
+        props = regionprops((cell_mask != 0).astype(np.uint8))
+        assert(len(props) == 1)
+        bbox = props[0]['bbox']
+
+        bbox_x0 = bbox[1]
+        bbox_xend = bbox[3] - 1
+        bbox_y0 = bbox[0]
+        bbox_yend = bbox[2] - 1
+
+        if DEBUG:
+            plt.subplot(223)
+            plt.cla()
+            plt.imshow(im)
+            plt.contour(cell_mask, linewidths=1)
+            plt.plot((bbox_x0, bbox_xend, bbox_xend, bbox_x0, bbox_x0),
+                     (bbox_y0, bbox_y0, bbox_yend, bbox_yend, bbox_y0))
+
+        # largest dimension of the bounding box (the training window is going to be a square, so we have to make the
+        # bounding box square too)
+        bbox_len = np.max((bbox_xend - bbox_x0 + 1, bbox_yend - bbox_y0 + 1))
+
+        # extract training image / segmentation / mask
+        train_im, train_label, _ = \
+            cytometer.utils.one_image_per_label(np.expand_dims(im_array, axis=0),
+                                                np.expand_dims(np.expand_dims(cell_seg_gtruth, axis=0), axis=3),
+                                                dataset_lab_ref=None, training_window_len=bbox_len,
+                                                smallest_cell_area=smallest_cell_area, clear_border_lab=True)
+        train_mask, _, _ = \
+            cytometer.utils.one_image_per_label(np.expand_dims(np.expand_dims(cell_mask, axis=0), axis=3),
+                                                np.expand_dims(np.expand_dims(cell_seg_gtruth, axis=0), axis=3),
+                                                dataset_lab_ref=None, training_window_len=bbox_len,
+                                                smallest_cell_area=smallest_cell_area, clear_border_lab=True)
+
+        if DEBUG:
+            plt.subplot(224)
+            plt.cla()
+            plt.imshow(train_im[0, :, :, :])
+            plt.contour(train_mask[0, :, :, 0], linewidths=1)
+
+        # resize the training image to training window size
+        assert(train_im.dtype == np.uint8)
+        assert(train_label.dtype == np.uint8)
+        assert(train_mask.dtype == np.int32)
+        train_im = Image.fromarray(train_im[0, :, :, :])
+        train_label = Image.fromarray(train_label[0, :, :, 0])
+        train_mask = Image.fromarray(train_mask[0, :, :, 0], mode='I')
+
+        train_im = train_im.resize(size=(training_window_len, training_window_len), resample=Image.NEAREST)
+        train_label = train_label.resize(size=(training_window_len, training_window_len), resample=Image.NEAREST)
+        train_mask = train_mask.resize(size=(training_window_len, training_window_len), resample=Image.NEAREST)
+
+        train_im = np.array(train_im)
+        train_label = np.array(train_label)
+        train_mask = np.array(train_mask)
+
+        if DEBUG:
+            plt.subplot(224)
+            plt.cla()
+            plt.imshow(train_im)
+            plt.contour(train_mask, linewidths=1)
+
+        # multiply the image by the mask
+        train_im[:, :, 0] * train_im[:, :, 0] * train_mask
+        train_im[:, :, 1] * train_im[:, :, 1] * train_mask
+        train_im[:, :, 2] * train_im[:, :, 2] * train_mask
+
+
+
+
+
+
+'''Save the log of computations
+'''
+
+# if we run the script with qsub on the cluster, the standard output is in file
+# klf14_b6ntac_exp_0001_cnn_dmap_contour.sge.sh.oPID where PID is the process ID
+# Save it to saved_models directory
+log_filename = os.path.join(saved_models_dir, experiment_id + '.log')
+stdout_filename = os.path.join(home, 'Software', 'cytometer', 'scripts', experiment_id + '.sge.sh.o*')
+stdout_filename = glob.glob(stdout_filename)[0]
+if stdout_filename and os.path.isfile(stdout_filename):
+    shutil.copy2(stdout_filename, log_filename)
+else:
+    # if we ran the script with nohup in linux, the standard output is in file nohup.out.
+    # Save it to saved_models directory
+    log_filename = os.path.join(saved_models_dir, experiment_id + '.log')
+    nohup_filename = os.path.join(home, 'Software', 'cytometer', 'scripts', 'nohup.out')
+    if os.path.isfile(nohup_filename):
+        shutil.copy2(nohup_filename, log_filename)
