1e15661cf910c6600062d1667849c93e871becd5
Ramón Casero <rcasero@gmail.com>
2019-06-17 12:49:20 +0100

    add missing package dependencies

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -49,8 +49,8 @@ conda install -y matplotlib pillow
 conda install -y scikit-image scikit-learn h5py 
 conda install -y nose pytest
 pip install opencv-python pysto openslide-python
-pip install tifffile mahotas networkx
-conda install -y pandas six
+pip install tifffile mahotas networkx svgpathtools
+conda install -y pandas six statsmodels
 
 ########################################################################
 ## python environment for DeepCell
