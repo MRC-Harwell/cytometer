f957303d3b560c37b79888ec0a57024c814a6bab
Ramón Casero <rcasero@gmail.com>
2019-07-02 18:51:35 +0100

    minor

--- scripts/klf14_b6ntac_exp_0074_cnn_tissue_classifier_fcn.py
+++ scripts/klf14_b6ntac_exp_0074_cnn_tissue_classifier_fcn.py
@@ -42,7 +42,7 @@ import time
 import random
 
 # limit number of GPUs
-os.environ['CUDA_VISIBLE_DEVICES'] = '0,1,2,3'
+os.environ['CUDA_VISIBLE_DEVICES'] = '2,3'
 
 os.environ['KERAS_BACKEND'] = 'tensorflow'
 import keras
