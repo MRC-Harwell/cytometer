a4154b4deaec6de1189e6089c89973264b976bce
Ramón Casero <rcasero@gmail.com>
2019-07-02 18:51:45 +0100

    add cluster script for exp 0074

---
+++ scripts/klf14_b6ntac_exp_0074_cnn_tissue_classifier_fcn.sge.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#$ -P rittscher.prjc -q gpu8.q
+#$ -l gpu=4 -l gputype=p100
+#$ -cwd -V
+#$ -pe shmem 4
+
+echo "job ID: " $JOB_ID
+echo "Run on: " `hostname`
+echo "Started at: " `date`
+
+module load Anaconda3/5.1.0
+
+source activate cytometer_tensorflow
+
+python klf14_b6ntac_exp_0074_cnn_tissue_classifier_fcn.py
+
+echo "Finished at :"`date`
+exit 0
