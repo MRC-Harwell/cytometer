53380f9d6924222ebaf0d7ac2ef65b96616ae921
Ramón Casero <rcasero@har.mrc.ac.uk>
2019-07-31 00:34:41 +0100

    adjust the code so that we can reproduce Gianluca's training set

--- scripts/c3h_hfd_exp_0001_generate_training_images.py
+++ scripts/c3h_hfd_exp_0001_generate_training_images.py
@@ -46,8 +46,7 @@ box_size = 1001
 box_half_size = int((box_size - 1) / 2)
 n_samples = 5
 
-# explicit list of files, to avoid differences if the files in the directory change (note that the order of the
-# files is important, because it's used for the random seed when selecting training windows)
+# explicit list of files, to avoid different results if the files in the directory change
 hand_segmented_files_list = [
     'C3H-H339.7a  363-16 A1 - 2016-04-20 12.15.22_row_001108_col_007204.xcf',
     'C3H-H339.7a  363-16 A1 - 2016-04-20 12.15.22_row_010356_col_031316.xcf',
@@ -298,11 +297,12 @@ hand_segmented_files_list = [
     'C3HHM 3104.1e 371-16 B3 - 2016-05-04 16.20.08_row_008100_col_045204.xcf',
     'C3HHM 3104.1e 371-16 B3 - 2016-05-04 16.20.08_row_010060_col_008028.xcf'
 ]
-ndpi_files_list = foo
+ndpi_files_list = [x.split('_row')[0] + '.ndpi' for x in hand_segmented_files_list]
+ndpi_files_list = np.unique(ndpi_files_list)
 ndpi_files_list = [os.path.join(ndpi_dir, x) for x in ndpi_files_list]
 
-# Note: if you want to read the full list of KLF14*.ndpi
-# ndpi_files_list = glob.glob(os.path.join(ndpi_dir, 'KLF14*.ndpi'))
+# Note: if you want to read the full list of C3H*.ndpi
+# ndpi_files_list = glob.glob(os.path.join(ndpi_dir, 'C3H*.ndpi'))
 
 for i_file, ndpi_file in enumerate(ndpi_files_list):
 
@@ -358,7 +358,7 @@ for i_file, ndpi_file in enumerate(ndpi_files_list):
         lblareas = stats[:, cv2.CC_STAT_AREA]
 
         # labels of large components, that we assume correspond to tissue areas
-        labels_large = np.where(lblareas > 1e5)[0]
+        labels_large = np.where(lblareas > 5e5)[0]
         labels_large = list(labels_large)
 
         # label=0 is the background, so we remove it
@@ -394,69 +394,20 @@ for i_file, ndpi_file in enumerate(ndpi_files_list):
 
     # these are the low-res centroids within the segmentation masks that were randomly selected in the old script
     if OLD_CODE:
-        if os.path.basename(ndpi_file) == 'KLF14-B6NTAC-MAT-17.1c  46-16 C1 - 2016-02-01 14.02.04.ndpi':
-            sample_centroid = [(10716, 8924), (19676, 18716), (20860, 64580),
-                               (22276, 15492), (26108, 68956)]
-        elif os.path.basename(ndpi_file) == 'KLF14-B6NTAC-MAT-18.2d  60-16 C1 - 2016-02-03 13.13.57.ndpi':
-            sample_centroid = [(6788, 5516), (13860, 10964), (13980, 4956),
-                               (18380, 63068), (24076, 20404)]
-        elif os.path.basename(ndpi_file) == 'KLF14-B6NTAC-MAT-18.2b  58-16 C1 - 2016-02-03 11.10.52.ndpi':
-            sample_centroid = [(4852, 58628), (6124, 58212), (6268, 13820),
-                               (13820, 57052), (14220, 60412)]
-        elif os.path.basename(ndpi_file) == 'KLF14-B6NTAC-MAT-17.2c  66-16 C1 - 2016-02-04 11.46.39.ndpi':
-            sample_centroid = [(10372, 21228), (22956, 57156), (25644, 26132),
-                               (30820, 22204), (33004, 61228)]
-        elif os.path.basename(ndpi_file) == 'KLF14-B6NTAC-MAT-17.2f  68-16 C1 - 2016-02-04 15.05.54.ndpi':
-            sample_centroid = [(7500, 50372), (11452, 16252), (13348, 19316),
-                               (14332, 19564), (26132, 12148)]
-        elif os.path.basename(ndpi_file) == 'KLF14-B6NTAC-36.1a PAT 96-16 C1 - 2016-02-10 16.12.38.ndpi':
-            sample_centroid = [(7676, 28900), (9588, 28676), (12476, 31852),
-                               (13804, 8004), (21796, 55852)]
-        elif os.path.basename(ndpi_file) == 'KLF14-B6NTAC-MAT-18.1a  50-16 C1 - 2016-02-02 09.12.41.ndpi':
-            sample_centroid = [(4324, 55084), (6124, 59516), (12908, 10212),
-                               (24276, 18652), (28156, 18596)]
-        elif os.path.basename(ndpi_file) == 'KLF14-B6NTAC-MAT-18.2g  63-16 C1 - 2016-02-03 16.58.52.ndpi':
-            sample_centroid = []
-        elif os.path.basename(ndpi_file) == 'KLF14-B6NTAC-MAT-16.2d  214-16 C1 - 2016-02-17 16.02.46.ndpi':
-            sample_centroid = [(4124, 12524), (8532, 9804), (17044, 31228),
-                               (18852, 29164), (21804, 35412)]
-        elif os.path.basename(ndpi_file) == 'KLF14-B6NTAC-37.1d PAT 109-16 C1 - 2016-02-15 15.19.08.ndpi':
-            sample_centroid = [(11684, 51748), (12172, 49588), (12700, 52388),
-                               (16068, 7276), (20380, 19420)]
-        elif os.path.basename(ndpi_file) == 'KLF14-B6NTAC-MAT-18.1e  54-16 C1 - 2016-02-02 15.26.33.ndpi':
-            sample_centroid = [(9236, 18316), (16540, 18252), (16756, 63692),
-                               (24836, 55124), (29564, 19260)]
-        elif os.path.basename(ndpi_file) == 'KLF14-B6NTAC 36.1i PAT 104-16 C1 - 2016-02-12 12.14.38.ndpi':
-            sample_centroid = [(5348, 19844), (6548, 15236), (6652, 61724),
-                               (6900, 71980), (10356, 31316), (10732, 16692),
-                               (12828, 18388), (14980, 27052), (18780, 69468),
-                               (27388, 18468)]
-        elif os.path.basename(ndpi_file) == 'KLF14-B6NTAC-MAT-18.3d  224-16 C1 - 2016-02-26 11.13.53.ndpi':
-            sample_centroid = [(8212, 15364), (11004, 5988), (15004, 10364),
-                               (19556, 57972), (21812, 22916)]
-        elif os.path.basename(ndpi_file) == 'KLF14-B6NTAC-37.1c PAT 108-16 C1 - 2016-02-15 14.49.45.ndpi':
-            sample_centroid = [(1292, 4348), (7372, 8556), (12588, 53084),
-                               (12732, 39812), (16612, 37372)]
-        elif os.path.basename(ndpi_file) == 'KLF14-B6NTAC-MAT-18.3b  223-16 C2 - 2016-02-26 10.35.52.ndpi':
-            sample_centroid = [(5428, 58372), (12404, 54316), (13604, 24644),
-                               (14628, 69148), (19340, 17348)]
-        elif os.path.basename(ndpi_file) == 'KLF14-B6NTAC-PAT-37.4a  417-16 C1 - 2016-03-16 15.55.32.ndpi':
-            sample_centroid = [(6412, 12484), (13012, 19820), (31172, 25996),
-                               (34628, 40116), (35948, 41492)]
-        elif os.path.basename(ndpi_file) == 'KLF14-B6NTAC-PAT-36.3d  416-16 C1 - 2016-03-16 14.44.11.ndpi':
-            sample_centroid = [(10084, 58476), (16260, 58300), (19220, 61724),
-                               (21012, 57844), (23236, 11084)]
-        elif os.path.basename(ndpi_file) == 'KLF14-B6NTAC-36.1b PAT 97-16 C1 - 2016-02-10 17.38.06.ndpi':
-            sample_centroid = [(9644, 61660), (11852, 71620), (13300, 55476),
-                               (16332, 65356), (17204, 19444)]
-        elif os.path.basename(ndpi_file) == 'KLF14-B6NTAC-PAT-37.2g  415-16 C1 - 2016-03-16 11.47.52.ndpi':
-            sample_centroid = [(6124, 82236), (7436, 19092), (16556, 10292),
-                               (23100, 9220), (31860, 33476)]
-        elif os.path.basename(ndpi_file) == 'KLF14-B6NTAC 36.1c PAT 98-16 C1 - 2016-02-11 10.45.00.ndpi':
-            sample_centroid = [(2772, 5332), (7124, 13028), (16812, 17484),
-                               (19228, 15060), (29684, 15172)]
-        else:
-            raise ValueError('Unknown NDPI file:' + ndpi_file)
+
+        # get high resolution centroids for the current image according to the filenames
+        ndpi_file_base = os.path.basename(ndpi_file).split('.ndpi')[0]
+        idx = [ndpi_file_base in x for x in hand_segmented_files_list]
+        hand_segmented_files_current = np.array(hand_segmented_files_list)[idx]
+
+        # compute the centroid in high-res
+        sample_centroid_upsampled = []
+        for file in hand_segmented_files_current:
+            row = int(file.split('_row_')[1].split('_col_')[0])
+            col = int(file.split('_row_')[1].split('_col_')[1].split('.xcf')[0])
+            sample_centroid_upsampled.append(
+                (row, col)
+            )
 
     else:  # not OLD_CODE
 
@@ -471,23 +422,19 @@ for i_file, ndpi_file in enumerate(ndpi_files_list):
                 # ... add it to the list of random samples
                 sample_centroid.append((row, col))
 
-    # compute the centroid in high-res
-    sample_centroid_upsampled = []
-    for row, col in sample_centroid:
-        sample_centroid_upsampled.append(
-            (int(row * downsample_factor + np.round((downsample_factor - 1) / 2)),
-             int(col * downsample_factor + np.round((downsample_factor - 1) / 2)))
-        )
+        # compute the centroid in high-res
+        sample_centroid_upsampled = []
+        for row, col in sample_centroid:
+            sample_centroid_upsampled.append(
+                (int(row * downsample_factor + np.round((downsample_factor - 1) / 2)),
+                 int(col * downsample_factor + np.round((downsample_factor - 1) / 2)))
+            )
 
     # create the training dataset by sampling the full resolution image with boxes around the centroids
     for j in range(len(sample_centroid_upsampled)):
 
         # high resolution centroid
-        if OLD_CODE:
-            # Note: this is a bug. The centroid used should have been the high resolution one
-            row, col = sample_centroid[j]
-        else:
-            row, col = sample_centroid_upsampled[j]
+        row, col = sample_centroid_upsampled[j]
 
         # compute top-left corner of the box from the centroid
         box_corner_row = row - box_half_size
