a9863b9de784c6d22bf70bf33a6aed3a708c95f1
Ramón Casero <rcasero@gmail.com>
2019-07-31 18:33:07 +0100

    compute proportion of primes by row/col and diagonal/antidiagonal

--- scripts/primes_scratch.py
+++ scripts/primes_scratch.py
@@ -111,6 +111,60 @@ def diagonal_zigzag_square(x):
     return y
 
 
+def prop_primes(x):
+    """
+    Compute proportion of prime numbers in each row/column (even length squares) or diagonal/antidiagonal (odd length
+    squares).
+
+    :param x:
+    :return:
+    """
+
+    # length of square length
+    n = x.shape[0]
+
+    if n % 2 == 1:  # odd length square
+
+        prop_fw = []
+        for k in range(n - 1, -n, -1):
+            diag = np.diagonal(x, k)
+            prop_fw.append(np.count_nonzero(diag) / len(diag))
+
+        x = np.fliplr(x)
+        prop_bk = []
+        for k in range(n - 1, -n, -1):
+            diag = np.diagonal(x, k)
+            prop_bk.append(np.count_nonzero(diag) / len(diag))
+
+    else:  # even length square
+
+        prop_fw = np.sum(x, axis=1) / n  # rows
+        prop_bk = np.sum(x, axis=0) / n  # columns
+
+    if DEBUG:
+        if n % 2 == 1:  # odd length square
+
+            plt.clf()
+            (markers, stemlines, baseline) = plt.stem(range(n - 1, -n, -1), prop_fw, label='Diagonals')
+            plt.setp(markers, marker='D', markersize=6, markeredgecolor="C0", markeredgewidth=2)
+            plt.stem(range(n - 1, -n, -1), prop_bk, linefmt='C1', markerfmt='C1o', label='Antidiagonals')
+            plt.xlabel('Diagonal index')
+            plt.ylabel('Primes proportion')
+            plt.legend()
+
+        else:
+
+            plt.clf()
+            (markers, stemlines, baseline) = plt.stem(prop_fw, label='Rows')
+            plt.setp(markers, marker='D', markersize=6, markeredgecolor="C0", markeredgewidth=2)
+            plt.stem(prop_bk, linefmt='C1', markerfmt='C1o', label='Columns')
+            plt.xlabel('Row/column index')
+            plt.ylabel('Primes proportion')
+            plt.legend()
+
+    return np.array(prop_fw), np.array(prop_bk)
+
+
 ############################################################################################
 # example of number square
 #
@@ -159,8 +213,21 @@ n_fibo = 16
 fibo_len = fibonacci(0, 1, n_fibo + 1)
 
 # init dataframe with results
-df_total = pd.DataFrame([], columns=['i_fibo', 'n', 'is_primes_rank', 'primes_with_neigh_d', 'primes_with_neigh_4',
-                                     'time'])
+df_total = pd.DataFrame([], columns=['i_fibo', 'n', 'rank', 'primes_neigh_d', 'primes_neigh_4',
+                                     'prop_fw', 'prop_bk', 'time'])
+
+# for debugging purposes, this can be used to directly get the initial parameters for any matrix, without having to
+# check for primes, etc.
+if DEBUG:
+    n_fibo_to_stop = 15
+    last_number = 1
+    for i_fibo in range(1, n_fibo_to_stop + 1):
+        # length of current Fibonacci square
+        n = fibo_len[i_fibo]
+
+        # first and last numbers in the square
+        first_number = last_number
+        last_number = first_number + n ** 2  # one past the last number, to make use of range() easier
 
 # loop Fibonacci squares
 last_number = 1
@@ -208,6 +275,30 @@ for i_fibo in range(1, n_fibo + 1):
             for j in range(n):
                 plt.text(j, i, '{:d}'.format(x_square[i, j]), color='w', ha='center', va='center', fontsize=int(72/n))
 
+    # compute proportion of primes in each diagonal or lateral line
+    prop_fw, prop_bk = prop_primes(is_prime_square)
+
+    if DEBUG:
+        if n % 2 == 1:  # odd length square
+
+            plt.clf()
+            (markers, stemlines, baseline) = plt.stem(range(n - 1, -n, -1), prop_fw, label='Diagonals')
+            plt.setp(markers, marker='D', markersize=6, markeredgecolor="C0", markeredgewidth=2)
+            plt.stem(range(n - 1, -n, -1), prop_bk, linefmt='C1', markerfmt='C1o', label='Antidiagonals')
+            plt.xlabel('Diagonal index')
+            plt.ylabel('Primes proportion')
+            plt.legend()
+
+        else:
+
+            plt.clf()
+            (markers, stemlines, baseline) = plt.stem(prop_fw, label='Rows')
+            plt.setp(markers, marker='D', markersize=6, markeredgecolor="C0", markeredgewidth=2)
+            plt.stem(prop_bk, linefmt='C1', markerfmt='C1o', label='Columns')
+            plt.xlabel('Row/column index')
+            plt.ylabel('Primes proportion')
+            plt.legend()
+
     # compute neighbourhood connectivity
     df = pixel_connectivity(is_prime_square, x_square)
 
@@ -223,8 +314,9 @@ for i_fibo in range(1, n_fibo + 1):
     counts_4 = np.array([0, 0, 0, 0])
     counts_4[idx] = counts
 
-    df_total = df_total.append({'i_fibo': i_fibo, 'n': n, 'is_primes_rank': np.linalg.matrix_rank(is_prime_square),
-                                'primes_with_neigh_d': counts_d, 'primes_with_neigh_4': counts_4,
+    df_total = df_total.append({'i_fibo': i_fibo, 'n': n, 'rank': np.linalg.matrix_rank(is_prime_square),
+                                'primes_neigh_d': counts_d, 'primes_neigh_4': counts_4,
+                                'prop_fw': prop_fw, 'prop_bk': prop_bk,
                                 'time': (time.time() - t0)},
                                ignore_index=True)
 
@@ -245,13 +337,13 @@ n_fibo = 16
 fibo_len = fibonacci(0, 1, n_fibo + 1)
 
 # init dataframe with results
-df_total = pd.DataFrame([], columns=['i_fibo', 'n', 'is_primes_rank', 'primes_with_neigh_d', 'primes_with_neigh_4',
+df_total = pd.DataFrame([], columns=['i_fibo', 'n', 'rank', 'primes_neigh_d', 'primes_neigh_4',
                                      'time'])
 
 # for debugging purposes, this can be used to directly get the initial parameters for any matrix, without having to
 # check for primes, etc.
 if DEBUG:
-    n_fibo_to_stop = 7
+    n_fibo_to_stop = 9
     last_number = 1
     for i_fibo in range(1, n_fibo_to_stop + 1):
         # length of current Fibonacci square
@@ -326,8 +418,8 @@ for i_fibo in range(1, n_fibo + 1):
     counts_4 = np.array([0, 0, 0, 0])
     counts_4[idx] = counts
 
-    df_total = df_total.append({'i_fibo': i_fibo, 'n': n, 'is_primes_rank': np.linalg.matrix_rank(is_prime_square),
-                                'primes_with_neigh_d': counts_d, 'primes_with_neigh_4': counts_4,
+    df_total = df_total.append({'i_fibo': i_fibo, 'n': n, 'rank': np.linalg.matrix_rank(is_prime_square),
+                                'primes_neigh_d': counts_d, 'primes_neigh_4': counts_4,
                                 'time': (time.time() - t0)},
                                ignore_index=True)
 
