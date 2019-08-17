e641e1f81053dccda1eaeb338592849442fedb02
Ramón Casero <r.casero@har.mrc.ac.uk>
2019-08-17 01:16:08 +0100

    add exp 0085 to train dmap cnn with full images

---
+++ scripts/klf14_b6ntac_exp_0085_cnn_dmap.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#$ -P rittscher.prjc -q gpu8.q
+#$ -l gpu=4 -l gputype=p100
+#$ -cwd -V
+#$ -pe shmem 4
+
+echo "job ID: " $JOB_ID
+echo "Run on: " `hostname`
+echo "Started at: " `date`
+
+module load Anaconda3/5.1.0
+
+source activate cytometer_tensorflow
+
+python klf14_b6ntac_exp_0085_cnn_dmap.py
+
+echo "Finished at :"`date`
+exit 0
