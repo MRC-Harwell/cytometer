2f025e7a019e711e7aeca71866e058f61c119330
Ramón Casero <r.casero@har.mrc.ac.uk>
2019-08-23 00:43:39 +0100

    update old code for modern experiment

--- scripts/klf14_b6ntac_exp_0089_cnn_segmentation_correction_overlapping_scaled_contours.py
+++ scripts/klf14_b6ntac_exp_0089_cnn_segmentation_correction_overlapping_scaled_contours.py
@@ -1,5 +1,5 @@
 """
-New approach to Quality Network, using sherrah2016 CNN.
+Segmentation Correction Network.
 
 Use original hand traced cells (with overlaps) for training.
 
@@ -19,6 +19,10 @@ Training for the CNN:
 * Other: mask for the loss function, to avoid looking too far from the cell.
 """
 
+# script name to identify this experiment
+experiment_id = 'klf14_b6ntac_exp_0089_cnn_segmentation_correction_overlapping_scaled_contours'
+print('Experiment ID: ' + experiment_id)
+
 # cross-platform home directory
 from pathlib import Path
 home = str(Path.home())
@@ -29,7 +33,7 @@ import os
 import sys
 sys.path.extend([os.path.join(home, 'Software/cytometer')])
 import pickle
-import inspect
+import json
 
 # other imports
 import glob
@@ -40,15 +44,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 
-# limit number of GPUs
-os.environ['CUDA_VISIBLE_DEVICES'] = '0,1,2,3'
+# # limit number of GPUs
+# os.environ['CUDA_VISIBLE_DEVICES'] = '0,1,2,3'
 
 os.environ['KERAS_BACKEND'] = 'tensorflow'
 import keras
 import keras.backend as K
+import keras_contrib
 
 from keras.models import Model
-from keras.layers import Input, Conv2D, MaxPooling2D, AvgPool2D, Activation
+from keras.layers import Input, Conv2D, MaxPooling2D, AvgPool2D, Activation, BatchNormalization
 
 # for data parallelism in keras models
 from keras.utils import multi_gpu_model
@@ -58,11 +63,11 @@ import cytometer.utils
 import cytometer.data
 import tensorflow as tf
 
-# limit GPU memory used
-from keras.backend.tensorflow_backend import set_session
-config = tf.ConfigProto()
-config.gpu_options.per_process_gpu_memory_fraction = 0.95
-set_session(tf.Session(config=config))
+# # limit GPU memory used
+# from keras.backend.tensorflow_backend import set_session
+# config = tf.ConfigProto()
+# config.gpu_options.per_process_gpu_memory_fraction = 0.95
+# set_session(tf.Session(config=config))
 
 # specify data format as (n, row, col, channel)
 K.set_image_data_format('channels_last')
@@ -70,7 +75,7 @@ K.set_image_data_format('channels_last')
 DEBUG = False
 
 # number of epochs for training
-epochs = 25
+epochs = 100
 
 # area (pixel**2) of the smallest object we accept as a cell (pi * (16 pixel)**2 = 804.2 pixel**2)
 smallest_cell_area = 804
@@ -85,60 +90,7 @@ smallest_dice = 0.5
 dice_threshold = 0.9
 
 # batch size for training
-batch_size = 16
-
-
-'''CNN Model
-'''
-
-
-def fcn_sherrah2016_regression(input_shape, for_receptive_field=False):
-
-    cnn_input = Input(shape=input_shape, dtype='float32', name='input_image')
-
-    x = Conv2D(filters=32, kernel_size=(5, 5), strides=1, dilation_rate=1, padding='same')(cnn_input)
-    if for_receptive_field:
-        x = Activation('linear')(x)
-        x = AvgPool2D(pool_size=(3, 3), strides=1, padding='same')(x)
-    else:
-        x = Activation('relu')(x)
-        x = MaxPooling2D(pool_size=(3, 3), strides=1, padding='same')(x)
-
-    x = Conv2D(filters=int(96/2), kernel_size=(5, 5), strides=1, dilation_rate=2, padding='same')(x)
-    if for_receptive_field:
-        x = Activation('linear')(x)
-        x = AvgPool2D(pool_size=(5, 5), strides=1, padding='same')(x)
-    else:
-        x = Activation('relu')(x)
-        x = MaxPooling2D(pool_size=(5, 5), strides=1, padding='same')(x)
-
-    x = Conv2D(filters=int(128/2), kernel_size=(3, 3), strides=1, dilation_rate=4, padding='same')(x)
-    if for_receptive_field:
-        x = Activation('linear')(x)
-        x = AvgPool2D(pool_size=(9, 9), strides=1, padding='same')(x)
-    else:
-        x = Activation('relu')(x)
-        x = MaxPooling2D(pool_size=(9, 9), strides=1, padding='same')(x)
-
-    x = Conv2D(filters=int(196/2), kernel_size=(3, 3), strides=1, dilation_rate=8, padding='same')(x)
-    if for_receptive_field:
-        x = Activation('linear')(x)
-        x = AvgPool2D(pool_size=(17, 17), strides=1, padding='same')(x)
-    else:
-        x = Activation('relu')(x)
-        x = MaxPooling2D(pool_size=(17, 17), strides=1, padding='same')(x)
-
-    x = Conv2D(filters=int(512/2), kernel_size=(3, 3), strides=1, dilation_rate=16, padding='same')(x)
-    if for_receptive_field:
-        x = Activation('linear')(x)
-    else:
-        x = Activation('relu')(x)
-
-    # regression output
-    regression_output = Conv2D(filters=1, kernel_size=(1, 1), strides=1, dilation_rate=1, padding='same',
-                               name='regression_output')(x)
-
-    return Model(inputs=cnn_input, outputs=[regression_output])
+batch_size = 10
 
 
 '''Directories and filenames
@@ -152,27 +104,94 @@ training_non_overlap_data_dir = os.path.join(root_data_dir, 'klf14_b6ntac_traini
 training_augmented_dir = os.path.join(root_data_dir, 'klf14_b6ntac_training_augmented')
 saved_models_dir = os.path.join(root_data_dir, 'saved_models')
 
-saved_contour_model_basename = 'klf14_b6ntac_exp_0055_cnn_contour'
+saved_kfolds_filename = 'klf14_b6ntac_exp_0079_generate_kfolds.pickle'
 
-# script name to identify this experiment
-experiment_id = inspect.getfile(inspect.currentframe())
-if experiment_id == '<input>':
-    experiment_id = 'unknownscript'
-else:
-    experiment_id = os.path.splitext(os.path.basename(experiment_id))[0]
-print('Experiment ID: ' + experiment_id)
+
+'''CNN Model
+'''
+
+
+def fcn_sherrah2016_regression(input_shape, for_receptive_field=False):
+
+    def activation_if(for_receptive_field, x):
+        if for_receptive_field:
+            # for estimation of receptive field, we need to use linear operators
+            x = Activation('linear')(x)
+            x = BatchNormalization(axis=3)(x)
+        else:
+            # for regular training and inference, we use non-linear operators
+            x = Activation('relu')(x)
+            x = BatchNormalization(axis=3)(x)
+        return x
+
+    def activation_pooling_if(for_receptive_field, pool_size, x):
+        if for_receptive_field:
+            # for estimation of receptive field, we need to use linear operators
+            x = Activation('linear')(x)
+            x = AvgPool2D(pool_size=pool_size, strides=1, padding='same')(x)
+            x = BatchNormalization(axis=3)(x)
+        else:
+            # for regular training and inference, we use non-linear operators
+            x = Activation('relu')(x)
+            x = MaxPooling2D(pool_size=pool_size, strides=1, padding='same')(x)
+            x = BatchNormalization(axis=3)(x)
+        return x
+
+    cnn_input = Input(shape=input_shape, dtype='float32', name='input_image')
+
+    x = Conv2D(filters=32, kernel_size=(5, 5), strides=1, dilation_rate=1, padding='same',
+               kernel_initializer='he_uniform')(cnn_input)
+    x = activation_pooling_if(for_receptive_field=for_receptive_field, pool_size=(3, 3), x=x)
+
+    x = Conv2D(filters=48, kernel_size=(5, 5), strides=1, dilation_rate=2, padding='same',
+               kernel_initializer='he_uniform')(x)
+    x = activation_pooling_if(for_receptive_field=for_receptive_field, pool_size=(5, 5), x=x)
+
+    x = Conv2D(filters=64, kernel_size=(3, 3), strides=1, dilation_rate=4, padding='same',
+               kernel_initializer='he_uniform')(x)
+    x = activation_pooling_if(for_receptive_field=for_receptive_field, pool_size=(9, 9), x=x)
+
+    x = Conv2D(filters=98, kernel_size=(3, 3), strides=1, dilation_rate=8, padding='same',
+               kernel_initializer='he_uniform')(x)
+    x = activation_pooling_if(for_receptive_field=for_receptive_field, pool_size=(17, 17), x=x)
+
+    x = Conv2D(filters=256, kernel_size=(3, 3), strides=1, dilation_rate=16, padding='same',
+               kernel_initializer='he_uniform')(x)
+    x = activation_if(for_receptive_field=for_receptive_field, x=x)
+
+    # dimensionality reduction to 1 feature map
+    x = Conv2D(filters=64, kernel_size=(1, 1), strides=1, dilation_rate=1, padding='same',
+               kernel_initializer='he_uniform')(x)
+    x = activation_if(for_receptive_field=for_receptive_field, x=x)
+
+    x = Conv2D(filters=8, kernel_size=(1, 1), strides=1, dilation_rate=1, padding='same',
+               kernel_initializer='he_uniform')(x)
+    x = activation_if(for_receptive_field=for_receptive_field, x=x)
+
+    regression_output = Conv2D(filters=1, kernel_size=(1, 1), strides=1, dilation_rate=1, padding='same',
+                               kernel_initializer='he_uniform', name='regression_output')(x)
+
+    return Model(inputs=cnn_input, outputs=[regression_output])
+
+
+'''Load folds'''
 
 # load list of images, and indices for training vs. testing indices
-contour_model_kfold_filename = os.path.join(saved_models_dir, saved_contour_model_basename + '_kfold_info.pickle')
+contour_model_kfold_filename = os.path.join(saved_models_dir, saved_kfolds_filename)
 with open(contour_model_kfold_filename, 'rb') as f:
     aux = pickle.load(f)
-file_list = aux['file_list']
+file_svg_list = aux['file_list']
 idx_test_all = aux['idx_test']
 idx_train_all = aux['idx_train']
-n_folds = len(idx_train_all)
+
+# correct home directory
+file_svg_list = [x.replace('/home/rcasero', home) for x in file_svg_list]
 
 # number of images
-n_im = len(file_list)
+n_im = len(file_svg_list)
+
+# number of folds
+n_folds = len(idx_test_all)
 
 '''Process the data
 '''
@@ -185,9 +204,9 @@ window_im_all = []
 window_out_all = []
 window_mask_loss_all = []
 window_idx_all = []
-for i, file_svg in enumerate(file_list):
+for i, file_svg in enumerate(file_svg_list):
 
-    print('file ' + str(i) + '/' + str(len(file_list) - 1))
+    print('file ' + str(i) + '/' + str(len(file_svg_list) - 1))
 
     # change file extension from .svg to .tif
     file_tif = file_svg.replace('.svg', '.tif')
@@ -277,18 +296,18 @@ for i, file_svg in enumerate(file_list):
                 plt.subplot(221)
                 plt.cla()
                 plt.imshow(im_array)
-                plt.contour(cell_seg_gtruth, linewidths=1, levels=0.5, colors='green')
-                plt.contour(cell_seg, linewidths=1, levels=0.5, colors='blue')
-                plt.contour(cell_mask_loss, linewidths=1, levels=0.5, colors='red')
+                plt.contour(cell_seg_gtruth, linewidths=1, levels=[0.5], colors='green')
+                plt.contour(cell_seg, linewidths=1, levels=[0.5], colors='blue')
+                plt.contour(cell_mask_loss, linewidths=1, levels=[0.5], colors='red')
                 plt.plot((bbox_x0, bbox_xend, bbox_xend, bbox_x0, bbox_x0),
                          (bbox_y0, bbox_y0, bbox_yend, bbox_yend, bbox_y0), 'black')
 
                 plt.subplot(222)
                 plt.cla()
                 plt.imshow(window_im)
-                plt.contour(window_seg_gtruth, linewidths=1, levels=0.5, colors='green')
-                plt.contour(window_seg, linewidths=1, levels=0.5, colors='blue')
-                plt.contour(window_mask_loss, linewidths=1, levels=0.5, colors='red')
+                plt.contour(window_seg_gtruth, linewidths=1, levels=[0.5], colors='green')
+                plt.contour(window_seg, linewidths=1, levels=[0.5], colors='blue')
+                plt.contour(window_mask_loss, linewidths=1, levels=[0.5], colors='red')
 
             # input to the CNN: multiply histology by +1/-1 segmentation mask
             window_im = \
@@ -324,7 +343,7 @@ for i, file_svg in enumerate(file_list):
                 aux = 0.2989 * window_im[:, :, 0] + 0.5870 * window_im[:, :, 1] + 0.1140 * window_im[:, :, 2]
                 plt.imshow(aux)
                 plt.contour(window_out, linewidths=1, levels=(-0.5, 0.5), colors='white')
-                plt.contour(window_mask_loss, linewidths=1, levels=0.5, colors='red')
+                plt.contour(window_mask_loss, linewidths=1, levels=[0.5], colors='red')
 
             # add dummy dimensions for keras
             window_im = np.expand_dims(window_im, axis=0)
@@ -375,10 +394,9 @@ if DEBUG:
 device_list = K.get_session().list_devices()
 
 # number of GPUs
-gpu_number = np.count_nonzero(['GPU' in str(x) for x in device_list])
+gpu_number = np.count_nonzero([':GPU:' in str(x) for x in device_list])
 
-# HACK: skip folds that are already computed
-for i_fold in range(10, n_folds):
+for i_fold in range(n_folds):
 
     print('# Fold ' + str(i_fold) + '/' + str(n_folds - 1))
 
@@ -407,82 +425,59 @@ for i_fold in range(10, n_folds):
     with tf.device('/cpu:0'):
         model = fcn_sherrah2016_regression(input_shape=window_im_train.shape[1:])
 
+    # output filenames
     saved_model_filename = os.path.join(saved_models_dir, experiment_id + '_model_fold_' + str(i_fold) + '.h5')
+    saved_logs_dir = os.path.join(saved_models_dir, experiment_id + '_logs_fold_' + str(i_fold))
 
-    if gpu_number > 1:  # compile and train model: Multiple GPUs
+    # checkpoint to save model after each epoch
+    checkpointer = cytometer.model_checkpoint_parallel.ModelCheckpoint(filepath=saved_model_filename,
+                                                                       verbose=1, save_best_only=False)
+    # callback to write a log for TensorBoard
+    # Note: run this on the server where the training is happening:
+    #       tensorboard --logdir=saved_logs_dir
+    tensorboard = keras.callbacks.TensorBoard(log_dir=saved_logs_dir)
 
-        # checkpoint to save model after each epoch
-        checkpointer = cytometer.model_checkpoint_parallel.ModelCheckpoint(filepath=saved_model_filename,
-                                                                           verbose=1, save_best_only=True)
-        # compile model
-        parallel_model = multi_gpu_model(model, gpus=gpu_number)
-        parallel_model.compile(loss={'regression_output': 'mse'},
-                               optimizer='Adadelta',
-                               metrics={'regression_output': ['mse', 'mae']},
-                               sample_weight_mode='element')
+    # call for cyclical learning rate
+    clr = keras_contrib.callbacks.CyclicLR(
+        mode='triangular2',
+        base_lr=1e-7,
+        max_lr=1e-2,
+        step_size=8 * (window_im_train.shape[0] // batch_size))
 
-        # train model
-        tic = datetime.datetime.now()
-        parallel_model.fit(window_im_train,
-                           {'regression_output': window_out_train},
-                           sample_weight={'regression_output': window_mask_loss_train},
-                           validation_data=(window_im_test,
-                                            {'regression_output': window_out_test},
-                                            {'regression_output': window_mask_loss_test}),
-                           batch_size=10, epochs=epochs, initial_epoch=0,
-                           callbacks=[checkpointer])
-        toc = datetime.datetime.now()
-        print('Training duration: ' + str(toc - tic))
+    # compile model
+    parallel_model = multi_gpu_model(model, gpus=gpu_number)
+    parallel_model.compile(loss={'regression_output': 'mse'},
+                           optimizer='Adadelta',
+                           metrics={'regression_output': ['mse', 'mae']},
+                           sample_weight_mode='element')
 
-    else:  # compile and train model: One GPU
+    # train model
+    tic = datetime.datetime.now()
+    hist = parallel_model.fit(window_im_train,
+                              {'regression_output': window_out_train},
+                              sample_weight={'regression_output': window_mask_loss_train},
+                              validation_data=(window_im_test,
+                                               {'regression_output': window_out_test},
+                                               {'regression_output': window_mask_loss_test}),
+                              batch_size=10, epochs=epochs, initial_epoch=0,
+                              callbacks=[checkpointer])
+    toc = datetime.datetime.now()
+    print('Training duration: ' + str(toc - tic))
 
-        # checkpoint to save model after each epoch
-        checkpointer = keras.callbacks.ModelCheckpoint(filepath=saved_model_filename,
-                                                       verbose=1, save_best_only=True)
+    # cast history values to a type that is JSON serializable
+    history = hist.history
+    for key in history.keys():
+        history[key] = list(map(float, history[key]))
 
-        # compile model
-        model.compile(loss={'regression_output': 'mse'},
-                      optimizer='Adadelta',
-                      metrics={'regression_output': ['mse', 'mae']},
-                      sample_weight_mode='element')
+    # save training history
+    history_filename = os.path.join(saved_models_dir, experiment_id + '_history_fold_' + str(i_fold) + '.json')
+    with open(history_filename, 'w') as f:
+        json.dump(history, f)
 
-        # train model
-        tic = datetime.datetime.now()
-        model.fit(window_im_train,
-                  {'regression_output': window_out_train},
-                  sample_weight={'regression_output': window_mask_loss_train},
-                  validation_data=(window_im_test,
-                                   {'regression_output': window_out_test},
-                                   {'regression_output': window_mask_loss_test}),
-                  batch_size=10, epochs=epochs, initial_epoch=0,
-                  callbacks=[checkpointer])
-        toc = datetime.datetime.now()
-        print('Training duration: ' + str(toc - tic))
-
-        cytometer.utils.clear_mem()
-        del window_im_train
-        del window_im_test
-        del window_out_train
-        del window_out_test
-        del window_mask_loss_train
-        del window_mask_loss_test
-
-
-'''Save the log of computations
-'''
-
-# if we run the script with qsub on the cluster, the standard output is in file
-# klf14_b6ntac_exp_0001_cnn_dmap_contour.sge.sh.oPID where PID is the process ID
-# Save it to saved_models directory
-log_filename = os.path.join(saved_models_dir, experiment_id + '.log')
-stdout_filename = os.path.join(home, 'Software', 'cytometer', 'scripts', experiment_id + '.sge.sh.o*')
-stdout_filename = glob.glob(stdout_filename)[0]
-if stdout_filename and os.path.isfile(stdout_filename):
-    shutil.copy2(stdout_filename, log_filename)
-else:
-    # if we ran the script with nohup in linux, the standard output is in file nohup.out.
-    # Save it to saved_models directory
-    log_filename = os.path.join(saved_models_dir, experiment_id + '.log')
-    nohup_filename = os.path.join(home, 'Software', 'cytometer', 'scripts', 'nohup.out')
-    if os.path.isfile(nohup_filename):
-        shutil.copy2(nohup_filename, log_filename)
+    cytometer.utils.clear_mem()
+    del window_im_train
+    del window_im_test
+    del window_out_train
+    del window_out_test
+    del window_mask_loss_train
+    del window_mask_loss_test
