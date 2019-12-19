d2f7ebfc108776a4dd35c75a0b418c16b8121ca3
Ramón Casero <rcasero@gmail.com>
2019-12-19 16:45:45 +0000

    plots of EDT and contour CNNs

--- scripts/klf14_b6ntac_exp_0099_paper_figures.py
+++ scripts/klf14_b6ntac_exp_0099_paper_figures.py
@@ -15,8 +15,13 @@ home = str(Path.home())
 
 import os
 import sys
-import pickle
 sys.path.extend([os.path.join(home, 'Software/cytometer')])
+
+########################################################################################################################
+## Plots of get_next_roi_to_process()
+########################################################################################################################
+
+import pickle
 import cytometer.utils
 
 # Filter out INFO & WARNING messages
@@ -37,11 +42,15 @@ from cytometer.utils import rough_foreground_mask
 import PIL
 from keras import backend as K
 
-# # limit GPU memory used
-# from keras.backend.tensorflow_backend import set_session
-# config = tf.ConfigProto()
-# config.gpu_options.per_process_gpu_memory_fraction = 0.95
-# set_session(tf.Session(config=config))
+LIMIT_GPU_MEMORY = False
+
+# limit GPU memory used
+if LIMIT_GPU_MEMORY:
+    import tensorflow as tf
+    from keras.backend.tensorflow_backend import set_session
+    config = tf.ConfigProto()
+    config.gpu_options.per_process_gpu_memory_fraction = 0.95
+    set_session(tf.Session(config=config))
 
 DEBUG = False
 SAVE_FIGS = False
@@ -50,7 +59,6 @@ root_data_dir = os.path.join(home, 'Data/cytometer_data/klf14')
 data_dir = os.path.join(home, 'scan_srv2_cox/Maz Yon')
 training_dir = os.path.join(home, root_data_dir, 'klf14_b6ntac_training')
 seg_dir = os.path.join(home, root_data_dir, 'klf14_b6ntac_seg')
-# figures_dir = os.path.join(root_data_dir, 'figures')
 figures_dir = os.path.join(home, 'GoogleDrive/Research/20190727_cytometer_paper/figures')
 saved_models_dir = os.path.join(root_data_dir, 'saved_models')
 results_dir = os.path.join(root_data_dir, 'klf14_b6ntac_results')
@@ -121,10 +129,6 @@ for i_fold in range(len(idx_test_all)):
     # add to the dictionary {file: fold}
     for file in file_ndpi_test:
         ndpi_files_test_list[file] = i_fold
-
-########################################################################################################################
-## Plots of get_next_roi_to_process()
-########################################################################################################################
 
 # File 4/19: KLF14-B6NTAC-MAT-17.1c  46-16 C1 - 2016-02-01 14.02.04. Fold = 2
 i_file = 4
@@ -410,6 +414,217 @@ while np.count_nonzero(lores_istissue) > 0:
         lores_istissue[lores_first_row:lores_last_row, lores_first_col:lores_last_col] = lores_todo_edge
 
 ########################################################################################################################
-## Show examples of what each deep CNN do
+## Show examples of what each deep CNN do (code cannibilised from the "inspect" scripts of the networks)
 ########################################################################################################################
 
+import json
+import pickle
+import warnings
+
+# other imports
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+
+from tensorboard.backend.event_processing import event_accumulator
+import pandas as pd
+
+# # limit number of GPUs
+# os.environ['CUDA_VISIBLE_DEVICES'] = '3'
+
+os.environ['KERAS_BACKEND'] = 'tensorflow'
+import keras
+import keras.backend as K
+
+import cytometer.data
+import cytometer.utils
+import cytometer.model_checkpoint_parallel
+import tensorflow as tf
+
+LIMIT_GPU_MEMORY = False
+
+# limit GPU memory used
+if LIMIT_GPU_MEMORY:
+    from keras.backend.tensorflow_backend import set_session
+    config = tf.ConfigProto()
+    config.gpu_options.per_process_gpu_memory_fraction = 0.9
+    set_session(tf.Session(config=config))
+
+# specify data format as (n, row, col, channel)
+K.set_image_data_format('channels_last')
+
+DEBUG = False
+
+'''Directories and filenames'''
+
+# data paths
+klf14_root_data_dir = os.path.join(home, 'Data/cytometer_data/klf14')
+klf14_training_dir = os.path.join(klf14_root_data_dir, 'klf14_b6ntac_training')
+klf14_training_non_overlap_data_dir = os.path.join(klf14_root_data_dir, 'klf14_b6ntac_training_non_overlap')
+klf14_training_augmented_dir = os.path.join(klf14_root_data_dir, 'klf14_b6ntac_training_augmented')
+figures_dir = os.path.join(home, 'GoogleDrive/Research/20190727_cytometer_paper/figures')
+saved_models_dir = os.path.join(klf14_root_data_dir, 'saved_models')
+
+saved_kfolds_filename = 'klf14_b6ntac_exp_0079_generate_kfolds.pickle'
+
+# model names
+dmap_model_basename = 'klf14_b6ntac_exp_0086_cnn_dmap'
+contour_model_basename = 'klf14_b6ntac_exp_0091_cnn_contour_after_dmap'
+classifier_model_basename = 'klf14_b6ntac_exp_0095_cnn_tissue_classifier_fcn'
+correction_model_basename = 'klf14_b6ntac_exp_0089_cnn_segmentation_correction_overlapping_scaled_contours'
+
+
+'''Load folds'''
+
+# load list of images, and indices for training vs. testing indices
+contour_model_kfold_filename = os.path.join(saved_models_dir, saved_kfolds_filename)
+with open(contour_model_kfold_filename, 'rb') as f:
+    aux = pickle.load(f)
+svg_file_list = aux['file_list']
+idx_test_all = aux['idx_test']
+idx_train_all = aux['idx_train']
+
+if DEBUG:
+    for i, file in enumerate(svg_file_list):
+        print(str(i) + ': ' + file)
+
+# correct home directory
+svg_file_list = [x.replace('/home/rcasero', home) for x in svg_file_list]
+
+# KLF14-B6NTAC-36.1b PAT 97-16 C1 - 2016-02-10 17.38.06_row_017204_col_019444.tif (fold 5 for testing. No .svg)
+# KLF14-B6NTAC-36.1b PAT 97-16 C1 - 2016-02-10 17.38.06_row_009644_col_061660.tif (fold 5 for testing. No .svg)
+# KLF14-B6NTAC 36.1c PAT 98-16 C1 - 2016-02-11 10.45.00_row_019228_col_015060.svg (fold 7 for testing. With .svg)
+
+# find which fold the testing image belongs to
+np.where(['36.1c' in x for x in svg_file_list])
+idx_test_all[7]
+
+# TIFF files that correspond to the SVG files (without augmentation)
+im_orig_file_list = []
+for i, file in enumerate(svg_file_list):
+    im_orig_file_list.append(file.replace('.svg', '.tif'))
+    im_orig_file_list[i] = os.path.join(os.path.dirname(im_orig_file_list[i]) + '_augmented',
+                                        'im_seed_nan_' + os.path.basename(im_orig_file_list[i]))
+
+    # check that files exist
+    if not os.path.isfile(file):
+        # warnings.warn('i = ' + str(i) + ': File does not exist: ' + os.path.basename(file))
+        warnings.warn('i = ' + str(i) + ': File does not exist: ' + file)
+    if not os.path.isfile(im_orig_file_list[i]):
+        # warnings.warn('i = ' + str(i) + ': File does not exist: ' + os.path.basename(im_orig_file_list[i]))
+        warnings.warn('i = ' + str(i) + ': File does not exist: ' + im_orig_file_list[i])
+
+'''Inspect model results'''
+
+# for i_fold, idx_test in enumerate(idx_test_all):
+i_fold = 7; idx_test = idx_test_all[i_fold]
+
+print('Fold ' + str(i_fold) + '/' + str(len(idx_test_all)-1))
+
+'''Load data'''
+
+# split the data list into training and testing lists
+im_test_file_list, im_train_file_list = cytometer.data.split_list(im_orig_file_list, idx_test)
+
+# load the test data (im, dmap, mask)
+test_dataset, test_file_list, test_shuffle_idx = \
+    cytometer.data.load_datasets(im_test_file_list, prefix_from='im', prefix_to=['im', 'dmap', 'mask', 'contour'],
+                                 nblocks=1, shuffle_seed=None)
+
+# fill in the little gaps in the mask
+kernel = np.ones((3, 3), np.uint8)
+for i in range(test_dataset['mask'].shape[0]):
+    test_dataset['mask'][i, :, :, 0] = cv2.dilate(test_dataset['mask'][i, :, :, 0].astype(np.uint8),
+                                                  kernel=kernel, iterations=1)
+
+# load dmap model, and adjust input size
+saved_model_filename = os.path.join(saved_models_dir, dmap_model_basename + '_model_fold_' + str(i_fold) + '.h5')
+dmap_model = keras.models.load_model(saved_model_filename)
+if dmap_model.input_shape[1:3] != test_dataset['im'].shape[1:3]:
+    dmap_model = cytometer.utils.change_input_size(dmap_model, batch_shape=test_dataset['im'].shape)
+
+# estimate dmaps
+pred_dmap = dmap_model.predict(test_dataset['im'], batch_size=4)
+
+if DEBUG:
+    for i in range(test_dataset['im'].shape[0]):
+        plt.clf()
+        plt.subplot(221)
+        plt.imshow(test_dataset['im'][i, :, :, :])
+        plt.axis('off')
+        plt.subplot(222)
+        plt.imshow(test_dataset['dmap'][i, :, :, 0])
+        plt.axis('off')
+        plt.subplot(223)
+        plt.imshow(test_dataset['mask'][i, :, :, 0])
+        plt.axis('off')
+        plt.subplot(224)
+        plt.imshow(pred_dmap[i, :, :, 0])
+        plt.axis('off')
+
+if DEBUG:
+
+    # KLF14-B6NTAC 36.1c PAT 98-16 C1 - 2016-02-11 10.45.00_row_019228_col_015060.svg
+    i = 2
+
+    plt.clf()
+    plt.imshow(test_dataset['im'][i, :, :, :])
+    plt.axis('off')
+    plt.tight_layout()
+    plt.savefig(os.path.join(figures_dir, os.path.basename(im_test_file_list[i]).replace('.tif', '.png')),
+                bbox_inches='tight')
+
+    plt.clf()
+    plt.imshow(test_dataset['dmap'][i, :, :, 0])
+    plt.axis('off')
+    plt.tight_layout()
+    plt.savefig(os.path.join(figures_dir, 'dmap_' + os.path.basename(im_test_file_list[i]).replace('.tif', '.png')),
+                bbox_inches='tight')
+
+    plt.clf()
+    plt.imshow(pred_dmap[i, :, :, 0])
+    plt.axis('off')
+    plt.tight_layout()
+    plt.savefig(os.path.join(figures_dir, 'pred_dmap_' + os.path.basename(im_test_file_list[i]).replace('.tif', '.png')),
+                bbox_inches='tight')
+
+# load dmap to contour model, and adjust input size
+saved_model_filename = os.path.join(saved_models_dir, contour_model_basename + '_model_fold_' + str(i_fold) + '.h5')
+contour_model = keras.models.load_model(saved_model_filename)
+if contour_model.input_shape[1:3] != pred_dmap.shape[1:3]:
+    contour_model = cytometer.utils.change_input_size(contour_model, batch_shape=pred_dmap.shape)
+
+# estimate dmaps
+pred_contour = contour_model.predict(pred_dmap, batch_size=4)
+
+if DEBUG:
+    plt.clf()
+    plt.imshow(test_dataset['contour'][i, :, :, 0])
+    plt.axis('off')
+    plt.tight_layout()
+    plt.savefig(os.path.join(figures_dir, 'contour_' + os.path.basename(im_test_file_list[i]).replace('.tif', '.png')),
+                bbox_inches='tight')
+
+    plt.clf()
+    plt.imshow(pred_contour[i, :, :, 0])
+    plt.axis('off')
+    plt.tight_layout()
+    plt.savefig(os.path.join(figures_dir, 'pred_contour_' + os.path.basename(im_test_file_list[i]).replace('.tif', '.png')),
+                bbox_inches='tight')
+
+# load dmap to contour model, and adjust input size
+saved_model_filename = os.path.join(saved_models_dir, classifier_model_basename + '_model_fold_' + str(i_fold) + '.h5')
+classifier_model = keras.models.load_model(saved_model_filename)
+if classifier_model.input_shape[1:3] != test_dataset['im'].shape[1:3]:
+    classifier_model = cytometer.utils.change_input_size(contour_model, batch_shape=test_dataset['im'].shape)
+
+# estimate dmaps
+pred_class = classifier_model.predict(test_dataset['im'], batch_size=4)
+
+if DEBUG:
+    plt.clf()
+    plt.imshow(pred_class[i, :, :, 0])
+    plt.axis('off')
+    plt.tight_layout()
+    plt.savefig(os.path.join(figures_dir, 'pred_class_' + os.path.basename(im_test_file_list[i]).replace('.tif', '.png')),
+                bbox_inches='tight')
