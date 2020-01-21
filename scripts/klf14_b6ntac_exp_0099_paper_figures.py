252b2b40fda00d917da218961f49fcb511405ffd
Ramón Casero <rcasero@gmail.com>
2020-01-21 17:00:19 +0000

    start code to tabulate the training data

--- scripts/klf14_b6ntac_exp_0099_paper_figures.py
+++ scripts/klf14_b6ntac_exp_0099_paper_figures.py
@@ -18,6 +18,90 @@ import sys
 sys.path.extend([os.path.join(home, 'Software/cytometer')])
 
 ########################################################################################################################
+## Explore training/test data of different folds
+########################################################################################################################
+
+import pickle
+# import warnings
+#
+# # other imports
+import numpy as np
+# import cv2
+# import matplotlib.pyplot as plt
+#
+# os.environ['KERAS_BACKEND'] = 'tensorflow'
+# import keras
+import keras.backend as K
+
+# import cytometer.data
+# import cytometer.utils
+# import cytometer.model_checkpoint_parallel
+import tensorflow as tf
+
+# from PIL import Image, ImageDraw
+
+# # limit number of GPUs
+# os.environ['CUDA_VISIBLE_DEVICES'] = '1'
+
+LIMIT_GPU_MEMORY = False
+
+# limit GPU memory used
+if LIMIT_GPU_MEMORY:
+    from keras.backend.tensorflow_backend import set_session
+    config = tf.ConfigProto()
+    config.gpu_options.per_process_gpu_memory_fraction = 0.9
+    set_session(tf.Session(config=config))
+
+# specify data format as (n, row, col, channel)
+K.set_image_data_format('channels_last')
+
+DEBUG = False
+
+'''Directories and filenames'''
+
+# data paths
+klf14_root_data_dir = os.path.join(home, 'Data/cytometer_data/klf14')
+klf14_training_dir = os.path.join(klf14_root_data_dir, 'klf14_b6ntac_training')
+klf14_training_non_overlap_data_dir = os.path.join(klf14_root_data_dir, 'klf14_b6ntac_training_non_overlap')
+klf14_training_augmented_dir = os.path.join(klf14_root_data_dir, 'klf14_b6ntac_training_augmented')
+figures_dir = os.path.join(home, 'GoogleDrive/Research/20190727_cytometer_paper/figures')
+saved_models_dir = os.path.join(klf14_root_data_dir, 'saved_models')
+ndpi_dir = os.path.join(home, 'scan_srv2_cox/Maz Yon')
+
+saved_kfolds_filename = 'klf14_b6ntac_exp_0079_generate_kfolds.pickle'
+
+
+'''klf14_b6ntac_exp_0076_generate_training_images'''
+
+# explicit list of files, to avoid differences if the files in the directory change (note that the order of the
+# files is important, because it's used for the random seed when selecting training windows)
+ndpi_files_list = [
+    'KLF14-B6NTAC-MAT-17.1c  46-16 C1 - 2016-02-01 14.02.04.ndpi',
+    'KLF14-B6NTAC-MAT-18.2d  60-16 C1 - 2016-02-03 13.13.57.ndpi',
+    'KLF14-B6NTAC-MAT-18.2b  58-16 C1 - 2016-02-03 11.10.52.ndpi',
+    'KLF14-B6NTAC-MAT-17.2c  66-16 C1 - 2016-02-04 11.46.39.ndpi',
+    'KLF14-B6NTAC-MAT-17.2f  68-16 C1 - 2016-02-04 15.05.54.ndpi',
+    'KLF14-B6NTAC-36.1a PAT 96-16 C1 - 2016-02-10 16.12.38.ndpi',
+    'KLF14-B6NTAC-MAT-18.1a  50-16 C1 - 2016-02-02 09.12.41.ndpi',
+    'KLF14-B6NTAC-MAT-18.2g  63-16 C1 - 2016-02-03 16.58.52.ndpi',
+    'KLF14-B6NTAC-MAT-16.2d  214-16 C1 - 2016-02-17 16.02.46.ndpi',
+    'KLF14-B6NTAC-37.1d PAT 109-16 C1 - 2016-02-15 15.19.08.ndpi',
+    'KLF14-B6NTAC-MAT-18.1e  54-16 C1 - 2016-02-02 15.26.33.ndpi',
+    'KLF14-B6NTAC 36.1i PAT 104-16 C1 - 2016-02-12 12.14.38.ndpi',
+    'KLF14-B6NTAC-MAT-18.3d  224-16 C1 - 2016-02-26 11.13.53.ndpi',
+    'KLF14-B6NTAC-37.1c PAT 108-16 C1 - 2016-02-15 14.49.45.ndpi',
+    'KLF14-B6NTAC-MAT-18.3b  223-16 C2 - 2016-02-26 10.35.52.ndpi',
+    'KLF14-B6NTAC-PAT-37.4a  417-16 C1 - 2016-03-16 15.55.32.ndpi',
+    'KLF14-B6NTAC-PAT-36.3d  416-16 C1 - 2016-03-16 14.44.11.ndpi',
+    'KLF14-B6NTAC-36.1b PAT 97-16 C1 - 2016-02-10 17.38.06.ndpi',
+    'KLF14-B6NTAC-PAT-37.2g  415-16 C1 - 2016-03-16 11.47.52.ndpi',
+    'KLF14-B6NTAC 36.1c PAT 98-16 C1 - 2016-02-11 10.45.00.ndpi'
+]
+ndpi_files_list = [os.path.join(ndpi_dir, x) for x in ndpi_files_list]
+
+
+
+########################################################################################################################
 ## Plots of get_next_roi_to_process()
 ########################################################################################################################
 
@@ -859,8 +943,3 @@ if DEBUG:
 ########################################################################################################################
 
 # This is done in klf14_b6ntac_exp_0098_full_slide_size_analysis_v7
-
-########################################################################################################################
-## Images for cytometer_v7.svg schematic (pipeline diagram)
-########################################################################################################################
-
