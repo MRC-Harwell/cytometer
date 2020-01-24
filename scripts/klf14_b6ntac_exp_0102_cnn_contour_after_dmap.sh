cc44fac912c890f0ab0d2d2f69b0d3d95c530583
Ramón Casero <rcasero@gmail.com>
2020-01-24 15:05:10 +0000

    use all data, instead of folds

--- scripts/klf14_b6ntac_exp_0102_cnn_contour_after_dmap.sh
+++ scripts/klf14_b6ntac_exp_0102_cnn_contour_after_dmap.sh
@@ -13,7 +13,7 @@ module load Anaconda3/5.1.0
 
 source activate cytometer_tensorflow
 
-python klf14_b6ntac_exp_0091_cnn_contour_after_dmap.py
+python klf14_b6ntac_exp_0102_cnn_contour_after_dmap.py
 
 echo "Finished at :"`date`
 exit 0
