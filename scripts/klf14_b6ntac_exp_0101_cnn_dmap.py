2846894633e1534406f036a90ace1eec79f549b8
Ramón Casero <rcasero@gmail.com>
2020-01-24 14:42:51 +0000

    use all data for training instead of folds

--- scripts/klf14_b6ntac_exp_0101_cnn_dmap.py
+++ scripts/klf14_b6ntac_exp_0101_cnn_dmap.py
@@ -1,5 +1,5 @@
 '''
-Dmap regression for all folds (KLF14 dataset).
+Dmap regression with all training data (KLF14 dataset).
 
 Training vs testing is done at the histology slide level, not at the window level. This way, we really guarantee that
 the network has not been trained with data sampled from the same image as the test data.
@@ -10,10 +10,12 @@ Like 0056, but:
     * Save training history variable, instead of relying on text output.
 Like 0081, but:
     * 500 epochs instead of 100.
+Like 0086, but:
+    * No folds. All data is used for training.
 '''
 
 # script name to identify this experiment
-experiment_id = 'klf14_b6ntac_exp_0086_cnn_dmap'
+experiment_id = 'klf14_b6ntac_exp_0101_cnn_dmap'
 
 # cross-platform home directory
 from pathlib import Path
@@ -129,15 +131,13 @@ def fcn_sherrah2016_regression(input_shape, for_receptive_field=False):
     return Model(inputs=input, outputs=[regression_output])
 
 
-'''Load folds'''
+'''Load folds (although we are not going to use folds, we still want the list of files)'''
 
 # load list of images, and indices for training vs. testing indices
 contour_model_kfold_filename = os.path.join(saved_models_dir, saved_kfolds_filename)
 with open(contour_model_kfold_filename, 'rb') as f:
     aux = pickle.load(f)
 svg_file_list = aux['file_list']
-idx_test_all = aux['idx_test']
-idx_train_all = aux['idx_train']
 
 # correct home directory
 svg_file_list = [x.replace('/home/rcasero', home) for x in svg_file_list]
@@ -159,123 +159,92 @@ for i, file in enumerate(svg_file_list):
         # warnings.warn('i = ' + str(i) + ': File does not exist: ' + os.path.basename(im_orig_file_list[i]))
         warnings.warn('i = ' + str(i) + ': File does not exist: ' + im_orig_file_list[i])
 
-# loop each fold: we split the data into train vs test, train a model, and compute errors with the
-# test data. In each fold, the test data is different
-# for i_fold, idx_test in enumerate(idx_test_all):
-for i_fold, idx_test in enumerate(idx_test_all[::-1]):
+'''Load data'''
 
-    print('Fold ' + str(i_fold) + '/' + str(len(idx_test_all)-1))
+# add the augmented image files
+im_orig_file_list = cytometer.data.augment_file_list(im_orig_file_list, '_nan_', '_*_')
 
-    '''Load data'''
+# load the train and test data (im, dmap, mask)
+train_dataset, train_file_list, train_shuffle_idx = \
+    cytometer.data.load_datasets(im_orig_file_list, prefix_from='im', prefix_to=['im', 'dmap', 'mask'],
+                                 nblocks=nblocks, shuffle_seed=0)
 
-    # split the data list into training and testing lists
-    im_test_file_list, im_train_file_list = cytometer.data.split_list(im_orig_file_list, idx_test)
+# remove training data where the mask has very few valid pixels (note: this will discard all the images without
+# cells)
+train_dataset = cytometer.data.remove_poor_data(train_dataset, prefix='mask', threshold=1000)
 
-    # add the augmented image files
-    im_test_file_list = cytometer.data.augment_file_list(im_test_file_list, '_nan_', '_*_')
-    im_train_file_list = cytometer.data.augment_file_list(im_train_file_list, '_nan_', '_*_')
+# fill in the little gaps in the mask
+kernel = np.ones((3, 3), np.uint8)
+for i in range(train_dataset['mask'].shape[0]):
+    train_dataset['mask'][i, :, :, 0] = cv2.dilate(train_dataset['mask'][i, :, :, 0].astype(np.uint8),
+                                                   kernel=kernel, iterations=1)
 
-    # load the train and test data (im, dmap, mask)
-    train_dataset, train_file_list, train_shuffle_idx = \
-        cytometer.data.load_datasets(im_train_file_list, prefix_from='im', prefix_to=['im', 'dmap', 'mask'],
-                                     nblocks=nblocks, shuffle_seed=i_fold)
-    test_dataset, test_file_list, test_shuffle_idx = \
-        cytometer.data.load_datasets(im_test_file_list, prefix_from='im', prefix_to=['im', 'dmap', 'mask'],
-                                     nblocks=nblocks, shuffle_seed=i_fold)
+if DEBUG:
+    i = 150
+    plt.clf()
+    for pi, prefix in enumerate(train_dataset.keys()):
+        plt.subplot(1, len(train_dataset.keys()), pi + 1)
+        if train_dataset[prefix].shape[-1] < 3:
+            plt.imshow(train_dataset[prefix][i, :, :, 0])
+        else:
+            plt.imshow(train_dataset[prefix][i, :, :, :])
+        plt.title('out[' + prefix + ']')
 
-    # remove training data where the mask has very few valid pixels (note: this will discard all the images without
-    # cells)
-    train_dataset = cytometer.data.remove_poor_data(train_dataset, prefix='mask', threshold=1000)
-    test_dataset = cytometer.data.remove_poor_data(test_dataset, prefix='mask', threshold=1000)
+'''Convolutional neural network training
 
-    # fill in the little gaps in the mask
-    kernel = np.ones((3, 3), np.uint8)
-    for i in range(test_dataset['mask'].shape[0]):
-        test_dataset['mask'][i, :, :, 0] = cv2.dilate(test_dataset['mask'][i, :, :, 0].astype(np.uint8),
-                                                      kernel=kernel, iterations=1)
-    for i in range(train_dataset['mask'].shape[0]):
-        train_dataset['mask'][i, :, :, 0] = cv2.dilate(train_dataset['mask'][i, :, :, 0].astype(np.uint8),
-                                                       kernel=kernel, iterations=1)
+Note: you need to use my branch of keras with the new functionality, that allows element-wise weights of the loss
+function
+'''
 
-    if DEBUG:
-        i = 150
-        plt.clf()
-        for pi, prefix in enumerate(train_dataset.keys()):
-            plt.subplot(1, len(train_dataset.keys()), pi + 1)
-            if train_dataset[prefix].shape[-1] < 3:
-                plt.imshow(train_dataset[prefix][i, :, :, 0])
-            else:
-                plt.imshow(train_dataset[prefix][i, :, :, :])
-            plt.title('out[' + prefix + ']')
+# list all CPUs and GPUs
+device_list = K.get_session().list_devices()
 
-        i = 22
-        plt.clf()
-        for pi, prefix in enumerate(test_dataset.keys()):
-            plt.subplot(1, len(test_dataset.keys()), pi + 1)
-            if test_dataset[prefix].shape[-1] < 3:
-                plt.imshow(test_dataset[prefix][i, :, :, 0])
-            else:
-                plt.imshow(test_dataset[prefix][i, :, :, :])
-            plt.title('out[' + prefix + ']')
+# number of GPUs
+gpu_number = np.count_nonzero([':GPU:' in str(x) for x in device_list])
 
-    '''Convolutional neural network training
-    
-    Note: you need to use my branch of keras with the new functionality, that allows element-wise weights of the loss
-    function
-    '''
+# instantiate model
+with tf.device('/cpu:0'):
+    model = fcn_sherrah2016_regression(input_shape=train_dataset['im'].shape[1:])
 
-    # list all CPUs and GPUs
-    device_list = K.get_session().list_devices()
+# output filenames
+saved_model_filename = os.path.join(saved_models_dir, experiment_id + '_model.h5')
+saved_logs_dir = os.path.join(saved_models_dir, experiment_id + '_logs')
 
-    # number of GPUs
-    gpu_number = np.count_nonzero([':GPU:' in str(x) for x in device_list])
+# checkpoint to save model after each epoch
+checkpointer = cytometer.model_checkpoint_parallel.ModelCheckpoint(filepath=saved_model_filename,
+                                                                   verbose=1, save_best_only=False)
 
-    # instantiate model
-    with tf.device('/cpu:0'):
-        model = fcn_sherrah2016_regression(input_shape=train_dataset['im'].shape[1:])
+# callback to write a log for TensorBoard
+# Note: run this on the server where the training is happening:
+#       tensorboard --logdir=saved_logs_dir
+tensorboard = keras.callbacks.TensorBoard(log_dir=saved_logs_dir)
 
-    # output filenames
-    saved_model_filename = os.path.join(saved_models_dir, experiment_id + '_model_fold_' + str(i_fold) + '.h5')
-    saved_logs_dir = os.path.join(saved_models_dir, experiment_id + '_logs_fold_' + str(i_fold))
+# compile model
+parallel_model = multi_gpu_model(model, gpus=gpu_number)
+parallel_model.compile(loss={'regression_output': 'mean_absolute_error'},
+                       optimizer='Adadelta',
+                       metrics={'regression_output': ['mse', 'mae']},
+                       sample_weight_mode='element')
 
-    # checkpoint to save model after each epoch
-    checkpointer = cytometer.model_checkpoint_parallel.ModelCheckpoint(filepath=saved_model_filename,
-                                                                       verbose=1, save_best_only=False)
+# train model
+tic = datetime.datetime.now()
+hist = parallel_model.fit(train_dataset['im'],
+                          {'regression_output': train_dataset['dmap']},
+                          sample_weight={'regression_output': train_dataset['mask'][..., 0]},
+                          batch_size=batch_size, epochs=epochs, initial_epoch=0,
+                          callbacks=[checkpointer, tensorboard])
+toc = datetime.datetime.now()
+print('Training duration: ' + str(toc - tic))
 
-    # callback to write a log for TensorBoard
-    # Note: run this on the server where the training is happening:
-    #       tensorboard --logdir=saved_logs_dir
-    tensorboard = keras.callbacks.TensorBoard(log_dir=saved_logs_dir)
+# cast history values to a type that is JSON serializable
+history = hist.history
+for key in history.keys():
+    history[key] = list(map(float, history[key]))
 
-    # compile model
-    parallel_model = multi_gpu_model(model, gpus=gpu_number)
-    parallel_model.compile(loss={'regression_output': 'mean_absolute_error'},
-                           optimizer='Adadelta',
-                           metrics={'regression_output': ['mse', 'mae']},
-                           sample_weight_mode='element')
-
-    # train model
-    tic = datetime.datetime.now()
-    hist = parallel_model.fit(train_dataset['im'],
-                              {'regression_output': train_dataset['dmap']},
-                              sample_weight={'regression_output': train_dataset['mask'][..., 0]},
-                              validation_data=(test_dataset['im'],
-                                               {'regression_output': test_dataset['dmap']},
-                                               {'regression_output': test_dataset['mask'][..., 0]}),
-                              batch_size=batch_size, epochs=epochs, initial_epoch=0,
-                              callbacks=[checkpointer, tensorboard])
-    toc = datetime.datetime.now()
-    print('Training duration: ' + str(toc - tic))
-
-    # cast history values to a type that is JSON serializable
-    history = hist.history
-    for key in history.keys():
-        history[key] = list(map(float, history[key]))
-
-    # save training history
-    history_filename = os.path.join(saved_models_dir, experiment_id + '_history_fold_' + str(i_fold) + '.json')
-    with open(history_filename, 'w') as f:
-        json.dump(history, f)
+# save training history
+history_filename = os.path.join(saved_models_dir, experiment_id + '_history.json')
+with open(history_filename, 'w') as f:
+    json.dump(history, f)
 
 
 if DEBUG:
@@ -283,10 +252,10 @@ if DEBUG:
         history = json.load(f)
 
     plt.clf()
-    plt.plot(history[i_fold]['mean_absolute_error'], label='mean_absolute_error')
+    plt.plot(history[0]['mean_absolute_error'], label='mean_absolute_error')
     # plt.plot(history[i_fold]['mean_squared_error'], label='mean_squared_error')
-    plt.plot(history[i_fold]['val_mean_absolute_error'], label='val_mean_absolute_error')
+    plt.plot(history[0]['val_mean_absolute_error'], label='val_mean_absolute_error')
     # plt.plot(history[i_fold]['val_mean_squared_error'], label='val_mean_squared_error')
-    plt.plot(history[i_fold]['loss'], label='loss')
-    plt.plot(history[i_fold]['val_loss'], label='val_loss')
+    plt.plot(history[0]['loss'], label='loss')
+    plt.plot(history[0]['val_loss'], label='val_loss')
     plt.legend()
