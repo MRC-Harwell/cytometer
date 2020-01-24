e8c51a4c705a60610569223b1e001bb58edf8094
Ramón Casero <rcasero@gmail.com>
2020-01-24 14:42:51 +0000

    use all data for training instead of folds

--- scripts/klf14_b6ntac_exp_0101_cnn_dmap.sh
+++ scripts/klf14_b6ntac_exp_0101_cnn_dmap.sh
@@ -13,7 +13,7 @@ module load Anaconda3/5.1.0
 
 source activate cytometer_tensorflow
 
-python klf14_b6ntac_exp_0086_cnn_dmap.py
+python klf14_b6ntac_exp_0101_cnn_dmap.py
 
 echo "Finished at :"`date`
 exit 0
