32d703f6744c804bfb05aea452e128b8debce847
Ramón Casero <rcasero@gmail.com>
2020-01-30 14:14:46 +0000

    add temp scripts to analyse more .ndpis on the rescomp cluster

---
+++ scripts/klf14_b6ntac_exp_0097_full_slide_pipeline_v7_reverse.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#$ -P rittscher.prjc -q gpu8.q
+#$ -l gpu=1 -l gputype=p100
+#$ -cwd -V
+#$ -pe shmem 1
+
+echo "job ID: " $JOB_ID
+echo "Run on: " `hostname`
+echo "Started at: " `date`
+
+module load Anaconda3/5.1.0
+
+source activate cytometer_tensorflow
+
+python klf14_b6ntac_exp_0097_full_slide_pipeline_v7_reverse.py
+
+echo "Finished at :"`date`
+exit 0
