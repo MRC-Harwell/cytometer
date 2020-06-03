f5b93901e16844a0efdba3308c916ec7e4c398d0
Ramón Casero <r.casero@har.mrc.ac.uk>
2020-06-03 17:33:35 +0100

    rename file

--- scripts/rreb1_tm1b_exp_0001_full_slide_pipeline_v7.py
+++ scripts/rreb1_tm1b_exp_0001_full_slide_pipeline_v7.py
@@ -64,7 +64,7 @@ Processing full slides of RREB1-TM1B_B6N-IC with pipeline v7:
 """
 
 # script name to identify this experiment
-experiment_id = 'rreb1-tm1b_exp_0001_full_slide_pipeline_v7.py'
+experiment_id = 'rreb1_tm1b_exp_0001_full_slide_pipeline_v7.py'
 
 # cross-platform home directory
 from pathlib import Path
