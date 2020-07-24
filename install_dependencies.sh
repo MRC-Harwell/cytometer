17ed70ad1bdb8d3aefa49b62f6ce5171dfce5573
Ramón Casero <r.casero@har.mrc.ac.uk>
2020-07-24 18:36:06 +0100

    remove unused code

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -100,39 +100,6 @@ else
     echo UBUNTU_VERSION=${UBUNTU_VERSION}
 fi
 
-## CUDA Toolkit 11
-#case ${UBUNTU_VERSION}
-#in
-#    16.04)
-#        wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-ubuntu1604.pin
-#        sudo mv cuda-ubuntu1604.pin /etc/apt/preferences.d/cuda-repository-pin-600
-#        sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
-#        sudo add-apt-repository "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/ /"
-#        sudo apt-get update
-#        sudo apt-get -y install cuda
-#        ;;
-#    18.04)
-#        wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
-#        sudo mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
-#        sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
-#        sudo add-apt-repository "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/ /"
-#        sudo apt-get update
-#        sudo apt-get -y install cuda
-#        ;;
-#    20.04)
-#        wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
-#        sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
-#        sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/7fa2af80.pub
-#        sudo add-apt-repository "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/ /"
-#        sudo apt-get update
-#        sudo apt-get -y install cuda
-#        ;;
-#    *)
-#        echo "Error: Ubuntu version not recognised: $UBUNTU_VERSION"
-#        exit 1
-#        ;;
-#esac
-
 # CUDA Toolkit 10.2
 case ${UBUNTU_VERSION}
 in
