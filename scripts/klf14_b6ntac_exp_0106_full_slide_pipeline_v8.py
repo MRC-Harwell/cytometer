fb990bf00b2bd9cbf9979a7bcc35b42d429b5cec
Ramón Casero <rcasero@har.mrc.ac.uk>
2020-10-14 16:35:10 +0100

    move CNN models (*.h5 files) to new deepcytometer_pipeline_v7 or v8 directories

--- scripts/klf14_b6ntac_exp_0106_full_slide_pipeline_v8.py
+++ scripts/klf14_b6ntac_exp_0106_full_slide_pipeline_v8.py
@@ -138,11 +138,10 @@ else:
 DEBUG = False
 SAVE_FIGS = False
 
-pipeline_root_data_dir = os.path.join(home, 'Data/cytometer_data/klf14')  # CNN models
 histology_dir = os.path.join(home, 'scan_srv2_cox/Maz Yon')
 histology_ext = '.ndpi'
 area2quantile_dir = os.path.join(home, 'Data/cytometer_data/deepcytometer_pipeline_v8')
-saved_models_dir = os.path.join(pipeline_root_data_dir, 'saved_models')
+saved_models_dir = os.path.join(home, 'Data/cytometer_data/deepcytometer_pipeline_v8')
 annotations_dir = os.path.join(home, 'bit/cytometer_data/aida_data_Klf14_v8/annotations')
 
 # file with area->quantile map precomputed from all automatically segmented slides in klf14_b6ntac_exp_0098_full_slide_size_analysis_v7.py
