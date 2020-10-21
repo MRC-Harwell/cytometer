dc826a6b18b88945cba9e6c859416793e5c9a9d6
Ramón Casero <r.casero@har.mrc.ac.uk>
2020-10-21 02:01:06 +0100

    add first plots of area estimation errors

--- scripts/klf14_b6ntac_exp_0108_pipeline_v7_validation.py
+++ scripts/klf14_b6ntac_exp_0108_pipeline_v7_validation.py
@@ -239,3 +239,135 @@ if DEBUG:
 
     plt.clf()
     plt.scatter(df_corrected_all['ref_area'], df_corrected_all['test_area'] / df_corrected_all['ref_area'] - 1)
+
+########################################################################################################################
+## median and CI of segmentation auto area error vs. hand traced area
+## Note: If we perform a sign test to see whether the median = 0, we would assume a binomial distribution of number of
+## values < median, and with a Gaussian approximation to the binomial distribution, we'd be performing a normal null
+## hypothesis test. which corresponds to a CI-95% of -1.96*std, +1.96*std around the median value.
+## https://youtu.be/dLTvZUrs-CI?t=463
+########################################################################################################################
+
+import scipy
+import more_itertools
+import itertools
+
+# load dataframes to file
+df_auto_all = pd.read_csv(dataframe_auto_filename)
+df_corrected_all = pd.read_csv(dataframe_corrected_filename)
+
+# remove hand traced cells with no auto match, as we don't need those here
+df_auto_all.dropna(subset=['test_idx'], inplace=True)
+
+# remove very low Dice indices, as those indicate overlap with a neighbour, rather than a proper segmentation
+df_auto_all = df_auto_all[df_auto_all['dice'] >= 0.5]
+
+# sort manual areas from smallest to largest
+df_auto_all.sort_values(by=['ref_area'], ascending=True, ignore_index=True, inplace=True)
+
+# add area error for convenience
+df_auto_all['test_ref_area_diff'] = df_auto_all['test_area'] - df_auto_all['ref_area']
+
+# bin the points so that each bin has the same number of points (roughly)
+median_window_size = 150
+padding = [None] * (median_window_size - 6)
+idx_split = more_itertools.windowed(itertools.chain(padding, range(len(df_auto_all['ref_area'])), padding),
+                                    n=median_window_size)
+# the median of all the values in the bin is the "central" value for this bin that is used in plots
+x_bins = [scipy.stats.mstats.hdquantiles(df_auto_all['ref_area'][np.array(idx)[~np.equal(idx, None)].astype(np.int)],
+                                         prob=[0.50], axis=0).data[0] for idx in idx_split]
+
+idx_split = more_itertools.windowed(itertools.chain(padding, range(len(df_auto_all['test_ref_area_diff'])), padding),
+                                    n=median_window_size)
+y_bins_q2 = [scipy.stats.mstats.hdquantiles(df_auto_all['test_ref_area_diff'][np.array(idx)[~np.equal(idx, None)].astype(np.int)],
+                                            prob=[0.50], axis=0).data[0] for idx in idx_split]
+
+idx_split = more_itertools.windowed(itertools.chain(padding, range(len(df_auto_all['test_ref_area_diff'])), padding),
+                                    n=median_window_size)
+y_bins_q2_std = [scipy.stats.mstats.hdquantiles_sd(df_auto_all['test_ref_area_diff'][np.array(idx)[~np.equal(idx, None)].astype(np.int)],
+                                                   prob=[0.50], axis=0).data[0] for idx in idx_split]
+
+x_bins = np.array(x_bins)
+y_bins_q2 = np.array(y_bins_q2)
+y_bins_q2_std = np.array(y_bins_q2_std)
+
+# 95% confidence interval for the median estimate
+y_bins_ci_lo = y_bins_q2 - 1.96 * y_bins_q2_std
+y_bins_ci_hi = y_bins_q2 + 1.96 * y_bins_q2_std
+
+if DEBUG:
+    plt.clf()
+    plt.plot([x_bins[0] * 1e-3, x_bins[-1] * 1e-3], [0, 0], 'k')
+    plt.plot(x_bins * 1e-3, 100 * (y_bins_q2 / x_bins - 1) , 'r')
+
+if DEBUG:
+    plt.clf()
+    plt.plot([x_bins[0] * 1e-3, x_bins[-1] * 1e-3], [0, 0], 'k')
+    plt.scatter(df_auto_all['ref_area'] * 1e-3, df_auto_all['test_ref_area_diff'] * 1e-3, s=1)
+    plt.fill_between(x_bins * 1e-3, y_bins_ci_lo * 1e-3, y_bins_ci_hi * 1e-3, facecolor='r', alpha=0.5)
+    plt.plot(x_bins * 1e-3, y_bins_q2 * 1e-3, 'r')
+    plt.tick_params(axis='both', which='major', labelsize=14)
+    plt.xlabel('Area$_{ht}$ ($10^3\ \mu m^2$)', fontsize=14)
+    plt.ylabel('Area$_{auto}$ - Area$_{ht}$ ($10^3\ \mu m^2$)', fontsize=14)
+    plt.xlim(x_bins[0] * 1e-3, x_bins[-1] * 1e-3)
+    plt.tight_layout()
+
+    plt.savefig(os.path.join(saved_figures_dir, 'exp_0108_area_auto_manual_error.svg'))
+    plt.savefig(os.path.join(saved_figures_dir, 'exp_0108_area_auto_manual_error.png'))
+
+    # zoom in
+    plt.xlim(-0.25, 14)
+    plt.ylim(-2.6, 2.5)
+    plt.tight_layout()
+
+    plt.savefig(os.path.join(saved_figures_dir, 'exp_0108_area_auto_manual_error_zoom.svg'))
+    plt.savefig(os.path.join(saved_figures_dir, 'exp_0108_area_auto_manual_error_zoom.png'))
+
+# auto area: plot area error as ratio
+df_ols = pd.DataFrame()
+df_ols['area_manual_bins'] = x_bins
+df_ols['area_auto_manual_diff_bins_q2'] = y_bins_q2
+df_ols['area_auto_diff_ratio_bins_q2'] = df_ols['area_auto_manual_diff_bins_q2'] / df_ols['area_manual_bins']
+
+# median of median error ratios for cells > 950 um^2
+area_error_ratio_q2 = np.median(df_ols['area_auto_diff_ratio_bins_q2'][df_ols['area_manual_bins'] > 950])
+
+# range of error ratios where we consider the error acceptable
+area_error_ratio_q2_lo = area_error_ratio_q2 - 10 / 100
+area_error_ratio_q2_hi = area_error_ratio_q2 + 10 / 100
+
+# plot
+plt.clf()
+plt.scatter(df_auto_all['ref_area'] * 1e-3,
+            df_auto_all['test_ref_area_diff'] / df_auto_all['ref_area'] * 100, s=1)
+plt.plot([df_ols['area_manual_bins'].iloc[0] * 1e-3, df_ols['area_manual_bins'].iloc[-1] * 1e-3],
+         [area_error_ratio_q2 * 100, area_error_ratio_q2 * 100], 'k', linewidth=2)
+plt.plot([df_ols['area_manual_bins'].iloc[0] * 1e-3, df_ols['area_manual_bins'].iloc[-1] * 1e-3],
+         [area_error_ratio_q2_lo * 100, area_error_ratio_q2_lo * 100], 'k--', linewidth=2)
+plt.plot([df_ols['area_manual_bins'].iloc[0] * 1e-3, df_ols['area_manual_bins'].iloc[-1] * 1e-3],
+         [area_error_ratio_q2_hi * 100, area_error_ratio_q2_hi * 100], 'k--', linewidth=2)
+plt.plot(df_ols['area_manual_bins'] * 1e-3, df_ols['area_auto_diff_ratio_bins_q2'] * 100, 'r')
+plt.fill_between(df_ols['area_manual_bins'] * 1e-3,
+                 y_bins_ci_lo / df_ols['area_manual_bins'] * 100, y_bins_ci_hi / df_ols['area_manual_bins'] * 100,
+                 facecolor='r', alpha=0.5)
+plt.tick_params(axis='both', which='major', labelsize=14)
+plt.xlabel('Area$_{ht}$ ($10^3\ \mu m^2$)', fontsize=14)
+plt.ylabel('Area$_{auto}$ / Area$_{ht}$ - 1 (%)', fontsize=14)
+plt.xlim(-0.25, 11)
+plt.ylim(-75, 100)
+plt.tight_layout()
+
+plt.savefig(os.path.join(saved_figures_dir, 'exp_0108_area_auto_diff_ratio_error.svg'))
+plt.savefig(os.path.join(saved_figures_dir, 'exp_0108_area_auto_diff_ratio_error.png'))
+
+# zoom in
+plt.ylim(-22, 18)
+plt.tight_layout()
+
+plt.savefig(os.path.join(saved_figures_dir, 'exp_0108_area_auto_diff_ratio_error_zoom.svg'))
+plt.savefig(os.path.join(saved_figures_dir, 'exp_0108_area_auto_diff_ratio_error_zoom.png'))
+
+# compute what proportion of cells are poorly segmented
+ecdf = sm.distributions.empirical_distribution.ECDF(df_manual_all['area_manual'])
+cell_area_threshold = 780
+print('Unusuable segmentations = ' + str(ecdf(cell_area_threshold)))
