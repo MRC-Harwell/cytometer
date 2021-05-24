45278e48a16de21f23d655ee7905196bdd8ded75
Ramón Casero <rcasero@gmail.com>
2021-05-24 16:50:59 +0100

    fix bug. Code line cannot be after comment

--- cytometer/__init__.py
+++ cytometer/__init__.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+from __future__ import absolute_import
+
 """
 Created on Thu Jun 29 18:21:57 2017
 
@@ -12,8 +14,6 @@ Copyright 2021 Medical Research Council
 SPDX-License-Identifier: Apache-2.0
 Author: Ramon Casero <rcasero@gmail.com>
 """
-
-from __future__ import absolute_import
 
 # cytometer package version (note: duplicated in /setup.py)
 __version__ = '1.0.0'
